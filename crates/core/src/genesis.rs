5ef5ec389b23c370c526cfb76a26e7659a8aaf5e
Federica <fedemoletta@hotmail.com>
2024-06-24 16:58:24 -0300

    Add some docs

--- crates/core/src/genesis.rs
+++ crates/core/src/genesis.rs
@@ -6,8 +6,11 @@ use serde::Deserialize;
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Genesis {
+    /// Chain configuration
     config: ChainConfig,
+    /// The initial state of the accounts in the genesis block.
     alloc: HashMap<H256, Account>,
+    /// Genesis header values
     coinbase: H256,
     difficulty: U256,
     extra_data: Vec<u8>,
@@ -17,14 +20,22 @@ pub struct Genesis {
     timestamp: u64,
 }
 
+
+/// Blockchain settings defined per block
 #[allow(unused)]
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct ChainConfig {
+    /// Current chain identifier
     chain_id: U256,
+
+    /// Block numbers for the block where each fork was activated
+    /// (None = no fork, 0 = fork is already active)
+
     homestead_block: Option<u64>,
 
     dao_fork_block: Option<u64>,
+    /// Whether the nodes supports or opposes the DAO hard-fork
     #[serde(default)]
     dao_fork_support: bool,
 
@@ -43,12 +54,17 @@ pub struct ChainConfig {
     gray_glacier_block: Option<u64>,
     merge_netsplit_block: Option<u64>,
 
+    /// Timestamp at which each fork was activated
+    /// (None = no fork, 0 = fork is already active)
+
     shangai_time: Option<u64>,
     cancun_time: Option<u64>,
     prague_time: Option<u64>,
     verkle_time: Option<u64>,
 
+    /// Amount of total difficulty reached by the network that triggers the consensus upgrade.
     terminal_total_difficulty: Option<U256>,
+    /// Network has already passed the terminal total difficult
     #[serde(default)]
     terminal_total_difficulty_passed: bool,
 }
