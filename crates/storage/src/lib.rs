025a778d90de08d98bdd53d0bcd2ce8ff833ccb0
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2024-06-24 15:53:41 -0300

    feat: add storage setup code

--- crates/storage/src/lib.rs
+++ crates/storage/src/lib.rs
@@ -1,14 +1,58 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use libmdbx::{
+    orm::{table, Database},
+    table_info,
+};
+use std::path::Path;
+
+// Define tables
+table!(
+    /// Example table.
+    /// TODO: remove when adding real tables.
+    ( Example ) String => String
+);
+
+/// Initializes a new database with the provided path. If the path is `None`, the database
+/// will be temporary.
+pub fn init_db(path: Option<impl AsRef<Path>>) -> Database {
+    let tables = [table_info!(Example)].into_iter().collect();
+    let path = path.map(|p| p.as_ref().to_path_buf());
+    Database::create(path, &tables).unwrap()
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use libmdbx::{
+        orm::{table, Database, DatabaseChart},
+        table_info,
+    };
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn mdbx_smoke_test() {
+        // Declare tables used for the smoke test
+        table!(
+            /// Example table.
+            ( Example ) String => String
+        );
+
+        // Assemble database chart
+        let tables: DatabaseChart = [table_info!(Example)].into_iter().collect();
+
+        let key = "Hello".to_string();
+        let value = "World!".to_string();
+
+        let db = Database::create(None, &tables).unwrap();
+
+        // Write values
+        {
+            let txn = db.begin_readwrite().unwrap();
+            txn.upsert::<Example>(key.clone(), value.clone()).unwrap();
+            txn.commit().unwrap();
+        }
+        // Read written values
+        let read_value = {
+            let txn = db.begin_read().unwrap();
+            txn.get::<Example>(key).unwrap()
+        };
+        assert_eq!(read_value, Some(value));
     }
 }
