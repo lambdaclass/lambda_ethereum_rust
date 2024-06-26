c904c05e68590a8c6e88117c757b111978bd5b32
Mateo Rico <89949621+ricomateo@users.noreply.github.com>
2024-06-26 16:19:44 -0300

    feat: add `BlockBodies` table to storage (#60)
    Closes #29
    
    ---------
    
    Co-authored-by: Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>

--- crates/storage/src/lib.rs
+++ crates/storage/src/lib.rs
@@ -1,6 +1,6 @@
 mod block;
 
-use block::BlockHeaderRLP;
+use block::{BlockBodyRLP, BlockHeaderRLP};
 use core::types::BlockNumber;
 use libmdbx::{
     orm::{table, Database},
@@ -13,11 +13,16 @@ table!(
     /// Block headers table.
     ( Headers ) BlockNumber => BlockHeaderRLP
 );
-
+table!(
+    /// Block bodies table.
+    ( Bodies ) BlockNumber => BlockBodyRLP
+);
 /// Initializes a new database with the provided path. If the path is `None`, the database
 /// will be temporary.
 pub fn init_db(path: Option<impl AsRef<Path>>) -> Database {
-    let tables = [table_info!(Headers)].into_iter().collect();
+    let tables = [table_info!(Headers), table_info!(Bodies)]
+        .into_iter()
+        .collect();
     let path = path.map(|p| p.as_ref().to_path_buf());
     Database::create(path, &tables).unwrap()
 }
