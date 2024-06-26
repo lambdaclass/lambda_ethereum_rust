af59cf4252510007920f2e7d44bed86e7790d06a
Federica <fedemoletta@hotmail.com>
2024-06-26 18:21:58 -0300

    Draft

--- crates/evm/src/lib.rs
+++ crates/evm/src/lib.rs
@@ -1,5 +1,17 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use bytes::Bytes;
+use revm::{primitives::{Address, ExecutionResult, Output, TxEnv, TxKind, U256}, Evm};
+
+fn run_evm() {
+    let caller = Address([0;20].into());
+    let to = Address([0;20].into());
+    let code = Bytes::new();
+    let mut evm = Evm::builder().modify_tx_env(|tx| {
+        tx.caller = caller;
+        tx.transact_to = TxKind::Call(to);
+        tx.data = code.into();
+        tx.value = U256::from(0);
+    }).build();
+    let tx_result = evm.transact().unwrap();
 }
 
 #[cfg(test)]
