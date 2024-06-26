95e2a9f8b819dd27d56c18f6a805ffc13ed1c0e0
Federica <fedemoletta@hotmail.com>
2024-06-26 12:21:20 -0300

    Push file

---
+++ crates/storage/src/account.rs
@@ -0,0 +1,33 @@
+use libmdbx::orm::{Decodable, Encodable};
+
+pub struct AccountInfoRLP(Vec<u8>);
+
+pub struct AddressRLP(Vec<u8>);
+
+impl Encodable for AccountInfoRLP {
+    type Encoded = Vec<u8>;
+
+    fn encode(self) -> Self::Encoded {
+        self.0
+    }
+}
+
+impl Decodable for AccountInfoRLP {
+    fn decode(b: &[u8]) -> anyhow::Result<Self> {
+        Ok(AccountInfoRLP(b.to_vec()))
+    }
+}
+
+impl Encodable for AddressRLP {
+    type Encoded = Vec<u8>;
+
+    fn encode(self) -> Self::Encoded {
+        self.0
+    }
+}
+
+impl Decodable for AddressRLP {
+    fn decode(b: &[u8]) -> anyhow::Result<Self> {
+        Ok(AddressRLP(b.to_vec()))
+    }
+}
