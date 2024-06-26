782896915784c5e302a9d06242d196b8f7a27d6f
Federica <fedemoletta@hotmail.com>
2024-06-26 14:23:18 -0300

    Add `AccountStorages` table to init_db`

--- crates/storage/src/lib.rs
+++ crates/storage/src/lib.rs
@@ -29,9 +29,13 @@ dupsort!(
 /// Initializes a new database with the provided path. If the path is `None`, the database
 /// will be temporary.
 pub fn init_db(path: Option<impl AsRef<Path>>) -> Database {
-    let tables = [table_info!(Headers), table_info!(AccountInfos)]
-        .into_iter()
-        .collect();
+    let tables = [
+        table_info!(Headers),
+        table_info!(AccountInfos),
+        table_info!(AccountStorages),
+    ]
+    .into_iter()
+    .collect();
     let path = path.map(|p| p.as_ref().to_path_buf());
     Database::create(path, &tables).unwrap()
 }
