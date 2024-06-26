d227fb5712082af36c5fcc512591e3147ea6fade
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2024-06-26 11:33:33 -0300

    feat: add `Headers` table to storage (#58)
    Closes #28

--- crates/core/src/lib.rs
+++ crates/core/src/lib.rs
@@ -1,5 +1,4 @@
 pub use ethereum_types::*;
-mod account;
-pub mod genesis;
-mod rlp;
-mod serde_utils;
+pub mod rlp;
+pub mod serde_utils;
+pub mod types;
---
+++ crates/storage/src/block.rs
@@ -0,0 +1,17 @@
+use libmdbx::orm::{Decodable, Encodable};
+
+pub struct BlockHeaderRLP(Vec<u8>);
+
+impl Encodable for BlockHeaderRLP {
+    type Encoded = Vec<u8>;
+
+    fn encode(self) -> Self::Encoded {
+        self.0
+    }
+}
+
+impl Decodable for BlockHeaderRLP {
+    fn decode(b: &[u8]) -> anyhow::Result<Self> {
+        Ok(BlockHeaderRLP(b.to_vec()))
+    }
+}
