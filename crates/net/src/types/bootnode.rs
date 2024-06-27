181317dc1899dc23c4a7a2572515d5186f354524
Mateo Rico <89949621+ricomateo@users.noreply.github.com>
2024-06-27 15:17:03 -0300

    feat: add support to passing bootnodes as argument through the cli (#67)
    **Motivation**
    
    According to the issue this will be used for P2P discovery
    
    **Description**
    
    This PR adds support to passing bootnodes as argument through the cli
    
    
    Closes #20

---
+++ crates/net/src/types/bootnode.rs
@@ -0,0 +1,47 @@
+use ethrex_core::H512;
+use std::{net::SocketAddr, num::ParseIntError, str::FromStr};
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct BootNode {
+    pub node_id: H512,
+    pub socket_address: SocketAddr,
+}
+
+impl FromStr for BootNode {
+    type Err = ParseIntError;
+    /// Takes a str with the format "enode://<node ID>@<IP address>:<port>" and
+    /// parses it to a BootNode
+    fn from_str(input: &str) -> Result<BootNode, ParseIntError> {
+        // TODO: error handling
+        let node_id = H512::from_str(&input[8..136]).expect("Failed to parse node id");
+        let socket_address: SocketAddr = input[137..]
+            .parse()
+            .expect("Failed to parse bootnode address and port");
+        Ok(BootNode {
+            node_id,
+            socket_address,
+        })
+    }
+}
+
+pub fn decode_hex(s: &str) -> Result<Vec<u8>, ParseIntError> {
+    (0..s.len())
+        .step_by(2)
+        .map(|i| u8::from_str_radix(&s[i..i + 2], 16))
+        .collect()
+}
+
+#[test]
+fn parse_bootnode_from_string() {
+    let input = "enode://d860a01f9722d78051619d1e2351aba3f43f943f6f00718d1b9baa4101932a1f5011f16bb2b1bb35db20d6fe28fa0bf09636d26a87d31de9ec6203eeedb1f666@18.138.108.67:30303";
+    let bootnode = BootNode::from_str(input).unwrap();
+    let node_id = H512::from_str(
+        "d860a01f9722d78051619d1e2351aba3f43f943f6f00718d1b9baa4101932a1f5011f16bb2b1bb35db20d6fe28fa0bf09636d26a87d31de9ec6203eeedb1f666")
+        .unwrap();
+    let socket_address = SocketAddr::from_str("18.138.108.67:30303").unwrap();
+    let expected_bootnode = BootNode {
+        node_id,
+        socket_address,
+    };
+    assert_eq!(bootnode, expected_bootnode);
+}
