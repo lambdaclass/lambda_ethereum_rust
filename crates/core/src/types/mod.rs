9561e6fab394d829bd5b4c9806c3c69dc6244b45
Federica <fedemoletta@hotmail.com>
2024-07-02 13:43:45 -0300

    Draft ExecutionPayloadV3

--- crates/core/src/types/mod.rs
+++ crates/core/src/types/mod.rs
@@ -1,11 +1,11 @@
 mod account;
 mod block;
 mod genesis;
-mod receipt;
 mod payload;
+mod receipt;
 
 pub use account::*;
 pub use block::*;
 pub use genesis::*;
-pub use receipt::*;
 pub use payload::*;
+pub use receipt::*;
--- crates/core/src/types/payload.rs
+++ crates/core/src/types/payload.rs
@@ -1,5 +1,38 @@
+use bytes::Bytes;
+use ethereum_types::{Address, Bloom};
 use keccak_hash::H256;
 use serde::{Deserialize, Serialize};
+
+#[allow(unused)]
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ExecutionPayloadV3 {
+    parent_hash: H256,
+    fee_recipient: Address,
+    state_root: H256,
+    receipts_root: H256,
+    logs_bloom: Bloom,
+    pev_randao: H256,
+    #[serde(deserialize_with = "crate::serde_utils::u64::deser_hex_str")]
+    block_number: u64,
+    #[serde(deserialize_with = "crate::serde_utils::u64::deser_hex_str")]
+    gas_limit: u64,
+    #[serde(deserialize_with = "crate::serde_utils::u64::deser_hex_str")]
+    gas_used: u64,
+    #[serde(deserialize_with = "crate::serde_utils::u64::deser_hex_str")]
+    timestamp: u64,
+    #[serde(deserialize_with = "crate::serde_utils::bytes::deser_hex_str")]
+    extra_data: Bytes,
+    #[serde(deserialize_with = "crate::serde_utils::u64::deser_hex_str")]
+    base_fee_per_gas: u64,
+    block_hash: H256,
+    transactions: (),
+    withdrawals: (),
+    #[serde(deserialize_with = "crate::serde_utils::u64::deser_hex_str")]
+    blob_gas_used: u64,
+    #[serde(deserialize_with = "crate::serde_utils::u64::deser_hex_str")]
+    excess_blob_gas: u64,
+}
 
 #[allow(unused)]
 #[derive(Debug, Deserialize, Serialize)]
@@ -7,7 +40,7 @@ use serde::{Deserialize, Serialize};
 pub struct PayloadStatus {
     status: PayloadValidationStatus,
     latest_valid_hash: H256,
-    validation_error: Option<String>
+    validation_error: Option<String>,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
