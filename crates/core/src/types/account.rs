51f8a9f34f6c4a4c61495a25ccdc805cb825b4e8
fmoletta <99273364+fmoletta@users.noreply.github.com>
2024-07-11 10:29:45 -0300

    feat: compute world state (#122)
    **Motivation**
    
    Obtain world state from account info stored in db
    
    **Description**
    
    * Implement storage root computation
    * Implement word state
    
    <!-- Link to issues: Resolves #111, Resolves #222 -->
    
    Closes #44

--- crates/core/src/types/account.rs
+++ crates/core/src/types/account.rs
@@ -2,8 +2,15 @@ use std::collections::HashMap;
 
 use bytes::Bytes;
 use ethereum_types::{H256, U256};
+use patricia_merkle_tree::PatriciaMerkleTree;
+use sha3::Keccak256;
 
-use crate::rlp::{encode::RLPEncode, structs::Encoder};
+use crate::rlp::{
+    decode::RLPDecode,
+    encode::RLPEncode,
+    error::RLPDecodeError,
+    structs::{Decoder, Encoder},
+};
 
 use super::GenesisAccount;
 
@@ -20,6 +27,14 @@ pub struct AccountInfo {
     pub code_hash: H256,
     pub balance: U256,
     pub nonce: u64,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct AccountState {
+    pub nonce: u64,
+    pub balance: U256,
+    pub storage_root: H256,
+    pub code_hash: H256,
 }
 
 impl From<GenesisAccount> for Account {
@@ -47,6 +62,80 @@ impl RLPEncode for AccountInfo {
             .encode_field(&self.balance)
             .encode_field(&self.nonce)
             .finish();
+    }
+}
+
+impl RLPDecode for AccountInfo {
+    fn decode_unfinished(rlp: &[u8]) -> Result<(AccountInfo, &[u8]), RLPDecodeError> {
+        let decoder = Decoder::new(rlp)?;
+        let (code_hash, decoder) = decoder.decode_field("code_hash")?;
+        let (balance, decoder) = decoder.decode_field("balance")?;
+        let (nonce, decoder) = decoder.decode_field("nonce")?;
+
+        let account_info = AccountInfo {
+            code_hash,
+            balance,
+            nonce,
+        };
+        Ok((account_info, decoder.finish()?))
+    }
+}
+
+impl RLPEncode for AccountState {
+    fn encode(&self, buf: &mut dyn bytes::BufMut) {
+        Encoder::new(buf)
+            .encode_field(&self.nonce)
+            .encode_field(&self.balance)
+            .encode_field(&self.storage_root)
+            .encode_field(&self.code_hash)
+            .finish();
+    }
+}
+
+impl RLPDecode for AccountState {
+    fn decode_unfinished(rlp: &[u8]) -> Result<(AccountState, &[u8]), RLPDecodeError> {
+        let decoder = Decoder::new(rlp)?;
+        let (nonce, decoder) = decoder.decode_field("nonce")?;
+        let (balance, decoder) = decoder.decode_field("balance")?;
+        let (storage_root, decoder) = decoder.decode_field("storage_root")?;
+        let (code_hash, decoder) = decoder.decode_field("code_hash")?;
+        let state = AccountState {
+            nonce,
+            balance,
+            storage_root,
+            code_hash,
+        };
+        Ok((state, decoder.finish()?))
+    }
+}
+
+pub fn compute_storage_root(storage: &HashMap<H256, H256>) -> H256 {
+    let rlp_storage = storage
+        .iter()
+        .map(|(k, v)| {
+            let mut k_buf = vec![];
+            let mut v_buf = vec![];
+            k.encode(&mut k_buf);
+            v.encode(&mut v_buf);
+            (k_buf, v_buf)
+        })
+        .collect::<Vec<_>>();
+    let root =
+        PatriciaMerkleTree::<_, _, Keccak256>::compute_hash_from_sorted_iter(rlp_storage.iter());
+    H256(root.into())
+}
+
+impl AccountState {
+    pub fn from_info_and_storage(
+        info: &AccountInfo,
+        storage: &HashMap<H256, H256>,
+    ) -> AccountState {
+        AccountState {
+            nonce: info.nonce,
+            balance: info.balance,
+            storage_root: compute_storage_root(storage),
+            code_hash: info.code_hash,
+        }
     }
 }
 
