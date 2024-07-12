9e485c5b4090292efc219d57779a92f75739ac7f
Federica <fedemoletta@hotmail.com>
2024-07-12 16:49:47 -0300

    Move test to main module + fix api

--- crates/storage/src/libmdbx.rs
+++ crates/storage/src/libmdbx.rs
@@ -179,22 +179,10 @@ pub fn init_db(path: Option<impl AsRef<Path>>) -> Database {
 
 #[cfg(test)]
 mod tests {
-    use std::str::FromStr;
-
-    use bytes::Bytes;
-    use ethereum_rust_core::{
-        rlp::decode::RLPDecode,
-        types::{BlockBody, BlockHeader, Bloom, Transaction},
-    };
-    use ethereum_types::{Address, H256, U256};
     use libmdbx::{
         orm::{table, Database, Decodable, Encodable},
         table_info,
     };
-
-    use crate::StoreEngine;
-
-    use super::init_db;
 
     #[test]
     fn mdbx_smoke_test() {
@@ -289,75 +277,5 @@ mod tests {
             txn.get::<StructsExample>(key).unwrap()
         };
         assert_eq!(read_value, Some(value));
-    }
-
-    #[test]
-    fn store_and_fetch_block() {
-        // Create block
-        let block_header = BlockHeader {
-            parent_hash: H256::from_str(
-                "0x1ac1bf1eef97dc6b03daba5af3b89881b7ae4bc1600dc434f450a9ec34d44999",
-            )
-            .unwrap(),
-            ommers_hash: H256::from_str(
-                "0x1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347",
-            )
-            .unwrap(),
-            coinbase: Address::from_str("0x2adc25665018aa1fe0e6bc666dac8fc2697ff9ba").unwrap(),
-            state_root: H256::from_str(
-                "0x9de6f95cb4ff4ef22a73705d6ba38c4b927c7bca9887ef5d24a734bb863218d9",
-            )
-            .unwrap(),
-            transactions_root: H256::from_str(
-                "0x578602b2b7e3a3291c3eefca3a08bc13c0d194f9845a39b6f3bcf843d9fed79d",
-            )
-            .unwrap(),
-            receipt_root: H256::from_str(
-                "0x035d56bac3f47246c5eed0e6642ca40dc262f9144b582f058bc23ded72aa72fa",
-            )
-            .unwrap(),
-            logs_bloom: Bloom::from([0; 256]),
-            difficulty: U256::zero(),
-            number: 1,
-            gas_limit: 0x016345785d8a0000,
-            gas_used: 0xa8de,
-            timestamp: 0x03e8,
-            extra_data: Bytes::new(),
-            prev_randao: H256::zero(),
-            nonce: 0x0000000000000000,
-            base_fee_per_gas: 0x07,
-            withdrawals_root: H256::from_str(
-                "0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421",
-            )
-            .unwrap(),
-            blob_gas_used: 0x00,
-            excess_blob_gas: 0x00,
-            parent_beacon_block_root: H256::zero(),
-        };
-        let block_body = BlockBody {
-            transactions: vec![Transaction::decode(&hex::decode("02f86c8330182480114e82f618946177843db3138ae69679a54b95cf345ed759450d870aa87bee53800080c080a0151ccc02146b9b11adf516e6787b59acae3e76544fdcd75e77e67c6b598ce65da064c5dd5aae2fbb535830ebbdad0234975cd7ece3562013b63ea18cc0df6c97d4").unwrap()).unwrap(),
-            Transaction::decode(&hex::decode("f86d80843baa0c4082f618946177843db3138ae69679a54b95cf345ed759450d870aa87bee538000808360306ba0151ccc02146b9b11adf516e6787b59acae3e76544fdcd75e77e67c6b598ce65da064c5dd5aae2fbb535830ebbdad0234975cd7ece3562013b63ea18cc0df6c97d4").unwrap()).unwrap()],
-            ommers: Default::default(),
-            withdrawals: Default::default(),
-        };
-
-        let block_number = 6;
-
-        let mut storage = super::Store {
-            db: init_db(None::<String>),
-        };
-
-        storage
-            .add_block_body(block_number, block_body.clone())
-            .unwrap();
-        storage
-            .add_block_header(block_number, block_header.clone())
-            .unwrap();
-
-        let fetched_body = storage.get_block_body(block_number).unwrap().unwrap();
-        let fetched_header = storage.get_block_header(block_number).unwrap().unwrap();
-
-        assert_eq!(block_body, fetched_body);
-        assert_eq!(block_header, fetched_header);
     }
 }
