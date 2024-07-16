6efad6a1d8a55b80ee8f7c71847666f4d997560c
Mateo Rico <89949621+ricomateo@users.noreply.github.com>
2024-07-16 10:21:13 -0300

    feat: parse cancun tests (#149)
    **Motivation**
    
    **Description**
    Allows to parse all tests in Cancun
    
    Closes #148

--- ef_tests/tests/cancun.rs
+++ ef_tests/tests/cancun.rs
@@ -2,13 +2,30 @@ use std::path::Path;
 
 mod common;
 
-fn eip4788_tests(path: &Path) -> datatest_stable::Result<()> {
+fn cancun_tests(path: &Path) -> datatest_stable::Result<()> {
     common::parse_and_execute_test_file(path);
     Ok(())
 }
 
+fn parse_test(path: &Path) -> datatest_stable::Result<()> {
+    common::parse_test_file(path);
+    Ok(())
+}
+
 datatest_stable::harness!(
-    eip4788_tests,
-    "vectors/cancun/eip4788_beacon_root",
-    r"^.*beacon_root_contract_calls.json"
+    cancun_tests,
+    "vectors/cancun/eip4844_blobs/",
+    r"^.*/valid_precompile_calls.json",
+    cancun_tests,
+    "vectors/cancun/eip4788_beacon_root/",
+    r"^.*beacon_root_contract_calls.json",
+    cancun_tests,
+    "vectors/cancun/eip5656_mcopy/",
+    r"^.*mcopy_huge_memory_expansion.json",
+    cancun_tests,
+    "vectors/cancun/eip4788_beacon_root/",
+    r"^.*invalid_beacon_root_calldata_value.json",
+    parse_test,
+    "vectors/cancun/",
+    r"^.*.json",
 );
