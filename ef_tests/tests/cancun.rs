0c36bfde3c3e974115589762236c6c34342e4e69
ricomateo <mrico@fi.uba.ar>
2024-07-16 09:39:22 -0300

    change test harness so that it only executes the working tests and parses the rest

--- ef_tests/tests/cancun.rs
+++ ef_tests/tests/cancun.rs
@@ -3,8 +3,29 @@ use std::path::Path;
 mod common;
 
 fn cancun_tests(path: &Path) -> datatest_stable::Result<()> {
+    common::parse_and_execute_test_file(path);
+    Ok(())
+}
+
+fn parse_test(path: &Path) -> datatest_stable::Result<()> {
     common::parse_test_file(path);
     Ok(())
 }
 
-datatest_stable::harness!(cancun_tests, "vectors/cancun/", r"^.*.json");
+datatest_stable::harness!(
+    cancun_tests,
+    "vectors/cancun/",
+    r"^.*/point_evaluation_precompile/valid_precompile_calls.json",
+    cancun_tests,
+    "vectors/cancun/",
+    r"^.*beacon_root_contract_calls.json",
+    cancun_tests,
+    "vectors/cancun/",
+    r"^.*mcopy_huge_memory_expansion.json",
+    cancun_tests,
+    "vectors/cancun/",
+    r"^.*invalid_beacon_root_calldata_value.json",
+    parse_test,
+    "vectors/cancun/",
+    r"^.*.json",
+);
