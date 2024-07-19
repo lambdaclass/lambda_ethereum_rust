7c49d0c3a98f5dce67c2aaaf802044bece88a795
Federica <fedemoletta@hotmail.com>
2024-07-19 11:04:21 -0300

    Add prefixes to distinguish between core and revm types

--- crates/evm/database.rs
+++ crates/evm/database.rs
@@ -1,6 +1,9 @@
-use ethereum_rust_core::Address;
+use ethereum_rust_core::{Address as CoreAddress, H256 as CoreH256};
 use ethereum_rust_storage::{error::StoreError, Store};
-use revm::primitives::{AccountInfo, Address as RevmAddress, Bytecode, Bytes, B256, U256};
+use revm::primitives::{
+    AccountInfo as RevmAccountInfo, Address as RevmAddress, Bytecode as RevmBytecode,
+    Bytes as RevmBytes, B256 as RevmB256, U256 as RevmU256,
+};
 
 pub struct StoreWrapper(pub Store);
 
@@ -9,36 +12,42 @@ impl revm::Database for StoreWrapper {
     type Error = StoreError;
 
     #[doc = " Get basic account information."]
-    fn basic(&mut self, address: RevmAddress) -> Result<Option<AccountInfo>, Self::Error> {
-        let acc_info = match self.0.get_account_info(Address::from(address.0.as_ref()))? {
+    fn basic(&mut self, address: RevmAddress) -> Result<Option<RevmAccountInfo>, Self::Error> {
+        let acc_info = match self
+            .0
+            .get_account_info(CoreAddress::from(address.0.as_ref()))?
+        {
             None => return Ok(None),
             Some(acc_info) => acc_info,
         };
         let code = self
             .0
             .get_account_code(acc_info.code_hash)?
-            .map(|b| Bytecode::new_raw(Bytes(b)));
+            .map(|b| RevmBytecode::new_raw(RevmBytes(b)));
 
-        Ok(Some(AccountInfo {
-            balance: U256::from_limbs(acc_info.balance.0),
+        Ok(Some(RevmAccountInfo {
+            balance: RevmU256::from_limbs(acc_info.balance.0),
             nonce: acc_info.nonce,
-            code_hash: B256::from(acc_info.code_hash.0),
+            code_hash: RevmB256::from(acc_info.code_hash.0),
             code,
         }))
     }
 
     #[doc = " Get account code by its hash."]
-    fn code_by_hash(&mut self, code_hash: B256) -> Result<Bytecode, Self::Error> {
-        todo!()
+    fn code_by_hash(&mut self, code_hash: RevmB256) -> Result<RevmBytecode, Self::Error> {
+        self.0
+            .get_account_code(CoreH256::from(code_hash.as_ref()))?
+            .map(|b| RevmBytecode::new_raw(RevmBytes(b)))
+            .ok_or_else(|| StoreError::Custom(format!("No code for hash {code_hash}")))
     }
 
     #[doc = " Get storage value of address at index."]
-    fn storage(&mut self, address: RevmAddress, index: U256) -> Result<U256, Self::Error> {
+    fn storage(&mut self, address: RevmAddress, index: RevmU256) -> Result<RevmU256, Self::Error> {
         todo!()
     }
 
     #[doc = " Get block hash by block number."]
-    fn block_hash(&mut self, number: U256) -> Result<B256, Self::Error> {
+    fn block_hash(&mut self, number: RevmU256) -> Result<RevmB256, Self::Error> {
         todo!()
     }
 }
