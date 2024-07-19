1302675dea70eabcd772ec9f25f509ffc1b7808c
fmoletta <99273364+fmoletta@users.noreply.github.com>
2024-07-19 18:20:42 -0300

    feat: add rpc endpoint `eth_getTransactionByHash` (#181)
    **Motivation**
    
    Support  rpc endpoint `eth_getTransactionByHash`
    
    <!-- Why does this pull request exist? What are its goals? -->
    
    **Description**
    
    * Add rpc endpoint `eth_getTransactionByHash`
    * Add method `get_transaction_by_hash` to store api
    
    <!-- A clear and concise general description of the changes this PR
    introduces -->
    
    <!-- Link to issues: Resolves #111, Resolves #222 -->
    
    Closes #173

--- crates/rpc/rpc.rs
+++ crates/rpc/rpc.rs
@@ -7,7 +7,7 @@ use eth::{
     block::{
         self, GetBlockByHashRequest, GetBlockByNumberRequest, GetBlockReceiptsRequest,
         GetBlockTransactionCountByNumberRequest, GetTransactionByBlockHashAndIndexRequest,
-        GetTransactionByBlockNumberAndIndexRequest,
+        GetTransactionByBlockNumberAndIndexRequest, GetTransactionByHashRequest,
     },
     client,
 };
@@ -123,6 +123,11 @@ pub fn map_requests(req: &RpcRequest, storage: Store) -> Result<Value, RpcErr> {
         "eth_getBlockReceipts" => {
             let request = GetBlockReceiptsRequest::parse(&req.params).ok_or(RpcErr::BadParams)?;
             block::get_block_receipts(&request, storage)
+        }
+        "eth_getTransactionByHash" => {
+            let request =
+                GetTransactionByHashRequest::parse(&req.params).ok_or(RpcErr::BadParams)?;
+            block::get_transaction_by_hash(&request, storage)
         }
         "engine_forkchoiceUpdatedV3" => engine::forkchoice_updated_v3(),
         "engine_newPayloadV3" => {
