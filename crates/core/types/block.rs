66813073bbf0623201fef9f9ca1956fc42217ae3
Federica <fedemoletta@hotmail.com>
2024-07-24 12:21:01 -0300

    Move empty keccak const to types/constants + derive Debug on structs

--- crates/core/types/block.rs
+++ crates/core/types/block.rs
@@ -27,14 +27,10 @@ pub use serializable::BlockSerializable;
 pub type BlockNumber = u64;
 pub type BlockHash = H256;
 
-use lazy_static::lazy_static;
-
-lazy_static! {
-    pub static ref DEFAULT_OMMERS_HASH: H256 = H256::from_slice(&hex::decode("1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347").unwrap()); // = Keccak256(RLP([])) as of EIP-3675
-}
+pub use super::EMPTY_KECCACK_HASH as DEFAULT_OMMERS_HASH;
 
 /// Header part of a block on the chain.
-#[derive(Clone, Debug, PartialEq, Eq, Serialize)]
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Default)]
 #[serde(rename_all = "camelCase")]
 pub struct BlockHeader {
     pub parent_hash: H256,
