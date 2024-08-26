3a4a023806e3364c9adf970c04254200f46fb4a9
avilagaston9 <gavila@fi.uba.ar>
2024-08-26 20:34:19 -0300

    fix: get_block_by_hash endpoint

---
+++ crates/rpc/engine/fork_choice.rs
@@ -0,0 +1,54 @@
+use ethereum_rust_storage::Store;
+use serde_json::{json, Value};
+
+use crate::{
+    types::fork_choice::{ForkChoiceState, PayloadAttributesV3},
+    RpcErr,
+};
+
+#[derive(Debug)]
+pub struct ForkChoiceUpdatedV3 {
+    pub fork_choice_state: ForkChoiceState,
+    #[allow(unused)]
+    pub payload_attributes: Option<PayloadAttributesV3>,
+}
+
+impl ForkChoiceUpdatedV3 {
+    pub fn parse(params: &Option<Vec<Value>>) -> Option<ForkChoiceUpdatedV3> {
+        let params = params.as_ref()?;
+        if params.len() != 2 {
+            return None;
+        }
+        Some(ForkChoiceUpdatedV3 {
+            fork_choice_state: serde_json::from_value(params[0].clone()).ok()?,
+            payload_attributes: serde_json::from_value(params[1].clone()).ok()?,
+        })
+    }
+}
+
+pub fn forkchoice_updated_v3(
+    request: ForkChoiceUpdatedV3,
+    storage: Store,
+) -> Result<Value, RpcErr> {
+    // Just a minimal implementation to pass rpc-compat Hive tests.
+    // TODO (#50): Implement `engine_forkchoiceUpdatedV3`
+    let safe = storage.get_block_number(request.fork_choice_state.safe_block_hash);
+    let finalized = storage.get_block_number(request.fork_choice_state.finalized_block_hash);
+
+    // Check if we already have the blocks stored.
+    let (safe_block_number, finalized_block_number) = match (safe, finalized) {
+        (Ok(Some(safe)), Ok(Some(finalized))) => (safe, finalized),
+        _ => return Err(RpcErr::Internal),
+    };
+
+    storage.update_finalized_block_number(finalized_block_number)?;
+    storage.update_safe_block_number(safe_block_number)?;
+    Ok(json!({
+        "payloadId": null,
+        "payloadStatus": {
+            "latestValidHash": null,
+            "status": "SYNCING",
+            "validationError": null
+        }
+    }))
+}
--- crates/rpc/rpc.rs
+++ crates/rpc/rpc.rs
@@ -7,7 +7,11 @@ use axum_extra::{
     headers::{authorization::Bearer, Authorization},
     TypedHeader,
 };
-use engine::{ExchangeCapabilitiesRequest, NewPayloadV3Request};
+use engine::{
+    fork_choice::{self, ForkChoiceUpdatedV3},
+    payload::{self, NewPayloadV3Request},
+    ExchangeCapabilitiesRequest,
+};
 use eth::{
     account::{self, GetBalanceRequest, GetCodeRequest, GetStorageAtRequest},
     block::{
@@ -208,10 +212,16 @@ pub fn map_requests(req: &RpcRequest, storage: Store) -> Result<Value, RpcErr> {
             let request = CallRequest::parse(&req.params).ok_or(RpcErr::BadParams)?;
             transaction::call(&request, storage)
         }
-        "engine_forkchoiceUpdatedV3" => engine::forkchoice_updated_v3(),
+        "engine_forkchoiceUpdatedV3" => {
+            let request = ForkChoiceUpdatedV3::parse(&req.params).ok_or(RpcErr::BadParams)?;
+            Ok(
+                serde_json::to_value(fork_choice::forkchoice_updated_v3(request, storage)?)
+                    .unwrap(),
+            )
+        }
         "engine_newPayloadV3" => {
             let request = NewPayloadV3Request::parse(&req.params).ok_or(RpcErr::BadParams)?;
-            Ok(serde_json::to_value(engine::new_payload_v3(request, storage)?).unwrap())
+            Ok(serde_json::to_value(payload::new_payload_v3(request, storage)?).unwrap())
         }
         "admin_nodeInfo" => admin::node_info(),
         _ => Err(RpcErr::MethodNotFound),
