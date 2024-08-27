eb0bc604f7099d1d1764c8be8215114f36ea114a
fmoletta <fedemoletta@hotmail.com>
2024-08-27 16:20:21 -0300

    Copy nibble impl from repo + impl rlp enco/decoding for NibbleVec

--- crates/storage/trie.rs
+++ crates/storage/trie.rs
@@ -1,4 +1,5 @@
 mod db;
+mod nibble;
 mod node;
 mod rlp;
 mod trie;
--- crates/storage/trie/node.rs
+++ crates/storage/trie/node.rs
@@ -1,6 +1,11 @@
 use ethereum_types::H256;
 
-use super::db::PathRLP;
+use crate::error::StoreError;
+
+use super::{
+    db::{PathRLP, TrieDB, ValueRLP},
+    nibble::NibbleVec,
+};
 pub type NodeHash = H256;
 
 pub enum Node {
@@ -16,11 +21,50 @@ pub struct BranchNode {
 
 pub struct ExtensionNode {
     pub hash: NodeHash,
-    pub prefix: bool, // Nibble vec
+    pub prefix: NibbleVec,
     pub child: NodeHash,
 }
 
+#[derive(Debug, Clone)]
 pub struct LeafNode {
     pub hash: NodeHash,
     pub path: PathRLP,
+}
+
+pub enum InsertAction {
+    Replace(PathRLP),
+}
+
+impl LeafNode {
+    pub fn new(path: PathRLP) -> Self {
+        Self {
+            hash: Default::default(),
+            path,
+        }
+    }
+
+    pub fn update_path(&mut self, new_path: PathRLP) {
+        self.path = new_path
+    }
+
+    pub fn get(&self, db: &TrieDB, path: PathRLP) -> Result<Option<ValueRLP>, StoreError> {
+        if path == self.path {
+            db.get_value(path)
+        } else {
+            Ok(None)
+        }
+    }
+
+    pub fn insert(&mut self, db: &TrieDB, path: PathRLP) -> (Node, InsertAction) {
+        // Mark hash as dirty
+        self.hash = Default::default();
+        if path == self.path {
+            return (
+                Node::Leaf(self.clone()),
+                InsertAction::Replace(self.path.clone()),
+            );
+        } else {
+        }
+        todo!()
+    }
 }
