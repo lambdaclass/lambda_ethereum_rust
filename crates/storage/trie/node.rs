377a23d089d3db8125149f02e04d2834281873d1
fmoletta <fedemoletta@hotmail.com>
2024-08-29 17:34:26 -0300

    Revert "[DEBUG]: print node insertion info"
    This reverts commit 5b9eb97dd7bb24a86278cb78d0669e32f3282ba0.

--- crates/storage/trie/node.rs
+++ crates/storage/trie/node.rs
@@ -103,22 +103,3 @@ impl Node {
         }
     }
 }
-
-impl Node {
-    pub fn info(&self) -> String {
-        match self {
-            Node::Branch(n) => {
-                let choices = n
-                    .choices
-                    .iter()
-                    .filter(|nr| nr.is_valid())
-                    .collect::<Vec<_>>();
-                format!("Node::Branch(choices: {choices:?}, path: {:?}", n.path)
-            }
-            Node::Extension(n) => {
-                format!("Node::Extension(child: {}, prefix {:?}", *n.child, n.prefix)
-            }
-            Node::Leaf(n) => format!("Node::Leaf(path: {:?}", n.path),
-        }
-    }
-}
