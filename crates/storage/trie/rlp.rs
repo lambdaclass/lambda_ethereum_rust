9057a9de03ea3e15e07064a32eee6dcde28ecb1a
fmoletta <fedemoletta@hotmail.com>
2024-09-09 10:56:01 -0300

    Add comment to clear confusion

--- crates/storage/trie/rlp.rs
+++ crates/storage/trie/rlp.rs
@@ -1,4 +1,5 @@
 // Contains RLP encoding and decoding implementations for Trie Nodes
+// This encoding is only used to store the nodes in the DB, it is not the encoding used for hash computation
 use ethereum_rust_core::rlp::{
     decode::RLPDecode,
     encode::RLPEncode,
