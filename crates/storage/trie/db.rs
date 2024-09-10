c7a11a51a1aad31c2e0cfb7936338ccb913b2897
fmoletta <fedemoletta@hotmail.com>
2024-09-10 16:59:15 -0300

    Remove RootNodes table

--- crates/storage/trie/db.rs
+++ crates/storage/trie/db.rs
@@ -7,10 +7,8 @@ use libmdbx::{
 };
 
 /// Libmbdx database representing trie state
-/// It contains two tables, one mapping node references to rlp encoded nodes, and another one mapping node hashes to node references
+/// It contains a table mapping node hashes to rlp encoded nodes
 /// All nodes are stored in the DB and no node is ever removed
-/// Only node hahses for root nodes computed with `Trie::compute_hash` are stored
-/// Contains the next node reference to be used when storing a new node
 use super::{dumb_hash::DumbNodeHash, node::Node, node_ref::NodeRef};
 pub struct TrieDB {
     db: Database,
@@ -26,12 +24,6 @@ table!(
     ( Nodes ) DumbNodeHash => NodeRLP
 );
 
-table!(
-    /// NodeHash to NodeRef table
-    /// Stores root nodes which's hashes have been computed by `compute_hash`
-    ( RootNodes ) NodeHashRLP => NodeRef
-);
-
 impl TrieDB {
     /// Opens a DB created by a previous execution or creates a new one if it doesn't exist
     pub fn init(trie_dir: &str) -> Result<TrieDB, StoreError> {
@@ -40,9 +32,7 @@ impl TrieDB {
 
     /// Creates a new clean DB
     pub fn create(trie_dir: &str) -> Result<TrieDB, StoreError> {
-        let tables = [table_info!(Nodes), table_info!(RootNodes)]
-            .into_iter()
-            .collect();
+        let tables = [table_info!(Nodes)].into_iter().collect();
         let path = Some(trie_dir.into());
         Ok(TrieDB {
             db: Database::create(path, &tables).map_err(StoreError::LibmdbxError)?,
@@ -53,9 +43,7 @@ impl TrieDB {
     /// Also returns root node reference if available
     pub fn open(trie_dir: &str) -> Result<TrieDB, StoreError> {
         // Open DB
-        let tables = [table_info!(Nodes), table_info!(RootNodes)]
-            .into_iter()
-            .collect();
+        let tables = [table_info!(Nodes)].into_iter().collect();
         let db = Database::open(trie_dir, &tables).map_err(StoreError::LibmdbxError)?;
         Ok(TrieDB { db })
     }
@@ -70,17 +58,6 @@ impl TrieDB {
     /// Inserts a node and returns its reference
     pub fn insert_node(&mut self, node: Node, hash: DumbNodeHash) -> Result<(), StoreError> {
         self.write::<Nodes>(hash, node.encode_to_vec())
-    }
-
-    /// Retrieves a node reference based on the hash of the referenced node
-    /// Will only work if the hash was computed using `Trie::compute_hash`
-    pub fn get_root_ref(&self, hash: H256) -> Result<Option<NodeRef>, StoreError> {
-        self.read::<RootNodes>(hash.0)
-    }
-
-    /// Inserts a node reference by its node's hash
-    pub fn insert_root_ref(&mut self, hash: H256, node_ref: NodeRef) -> Result<(), StoreError> {
-        self.write::<RootNodes>(hash.0, node_ref)
     }
 
     /// Helper method to write into a libmdbx table
@@ -107,9 +84,7 @@ impl TrieDB {
     #[cfg(test)]
     /// Creates a temporary DB, for testing purposes only
     pub fn init_temp() -> Self {
-        let tables = [table_info!(Nodes), table_info!(RootNodes)]
-            .into_iter()
-            .collect();
+        let tables = [table_info!(Nodes)].into_iter().collect();
         TrieDB {
             db: Database::create(None, &tables).expect("Failed to create temp DB"),
         }
