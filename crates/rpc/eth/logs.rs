44278c84e47e1335f8bbf1f451b99e9c833b1852
fkrause98 <fkrausear@gmail.com>
2024-09-12 19:31:59 -0300

    logs: set nones as parameters

--- crates/rpc/eth/logs.rs
+++ crates/rpc/eth/logs.rs
@@ -1,16 +1,18 @@
 use crate::{types::block_identifier::BlockIdentifier, RpcErr, RpcHandler};
 use ethereum_rust_core::{H160, U256};
 use ethereum_rust_storage::Store;
+use serde::Deserialize;
 use serde_json::{from_value, Value};
 
+#[derive(Debug)]
 pub struct LogsRequest {
     /// The oldest block from which to start
     /// retrieving logs.
     /// Will default to `latest` if not provided.
-    pub from: BlockIdentifier,
+    pub fromBlock: BlockIdentifier,
     /// Up to which block to stop retrieving logs.
     /// Will default to `latest` if not provided.
-    pub to: BlockIdentifier,
+    pub toBlock: BlockIdentifier,
     /// The addresses from where the logs origin from.
     pub address: Option<Vec<H160>>,
     /// Which topics to filter.
@@ -19,20 +21,40 @@ pub struct LogsRequest {
 impl RpcHandler for LogsRequest {
     fn parse(params: &Option<Vec<Value>>) -> Result<LogsRequest, RpcErr> {
         match params.as_deref() {
-            Some([from, to, address, topics]) => Ok(LogsRequest {
-                from: BlockIdentifier::parse(from.clone(), 0).unwrap(),
-                to: BlockIdentifier::parse(to.clone(), 1).unwrap(),
-                address: from_value(address.clone()).ok(),
-                topics: from_value(topics.clone()).ok(),
-            }),
+            Some([param]) => {
+                let param = param.as_object().ok_or(RpcErr::BadParams)?;
+                let fromBlock = {
+                    if let Some(param) = param.get("fromBlock") {
+                        BlockIdentifier::parse(param.clone(), 0)?
+                    } else {
+                        BlockIdentifier::latest()
+                    }
+                };
+                let toBlock = {
+                    if let Some(param) = param.get("toBlock") {
+                        BlockIdentifier::parse(param.clone(), 1)?
+                    } else {
+                        BlockIdentifier::latest()
+                    }
+                };
+                let address = None;
+                let topics = None;
+                Ok(LogsRequest {
+                    fromBlock,
+                    address,
+                    topics,
+                    toBlock,
+                })
+            }
+            Some(params) => unreachable!("{params:?}"),
             _ => Err(RpcErr::BadParams),
         }
     }
     fn handle(&self, storage: Store) -> Result<Value, RpcErr> {
-        let Ok(Some(from)) = self.from.resolve_block_number(&storage) else {
+        let Ok(Some(from)) = dbg!(self.fromBlock.resolve_block_number(&storage)) else {
             return Err(RpcErr::BadParams);
         };
-        let Ok(Some(to)) = self.to.resolve_block_number(&storage) else {
+        let Ok(Some(to)) = self.toBlock.resolve_block_number(&storage) else {
             return Err(RpcErr::BadParams);
         };
         let logs = storage
