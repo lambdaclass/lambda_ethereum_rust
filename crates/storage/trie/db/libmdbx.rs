5a6f75422af20ce5fd92e5b8af32ca8294fb287e
Tomás Arjovsky <t.arjovsky@gmail.com>
2024-09-13 18:06:46 +0200

    wip with different tables

--- crates/storage/trie/db/libmdbx.rs
+++ crates/storage/trie/db/libmdbx.rs
@@ -1,70 +1,82 @@
+use std::marker::PhantomData;
+
 use crate::error::StoreError;
 use libmdbx::{
-    orm::{table, Database},
+    orm::{table, Database, Table},
     table_info,
 };
 
+use crate::trie::db::{NodeHashRLP, NodeRLP};
+
 /// Libmdbx implementation for the TrieDB trait, with get and put operations.
-pub struct Libmdbx(Database);
+pub struct Libmdbx<'a, T: Table> {
+    db: &'a Database,
+    phantom: PhantomData<T>,
+}
 
 use super::TrieDB;
 
 table!(
     /// NodeHash to Node table
-    ( Nodes ) Vec<u8> => Vec<u8>
+    ( Nodes )  NodeHashRLP => NodeRLP
 );
 
-impl Libmdbx {
-    /// Opens a DB created by a previous execution or creates a new one if it doesn't exist
-
-    pub fn init(trie_dir: &str) -> Result<Self, StoreError> {
-        Self::open(trie_dir).or_else(|_| Self::create(trie_dir))
+impl<'a, T: Table> TrieDB for Libmdbx<'a, T> {
+    fn get(&self, key: NodeHashRLP) -> Result<Option<NodeRLP>, StoreError> {
+        let txn = self.db.begin_read().map_err(StoreError::LibmdbxError)?;
+        txn.get::<T>(key).map_err(StoreError::LibmdbxError)
     }
 
-    /// Creates a new clean DB
-    pub fn create(trie_dir: &str) -> Result<Self, StoreError> {
-        let tables = [table_info!(Nodes)].into_iter().collect();
-        let path = Some(trie_dir.into());
-        Ok(Self(
-            Database::create(path, &tables).map_err(StoreError::LibmdbxError)?,
-        ))
-    }
-
-    /// Opens a DB created by a previous execution
-    pub fn open(trie_dir: &str) -> Result<Self, StoreError> {
-        // Open DB
-        let tables = [table_info!(Nodes)].into_iter().collect();
-        let db = Database::open(trie_dir, &tables).map_err(StoreError::LibmdbxError)?;
-        Ok(Self(db))
-    }
-
-    #[cfg(test)]
-    /// Creates a temporary DB, for testing purposes only
-    pub fn init_temp() -> Self {
-        use tempdir::TempDir;
-        let tables = [table_info!(Nodes)].into_iter().collect();
-        Self(Database::create(None, &tables).expect("Failed to create temp DB"))
-    }
-}
-
-impl TrieDB for Libmdbx {
-    fn get(&self, key: Vec<u8>) -> Result<Option<Vec<u8>>, StoreError> {
-        let txn = self.0.begin_read().map_err(StoreError::LibmdbxError)?;
-        txn.get::<Nodes>(key).map_err(StoreError::LibmdbxError)
-    }
-
-    fn put(&self, key: Vec<u8>, value: Vec<u8>) -> Result<(), StoreError> {
-        let txn = self.0.begin_readwrite().map_err(StoreError::LibmdbxError)?;
-        txn.upsert::<Nodes>(key, value)
+    fn put(&self, key: NodeHashRLP, value: NodeRLP) -> Result<(), StoreError> {
+        let txn = self
+            .db
+            .begin_readwrite()
+            .map_err(StoreError::LibmdbxError)?;
+        txn.upsert::<T>(key, value)
             .map_err(StoreError::LibmdbxError)?;
         txn.commit().map_err(StoreError::LibmdbxError)
     }
 }
 
+#[cfg(test)]
+fn new_db() -> Database {
+    let tables = [table_info!(Nodes)].into_iter().collect();
+    Database::create(None, &tables).expect("Failed to create temp DB")
+}
+
 #[test]
 fn simple_addition() {
-    let db = Libmdbx::init_temp();
+    let inner_db = new_db();
+    let db = Libmdbx::<Nodes> { db: inner_db };
     assert_eq!(db.get("hello".into()).unwrap(), None);
     db.put("hello".into(), "value".into());
     assert_eq!(db.get("hello".into()).unwrap(), Some("value".into()));
+}
+
+#[test]
+fn different_tables() {
+    table!(
+        /// vec to vec
+        ( TableA ) Vec<u8> => Vec<u8>
+    );
+    table!(
+        /// vec to vec
+        ( TableB ) Vec<u8> => Vec<u8>
+    );
+    let inner_db = new_db();
+    let tables = [table_info!(TableA), table_info!(TableB)]
+        .into_iter()
+        .collect();
+
+    let db = Database::create(None, &tables).unwrap();
+    let trie_dba = Libmdbx::<TableA> {
+        db: &db,
+        phantom: PhantomData,
+    };
+    let trie_dbb = Libmdbx::<TableB> {
+        db: &db,
+        phantom: PhantomData,
+    };
+    trie_dba.put("hello".into(), "value".into());
+    assert_eq!(trie_dbb.get("hello".into()).unwrap(), Some("value".into()));
 }
