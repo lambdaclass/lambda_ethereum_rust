5fd2bc80a374086c154ceaf56ca71b8e94f436a2
fkrause98 <fkrausear@gmail.com>
2024-09-14 14:58:26 -0300

    logs: fix tests, tide up a bit

--- crates/rpc/eth/logs.rs
+++ crates/rpc/eth/logs.rs
@@ -65,43 +65,76 @@ impl RpcHandler for LogsRequest {
             return Err(RpcErr::BadParams);
         };
         let receipts = storage.get_receipts_in_range(from, to)?;
-        let tx_and_block_data = {
-            let mut data: Vec<(H256, Index, BlockNumber)> = vec![];
-            for receipt in receipts.clone() {
-                let tx_hash = receipt.tx_hash.clone();
-                let (block_num, tx_indx) =
-                    storage.get_transaction_location(tx_hash).unwrap().unwrap();
-                data.push((tx_hash, tx_indx, block_num))
-            }
-            data
-        };
-        let block_hashes: Vec<H256> = {
-            let mut hashes: Vec<H256> = vec![];
-            for (_, _, block_num) in tx_and_block_data.clone().into_iter() {
-                let block_hash = storage
-                    .get_block_header(block_num)
-                    .unwrap()
-                    .unwrap()
-                    .compute_block_hash();
-                hashes.push(block_hash)
-            }
-            hashes
-        };
-        let mut logs: Vec<RpcLog> = vec![];
+        // Process transaction and block data for eth_getLogs response
+        use std::collections::HashMap;
+
+        // Process transaction and block data for eth_getLogs response
+        let tx_and_block_data: Vec<(H256, Index, BlockNumber)> = receipts
+            .iter()
+            .filter_map(|receipt| {
+                let tx_hash = receipt.tx_hash;
+                storage
+                    .get_transaction_location(tx_hash)
+                    .ok()
+                    .flatten()
+                    .map(|(block_num, tx_indx)| (tx_hash, tx_indx, block_num))
+            })
+            .collect();
+
+        // Retrieve block hashes for each transaction
+        let block_hashes: Vec<H256> = tx_and_block_data
+            .iter()
+            .filter_map(|(_, _, block_num)| {
+                storage
+                    .get_block_header(*block_num)
+                    .ok()
+                    .flatten()
+                    .map(|header| header.compute_block_hash())
+            })
+            .collect();
+
+        // Group logs by block number
+        let mut logs_by_block: HashMap<BlockNumber, Vec<RpcLog>> = HashMap::new();
+
         for (r_indx, receipt) in receipts.iter().enumerate() {
-            for (log_indx, log) in receipt.logs.iter().enumerate() {
-                let log = RpcLog {
+            let tx_data = &tx_and_block_data[r_indx];
+            let block_hash = block_hashes[r_indx];
+
+            for (_, log) in receipt.logs.iter().enumerate() {
+                let rpc_log = RpcLog {
                     log: log.clone().into(),
-                    log_index: log_indx as u64,
-                    transaction_hash: tx_and_block_data[r_indx].0,
-                    transaction_index: tx_and_block_data[r_indx].1,
-                    block_number: tx_and_block_data[r_indx].2,
-                    block_hash: block_hashes[r_indx],
+                    // Ignore this value for now, we'll change it below...
+                    log_index: 0,
+                    transaction_hash: tx_data.0,
+                    transaction_index: tx_data.1,
+                    block_number: tx_data.2,
+                    block_hash,
                     removed: false,
                 };
-                logs.push(log)
+                logs_by_block
+                    .entry(tx_data.2)
+                    .or_insert_with(Vec::new)
+                    .push(rpc_log);
             }
         }
+
+        let mut logs: Vec<RpcLog> = Vec::new();
+        for (_, block_logs) in logs_by_block.iter_mut() {
+            for (index, log) in block_logs.iter_mut().enumerate() {
+                // Set properly the index
+                log.log_index = index as u64;
+            }
+            logs.extend(block_logs.drain(..));
+        }
+
+        // Sort logs by block number, then by log index
+        logs.sort_by(|a, b| {
+            a.block_number
+                .cmp(&b.block_number)
+                .then(a.log_index.cmp(&b.log_index))
+        });
+
+        // Serialize the logs to JSON, returning an error if serialization fails
         serde_json::to_value(logs).map_err(|_| RpcErr::Internal)
     }
 }
