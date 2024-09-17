12c2680918b355c968d2f4ceb8f4f0abdcdb0f4b
Esteban Dimitroff Hodi <esteban.dimitroff@lambdaclass.com>
2024-09-17 17:41:17 -0300

    Moved frame logic into it's own module

--- crates/net/rlpx/connection.rs
+++ crates/net/rlpx/connection.rs
@@ -1,23 +1,15 @@
-use aes::{
-    cipher::{BlockEncrypt, KeyInit, KeyIvInit, StreamCipher},
-    Aes256Enc,
-};
-use ethereum_rust_core::{
-    rlp::{decode::RLPDecode, encode::RLPEncode},
-    H128, H256,
-};
+use super::{frame, message as rlpx};
+use aes::cipher::KeyIvInit;
+use ethereum_rust_core::{rlp::decode::RLPDecode, H256};
 use sha3::{Digest, Keccak256};
-use tokio::io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt};
-
-use super::message as rlpx;
-
+use tokio::io::{AsyncRead, AsyncWrite};
 pub const SUPPORTED_CAPABILITIES: [(&str, u8); 1] = [("p2p", 5)];
 // pub const SUPPORTED_CAPABILITIES: [(&str, u8); 3] = [("p2p", 5), ("eth", 68), ("snap", 1)];
 
 pub(crate) type Aes256Ctr64BE = ctr::Ctr64BE<aes::Aes256>;
 
 /// Fully working RLPx connection.
-pub(crate) struct RLPxConnection<S: AsyncWrite> {
+pub(crate) struct RLPxConnection<S> {
     state: RLPxState,
     stream: S,
     established: bool,
@@ -36,11 +28,11 @@ impl<S: AsyncWrite + AsyncRead + std::marker::Unpin> RLPxConnection<S> {
     pub async fn send(&mut self, message: rlpx::Message) {
         let mut frame_buffer = vec![];
         message.encode(&mut frame_buffer);
-        self.write_frame(frame_buffer).await;
+        frame::write(frame_buffer, &mut self.state, &mut self.stream).await;
     }
 
     pub async fn receive(&mut self) -> rlpx::Message {
-        let frame_data = self.read_frame().await;
+        let frame_data = frame::read(&mut self.state, &mut self.stream).await;
         let (msg_id, msg_data): (u8, _) = RLPDecode::decode_unfinished(&frame_data).unwrap();
         if !self.established {
             if msg_id == 0 {
@@ -60,142 +52,18 @@ impl<S: AsyncWrite + AsyncRead + std::marker::Unpin> RLPxConnection<S> {
             rlpx::Message::decode(msg_id, msg_data).unwrap()
         }
     }
-
-    async fn write_frame(&mut self, mut frame_data: Vec<u8>) {
-        let egress_aes = &mut self.state.egress_aes;
-        let egress_mac = &mut self.state.egress_mac;
-        let mac_aes_cipher = Aes256Enc::new_from_slice(&self.state.mac_key.0).unwrap();
-
-        // header = frame-size || header-data || header-padding
-        let mut header = Vec::with_capacity(32);
-        let frame_size = frame_data.len().to_be_bytes();
-        header.extend_from_slice(&frame_size[5..8]);
-
-        // header-data = [capability-id, context-id]  (both always zero)
-        let header_data = (0_u8, 0_u8);
-        header_data.encode(&mut header);
-
-        header.resize(16, 0);
-        egress_aes.apply_keystream(&mut header[..16]);
-
-        let header_mac_seed = {
-            let mac_digest: [u8; 16] = egress_mac.clone().finalize()[..16].try_into().unwrap();
-            let mut seed = mac_digest.into();
-            mac_aes_cipher.encrypt_block(&mut seed);
-            H128(seed.into()) ^ H128(header[..16].try_into().unwrap())
-        };
-        egress_mac.update(header_mac_seed);
-        let header_mac = egress_mac.clone().finalize();
-        header.extend_from_slice(&header_mac[..16]);
-
-        // Write header
-        self.stream.write_all(&header).await.unwrap();
-
-        // Pad to next multiple of 16
-        frame_data.resize(frame_data.len().next_multiple_of(16), 0);
-        egress_aes.apply_keystream(&mut frame_data);
-        let frame_ciphertext = frame_data;
-
-        // Send frame
-        self.stream.write_all(&frame_ciphertext).await.unwrap();
-
-        // Compute frame-mac
-        egress_mac.update(&frame_ciphertext);
-
-        // frame-mac-seed = aes(mac-secret, keccak256.digest(egress-mac)[:16]) ^ keccak256.digest(egress-mac)[:16]
-        let frame_mac_seed = {
-            let mac_digest: [u8; 16] = egress_mac.clone().finalize()[..16].try_into().unwrap();
-            let mut seed = mac_digest.into();
-            mac_aes_cipher.encrypt_block(&mut seed);
-            (H128(seed.into()) ^ H128(mac_digest)).0
-        };
-        egress_mac.update(frame_mac_seed);
-        let frame_mac = egress_mac.clone().finalize();
-
-        // Send frame-mac
-        self.stream.write_all(&frame_mac[..16]).await.unwrap();
-    }
-
-    async fn read_frame(&mut self) -> Vec<u8> {
-        let ingress_aes = &mut self.state.ingress_aes;
-        let ingress_mac = &mut self.state.ingress_mac;
-        let mac_aes_cipher = Aes256Enc::new_from_slice(&self.state.mac_key.0).unwrap();
-
-        // Receive the message's frame header
-        let mut frame_header = [0; 32];
-        self.stream.read_exact(&mut frame_header).await.unwrap();
-        // Both are padded to the block's size (16 bytes)
-        let (header_ciphertext, header_mac) = frame_header.split_at_mut(16);
-
-        // Validate MAC header
-        // header-mac-seed = aes(mac-secret, keccak256.digest(egress-mac)[:16]) ^ header-ciphertext
-        let header_mac_seed = {
-            let mac_digest: [u8; 16] = ingress_mac.clone().finalize()[..16].try_into().unwrap();
-            let mut seed = mac_digest.into();
-            mac_aes_cipher.encrypt_block(&mut seed);
-            (H128(seed.into()) ^ H128(header_ciphertext.try_into().unwrap())).0
-        };
-
-        // ingress-mac = keccak256.update(ingress-mac, header-mac-seed)
-        ingress_mac.update(header_mac_seed);
-
-        // header-mac = keccak256.digest(egress-mac)[:16]
-        let expected_header_mac = H128(ingress_mac.clone().finalize()[..16].try_into().unwrap());
-
-        assert_eq!(header_mac, expected_header_mac.0);
-
-        let header_text = header_ciphertext;
-        ingress_aes.apply_keystream(header_text);
-
-        // header-data = [capability-id, context-id]
-        // Both are unused, and always zero
-        assert_eq!(&header_text[3..6], &(0_u8, 0_u8).encode_to_vec());
-
-        let frame_size: usize =
-            u32::from_be_bytes([0, header_text[0], header_text[1], header_text[2]])
-                .try_into()
-                .unwrap();
-        // Receive the hello message
-        let padded_size = frame_size.next_multiple_of(16);
-        let mut frame_data = vec![0; padded_size + 16];
-        self.stream.read_exact(&mut frame_data).await.unwrap();
-        let (frame_ciphertext, frame_mac) = frame_data.split_at_mut(padded_size);
-
-        // check MAC
-        #[allow(clippy::needless_borrows_for_generic_args)]
-        ingress_mac.update(&frame_ciphertext);
-        let frame_mac_seed = {
-            let mac_digest: [u8; 16] = ingress_mac.clone().finalize()[..16].try_into().unwrap();
-            let mut seed = mac_digest.into();
-            mac_aes_cipher.encrypt_block(&mut seed);
-            (H128(seed.into()) ^ H128(mac_digest)).0
-        };
-        ingress_mac.update(frame_mac_seed);
-        let expected_frame_mac: [u8; 16] = ingress_mac.clone().finalize()[..16].try_into().unwrap();
-
-        assert_eq!(frame_mac, expected_frame_mac);
-
-        // decrypt frame
-        ingress_aes.apply_keystream(frame_ciphertext);
-
-        let (frame_data, _padding) = frame_ciphertext.split_at(frame_size);
-
-        frame_data.to_vec()
-    }
 }
 
 /// The current state of an RLPx connection
 #[derive(Clone)]
 pub(crate) struct RLPxState {
-    // TODO: maybe discard aes_key, since we only need the cipher
     // TODO: maybe precompute some values that are used more than once
     #[allow(unused)]
-    aes_key: H256,
-    mac_key: H256,
-    ingress_mac: Keccak256,
-    egress_mac: Keccak256,
-    ingress_aes: Aes256Ctr64BE,
-    egress_aes: Aes256Ctr64BE,
+    pub mac_key: H256,
+    pub ingress_mac: Keccak256,
+    pub egress_mac: Keccak256,
+    pub ingress_aes: Aes256Ctr64BE,
+    pub egress_aes: Aes256Ctr64BE,
 }
 
 impl RLPxState {
@@ -221,7 +89,6 @@ impl RLPxState {
         let egress_aes = ingress_aes.clone();
 
         Self {
-            aes_key,
             mac_key,
             ingress_mac,
             egress_mac,
@@ -263,12 +130,9 @@ mod tests {
             auth_data,
         );
 
-        let expected_aes_secret =
-            hex!("80e8632c05fed6fc2a13b0f8d31a3cf645366239170ea067065aba8e28bac487");
         let expected_mac_secret =
             hex!("2ea74ec5dae199227dff1af715362700e989d889d7a493cb0639691efb8e5f98");
 
-        assert_eq!(state.aes_key.0, expected_aes_secret);
         assert_eq!(state.mac_key.0, expected_mac_secret);
     }
 }
