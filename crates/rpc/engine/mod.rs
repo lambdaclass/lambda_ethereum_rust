1cf8ce57610b6025aca259f224418f840c9cbe20
Mario Rugiero <mrugiero@gmail.com>
2024-09-18 16:10:47 -0300

    refactor: use `RpcHandler` for all RPC calls (#388)
    **Motivation**
    
    Improve code organization.
    
    **Description**
    
    This migrates all the remaining endpoints except for the admin requests,
    which require a `Node` argument the trait doesn't provide.
    
    Resolves #376
    Closes #376

--- crates/rpc/engine/mod.rs
+++ crates/rpc/engine/mod.rs
@@ -2,11 +2,22 @@ pub mod exchange_transition_config;
 pub mod fork_choice;
 pub mod payload;
 
-use crate::RpcErr;
+use crate::{RpcErr, RpcHandler, Store};
 use serde_json::{json, Value};
 
 pub type ExchangeCapabilitiesRequest = Vec<String>;
 
-pub fn exchange_capabilities(capabilities: &ExchangeCapabilitiesRequest) -> Result<Value, RpcErr> {
-    Ok(json!(capabilities))
+impl RpcHandler for ExchangeCapabilitiesRequest {
+    fn parse(params: &Option<Vec<Value>>) -> Result<Self, RpcErr> {
+        params
+            .as_ref()
+            .ok_or(RpcErr::BadParams)?
+            .first()
+            .ok_or(RpcErr::BadParams)
+            .and_then(|v| serde_json::from_value(v.clone()).map_err(|_| RpcErr::BadParams))
+    }
+
+    fn handle(&self, _storage: Store) -> Result<Value, RpcErr> {
+        Ok(json!(*self))
+    }
 }
