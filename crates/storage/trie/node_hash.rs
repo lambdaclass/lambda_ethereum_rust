8ffb263f61da1c33551a629ac7fa2694cafb0666
fmoletta <99273364+fmoletta@users.noreply.github.com>
2024-09-19 14:31:07 -0300

    feat: implement `get_proof` for `Trie` (#419)
    **Motivation**
    Adding support for Trie::get_proof so we can use it in rpc endpoint
    etc_getProof
    
    <!-- Why does this pull request exist? What are its goals? -->
    
    **Description**
    
    * Add `Trie` method `get_proof`
    * Add protests for `Trie::get_proof`
    
    Other:
    
    
    * Replace `NodeHasher` with `NodeEncoder`, which behaves similarly, but
    instead of building up the node's hash, it builds up the node's encoded
    format, leaving the hashing to a later instance
    
    * Add method `encode_raw` for all node variants
    
    
    * Add `NodeHash` method `from_encoded_raw` which will hash the encoded
    node returned by the `NodeEncoder`
    <!-- A clear and concise general description of the changes this PR
    introduces -->
    
    <!-- Link to issues: Resolves #111, Resolves #222 -->
    
    Closes None, but is needed in order to implement #291

--- crates/storage/trie/node_hash.rs
+++ crates/storage/trie/node_hash.rs
@@ -1,21 +1,9 @@
-use std::cmp::min;
-
 use ethereum_rust_core::rlp::{decode::RLPDecode, encode::RLPEncode};
 use ethereum_types::H256;
 use libmdbx::orm::{Decodable, Encodable};
 use sha3::{Digest, Keccak256};
 
-type Output = digest::Output<Keccak256>;
-
 use super::nibble::{NibbleSlice, NibbleVec};
-
-#[derive(Default)]
-pub struct NodeHasher {
-    hash: Output,
-    len: usize,
-    hasher: Keccak256,
-    no_inline: bool,
-}
 
 /// Struct representing a trie node hash
 /// If the encoded node is less than 32 bits, contains the encoded node itself
@@ -25,133 +13,6 @@ pub struct NodeHasher {
 pub enum NodeHash {
     Hashed(H256),
     Inline(Vec<u8>),
-}
-
-impl NodeHasher {
-    pub fn new() -> Self {
-        Self {
-            ..Default::default()
-        }
-    }
-
-    pub const fn path_len(value_len: usize) -> usize {
-        Self::bytes_len((value_len >> 1) + 1, 0)
-    }
-
-    pub const fn bytes_len(value_len: usize, first_value: u8) -> usize {
-        match value_len {
-            1 if first_value < 128 => 1,
-            l if l < 56 => l + 1,
-            l => l + compute_byte_usage(l) + 1,
-        }
-    }
-
-    pub fn write_list_header(&mut self, children_len: usize) {
-        self.write_len(0xC0, 0xF7, children_len);
-    }
-
-    fn write_len(&mut self, short_base: u8, long_base: u8, value: usize) {
-        match value {
-            l if l < 56 => self.write_raw(&[short_base + l as u8]),
-            l => {
-                let l_len = compute_byte_usage(l);
-                self.write_raw(&[long_base + l_len as u8]);
-                self.write_raw(&l.to_be_bytes()[size_of::<usize>() - l_len..]);
-            }
-        }
-    }
-
-    pub fn write_raw(&mut self, value: &[u8]) {
-        let mut length = self.len;
-        let mut hash = self.hash;
-
-        let mut current_pos = 0;
-        while current_pos < value.len() {
-            let copy_len = min(32 - length, value.len() - current_pos);
-
-            let target_slice = &mut hash[length..length + copy_len];
-            let source_slice = &value[current_pos..current_pos + copy_len];
-            target_slice.copy_from_slice(source_slice);
-
-            current_pos += copy_len;
-            length += copy_len;
-
-            if length == 32 {
-                self.no_inline = true;
-                self.hasher.update(hash);
-                length = 0;
-            }
-        }
-        self.hash = hash;
-        self.len = length;
-    }
-
-    pub fn write_path_slice(&mut self, value: &NibbleSlice, kind: PathKind) {
-        let mut flag = kind.into_flag();
-
-        // TODO: Do not use iterators.
-        let nibble_count = value.clone().count();
-        let nibble_iter = if nibble_count & 0x01 != 0 {
-            let mut iter = value.clone();
-            flag |= 0x10;
-            flag |= iter.next().unwrap() as u8;
-            iter
-        } else {
-            value.clone()
-        };
-
-        let i2 = nibble_iter.clone().skip(1).step_by(2);
-        if nibble_count > 1 {
-            self.write_len(0x80, 0xB7, (nibble_count >> 1) + 1);
-        }
-        self.write_raw(&[flag]);
-        for (a, b) in nibble_iter.step_by(2).zip(i2) {
-            self.write_raw(&[((a as u8) << 4) | (b as u8)]);
-        }
-    }
-
-    pub fn write_path_vec(&mut self, value: &NibbleVec, kind: PathKind) {
-        let mut flag = kind.into_flag();
-
-        // TODO: Do not use iterators.
-        let nibble_count = value.len();
-        let nibble_iter = if nibble_count & 0x01 != 0 {
-            let mut iter = value.iter();
-            flag |= 0x10;
-            flag |= iter.next().unwrap() as u8;
-            iter
-        } else {
-            value.iter()
-        };
-
-        let i2 = nibble_iter.clone().skip(1).step_by(2);
-        if nibble_count > 1 {
-            self.write_len(0x80, 0xB7, (nibble_count >> 1) + 1);
-        }
-        self.write_raw(&[flag]);
-        for (a, b) in nibble_iter.step_by(2).zip(i2) {
-            self.write_raw(&[((a as u8) << 4) | (b as u8)]);
-        }
-    }
-
-    pub fn write_bytes(&mut self, value: &[u8]) {
-        if value.len() == 1 && value[0] < 128 {
-            self.write_raw(&[value[0]]);
-        } else {
-            self.write_len(0x80, 0xB7, value.len());
-            self.write_raw(value);
-        }
-    }
-
-    pub fn finalize(mut self) -> NodeHash {
-        if self.no_inline {
-            let hash = self.hash;
-            self.hasher.update(&hash[..self.len]);
-            NodeHash::Hashed(H256::from_slice(self.hasher.finalize().as_slice()))
-        } else {
-            NodeHash::Inline(self.hash[..self.len].to_vec())
-        }
-    }
 }
 
 const fn compute_byte_usage(value: usize) -> usize {
@@ -184,6 +45,15 @@ impl AsRef<[u8]> for NodeHash {
 }
 
 impl NodeHash {
+    /// Returns the `NodeHash` of an encoded node (encoded using the NodeEncoder)
+    pub fn from_encoded_raw(encoded: Vec<u8>) -> NodeHash {
+        if encoded.len() >= 32 {
+            let hash = Keccak256::new_with_prefix(&encoded).finalize();
+            NodeHash::Hashed(H256::from_slice(hash.as_slice()))
+        } else {
+            NodeHash::Inline(encoded)
+        }
+    }
     /// Returns the finalized hash
     /// NOTE: This will hash smaller nodes, only use to get the final root hash, not for intermediate node hashes
     pub fn finalize(self) -> H256 {
@@ -279,5 +149,110 @@ impl RLPDecode for NodeHash {
         (hash, rest) = RLPDecode::decode_unfinished(rlp)?;
         let hash = NodeHash::from(hash);
         Ok((hash, rest))
+    }
+}
+
+#[derive(Default, Debug)]
+pub struct NodeEncoder {
+    encoded: Vec<u8>,
+}
+
+impl NodeEncoder {
+    pub fn new() -> Self {
+        Self {
+            ..Default::default()
+        }
+    }
+
+    pub const fn path_len(value_len: usize) -> usize {
+        Self::bytes_len((value_len >> 1) + 1, 0)
+    }
+
+    pub const fn bytes_len(value_len: usize, first_value: u8) -> usize {
+        match value_len {
+            1 if first_value < 128 => 1,
+            l if l < 56 => l + 1,
+            l => l + compute_byte_usage(l) + 1,
+        }
+    }
+
+    pub fn write_list_header(&mut self, children_len: usize) {
+        self.write_len(0xC0, 0xF7, children_len);
+    }
+
+    fn write_len(&mut self, short_base: u8, long_base: u8, value: usize) {
+        match value {
+            l if l < 56 => self.write_raw(&[short_base + l as u8]),
+            l => {
+                let l_len = compute_byte_usage(l);
+                self.write_raw(&[long_base + l_len as u8]);
+                self.write_raw(&l.to_be_bytes()[size_of::<usize>() - l_len..]);
+            }
+        }
+    }
+
+    pub fn write_raw(&mut self, value: &[u8]) {
+        self.encoded.extend_from_slice(value);
+    }
+
+    pub fn write_path_slice(&mut self, value: &NibbleSlice, kind: PathKind) {
+        let mut flag = kind.into_flag();
+
+        // TODO: Do not use iterators.
+        let nibble_count = value.clone().count();
+        let nibble_iter = if nibble_count & 0x01 != 0 {
+            let mut iter = value.clone();
+            flag |= 0x10;
+            flag |= iter.next().unwrap() as u8;
+            iter
+        } else {
+            value.clone()
+        };
+
+        let i2 = nibble_iter.clone().skip(1).step_by(2);
+        if nibble_count > 1 {
+            self.write_len(0x80, 0xB7, (nibble_count >> 1) + 1);
+        }
+        self.write_raw(&[flag]);
+        for (a, b) in nibble_iter.step_by(2).zip(i2) {
+            self.write_raw(&[((a as u8) << 4) | (b as u8)]);
+        }
+    }
+
+    pub fn write_path_vec(&mut self, value: &NibbleVec, kind: PathKind) {
+        let mut flag = kind.into_flag();
+
+        // TODO: Do not use iterators.
+        let nibble_count = value.len();
+        let nibble_iter = if nibble_count & 0x01 != 0 {
+            let mut iter = value.iter();
+            flag |= 0x10;
+            flag |= iter.next().unwrap() as u8;
+            iter
+        } else {
+            value.iter()
+        };
+
+        let i2 = nibble_iter.clone().skip(1).step_by(2);
+        if nibble_count > 1 {
+            self.write_len(0x80, 0xB7, (nibble_count >> 1) + 1);
+        }
+        self.write_raw(&[flag]);
+        for (a, b) in nibble_iter.step_by(2).zip(i2) {
+            self.write_raw(&[((a as u8) << 4) | (b as u8)]);
+        }
+    }
+
+    pub fn write_bytes(&mut self, value: &[u8]) {
+        if value.len() == 1 && value[0] < 128 {
+            self.write_raw(&[value[0]]);
+        } else {
+            self.write_len(0x80, 0xB7, value.len());
+            self.write_raw(value);
+        }
+    }
+
+    pub fn finalize(self) -> Vec<u8> {
+        self.encoded
     }
 }
