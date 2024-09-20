54a4d15c1855f853a4ababa04cd03108d6d9905c
fmoletta <fedemoletta@hotmail.com>
2024-09-20 18:26:01 -0300

    Improve errors

--- crates/trie/error.rs
+++ crates/trie/error.rs
@@ -1,15 +1,10 @@
 use ethereum_rust_rlp::error::RLPDecodeError;
 use thiserror::Error;
 
-// TODO improve errors
 #[derive(Debug, Error)]
 pub enum TrieError {
-    #[error("DecodeError")]
-    DecodeError,
     #[error("Libmdbx error: {0}")]
     LibmdbxError(anyhow::Error),
-    #[error("{0}")]
-    Custom(String),
     #[error(transparent)]
     RLPDecode(#[from] RLPDecodeError),
 }
