9373bcb957736244df1ff40ed16fcc7c1a61c627
ilitteri <ilitteri@fi.uba.ar>
2024-09-23 16:39:24 -0300

    LEVM scaffolding

---
+++ crates/levm/src/vm.rs
@@ -0,0 +1,86 @@
+use crate::opcodes::Opcode;
+use bytes::Bytes;
+use ethereum_types::U256;
+
+#[derive(Debug, Clone)]
+pub struct VM {
+    pub stack: Vec<U256>, // max 1024 in the future
+    pc: usize,
+}
+
+impl VM {
+    pub fn execute(&mut self, mut bytecode: Bytes) {
+        loop {
+            match self.next_opcode(&mut bytecode).unwrap() {
+                Opcode::STOP => break,
+                Opcode::ADD => {
+                    let a = self.stack.pop().unwrap();
+                    let b = self.stack.pop().unwrap();
+                    self.stack.push(a + b);
+                }
+                Opcode::PUSH32 => {
+                    let mut next_32_bytes = [0; 32];
+                    bytecode
+                        .iter()
+                        .skip(self.pc)
+                        .take(32)
+                        .rev()
+                        .enumerate()
+                        .for_each(|(i, byte)| {
+                            next_32_bytes[i] = *byte;
+                        });
+                    next_32_bytes.reverse();
+                    let value_to_push = U256::from(next_32_bytes);
+                    dbg!(value_to_push);
+                    self.stack.push(value_to_push);
+                    self.increment_pc_by(32);
+                }
+            }
+        }
+    }
+
+    fn next_opcode(&mut self, opcodes: &mut Bytes) -> Option<Opcode> {
+        let opcode = opcodes.get(self.pc).copied().map(Opcode::from);
+        self.increment_pc();
+        opcode
+    }
+
+    fn increment_pc(&mut self) {
+        self.increment_pc_by(1);
+    }
+
+    fn increment_pc_by(&mut self, n: usize) {
+        self.pc += n;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::operations::Operation;
+
+    use super::*;
+
+    #[test]
+    fn test() {
+        let mut vm = VM {
+            stack: vec![],
+            pc: 0,
+        };
+
+        let operations = [
+            Operation::Push32(U256::one()),
+            Operation::Push32(U256::zero()),
+            Operation::Add,
+            Operation::Stop,
+        ];
+
+        let bytecode = operations
+            .iter()
+            .flat_map(Operation::to_bytecode)
+            .collect::<Bytes>();
+
+        vm.execute(bytecode);
+
+        println!("{vm:?}");
+    }
+}
