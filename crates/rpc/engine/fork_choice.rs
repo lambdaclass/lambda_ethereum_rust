2b2f4dd5735b0919797622b27cb49c411718bee4
fmoletta <fedemoletta@hotmail.com>
2024-09-24 16:39:30 -0300

    Clippy + fmt

--- crates/rpc/engine/fork_choice.rs
+++ crates/rpc/engine/fork_choice.rs
@@ -38,7 +38,7 @@ impl RpcHandler for ForkChoiceUpdatedV3 {
             .map_err(|_| RpcErr::Internal)
         };
         if self.fork_choice_state.head_block_hash.is_zero() {
-            return Ok(error_response("forkchoice requested update to zero hash")?);
+            return error_response("forkchoice requested update to zero hash");
         }
         // Check if we have the block stored
         let Some(head_block) = storage.get_block_by_hash(self.fork_choice_state.head_block_hash)?
@@ -57,17 +57,15 @@ impl RpcHandler for ForkChoiceUpdatedV3 {
                 || total_difficulty.is_none()
                 || head_block.header.number > 0 && parent_total_difficulty.is_none()
             {
-                return Ok(error_response(
+                return error_response(
                     "total difficulties unavailable for terminal total difficulty check",
-                )?);
+                );
             }
             if total_difficulty.unwrap() < terminal_total_difficulty.unwrap().into() {
-                return Ok(error_response("refusing beacon update to pre-merge")?);
+                return error_response("refusing beacon update to pre-merge");
             }
             if head_block.header.number > 0 && parent_total_difficulty.unwrap() >= U256::zero() {
-                return Ok(error_response(
-                    "parent block is already post terminal total difficulty",
-                )?);
+                return error_response("parent block is already post terminal total difficulty");
             }
         }
         let canonical_block = storage.get_canonical_block(head_block.header.number)?;
@@ -82,7 +80,7 @@ impl RpcHandler for ForkChoiceUpdatedV3 {
         } else if current_block_hash.is_some_and(|h| h != self.fork_choice_state.head_block_hash) {
             // If the head block is already in our canonical chain, the beacon client is
             // probably resyncing. Ignore the update.
-            return Ok(serde_json::to_value(PayloadStatus::valid()).map_err(|_| RpcErr::Internal)?);
+            return serde_json::to_value(PayloadStatus::valid()).map_err(|_| RpcErr::Internal);
         }
 
         // Process finalized block
@@ -91,14 +89,14 @@ impl RpcHandler for ForkChoiceUpdatedV3 {
             let Some(finalized_block) =
                 storage.get_block_by_hash(self.fork_choice_state.finalized_block_hash)?
             else {
-                return Ok(error_response("final block not available in database")?);
+                return error_response("final block not available in database");
             };
 
             if !storage
                 .get_canonical_block(finalized_block.header.number)?
                 .is_some_and(|h| h == self.fork_choice_state.finalized_block_hash)
             {
-                return Ok(error_response("final block not in canonical chain")?);
+                return error_response("final block not in canonical chain");
             }
             // Set the finalized block
             storage.update_finalized_block_number(finalized_block.header.number)?;
@@ -110,14 +108,14 @@ impl RpcHandler for ForkChoiceUpdatedV3 {
             let Some(safe_block) =
                 storage.get_block_by_hash(self.fork_choice_state.safe_block_hash)?
             else {
-                return Ok(error_response("safe block not available in database")?);
+                return error_response("safe block not available in database");
             };
 
             if !storage
                 .get_canonical_block(safe_block.header.number)?
                 .is_some_and(|h| h == self.fork_choice_state.safe_block_hash)
             {
-                return Ok(error_response("safe block not in canonical chain")?);
+                return error_response("safe block not in canonical chain");
             }
             // Set the safe block
             storage.update_safe_block_number(safe_block.header.number)?;
@@ -143,6 +141,6 @@ impl RpcHandler for ForkChoiceUpdatedV3 {
             storage.add_local_block(payload_id, payload)?;
         }
 
-        Ok(serde_json::to_value(response).map_err(|_| RpcErr::Internal)?)
+        serde_json::to_value(response).map_err(|_| RpcErr::Internal)
     }
 }
