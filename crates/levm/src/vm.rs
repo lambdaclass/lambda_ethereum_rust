877757a74e13dc57804f5d523d45e2754ea722f7
ilitteri <ilitteri@fi.uba.ar>
2024-09-24 19:34:52 -0300

    Integrate `CallFrame`s in `VM`

--- crates/levm/src/vm.rs
+++ crates/levm/src/vm.rs
@@ -1,139 +1,93 @@
-use crate::opcodes::Opcode;
+use crate::{call_frame::CallFrame, opcodes::Opcode};
 use bytes::Bytes;
 use ethereum_types::U256;
 
 #[derive(Debug, Clone, Default)]
 pub struct VM {
-    pub stack: Vec<U256>, // max 1024 in the future
-    pub memory: Memory,
-    pub pc: usize,
+    pub call_frames: Vec<CallFrame>,
 }
 
 impl VM {
-    pub fn execute(&mut self, mut bytecode: Bytes) {
+    pub fn new(bytecode: Bytes) -> Self {
+        let initial_call_frame = CallFrame {
+            bytecode,
+            ..Default::default()
+        };
+        Self {
+            call_frames: vec![initial_call_frame],
+        }
+    }
+
+    pub fn execute(&mut self) {
+        let current_call_frame = self.current_call_frame();
         loop {
-            match self.next_opcode(&mut bytecode).unwrap() {
+            match current_call_frame.next_opcode().unwrap() {
                 Opcode::STOP => break,
                 Opcode::ADD => {
-                    let a = self.stack.pop().unwrap();
-                    let b = self.stack.pop().unwrap();
-                    self.stack.push(a + b);
+                    let a = current_call_frame.stack.pop().unwrap();
+                    let b = current_call_frame.stack.pop().unwrap();
+                    current_call_frame.stack.push(a + b);
                 }
                 Opcode::PUSH32 => {
-                    let next_32_bytes = bytecode.get(self.pc..self.pc + 32).unwrap();
+                    let next_32_bytes = current_call_frame
+                        .bytecode
+                        .get(current_call_frame.pc..current_call_frame.pc + 32)
+                        .unwrap();
                     let value_to_push = U256::from(next_32_bytes);
                     dbg!(value_to_push);
-                    self.stack.push(value_to_push);
-                    self.increment_pc_by(32);
+                    current_call_frame.stack.push(value_to_push);
+                    current_call_frame.increment_pc_by(32);
                 }
                 Opcode::MLOAD => {
                     // spend_gas(3);
-                    let offset = self.stack.pop().unwrap().try_into().unwrap();
-                    let value = self.memory.load(offset);
-                    self.stack.push(value);
+                    let offset = current_call_frame.stack.pop().unwrap().try_into().unwrap();
+                    let value = current_call_frame.memory.load(offset);
+                    current_call_frame.stack.push(value);
                 }
                 Opcode::MSTORE => {
                     // spend_gas(3);
-                    let offset = self.stack.pop().unwrap().try_into().unwrap();
-                    let value = self.stack.pop().unwrap();
+                    let offset = current_call_frame.stack.pop().unwrap().try_into().unwrap();
+                    let value = current_call_frame.stack.pop().unwrap();
                     let mut value_bytes = [0u8; 32];
                     value.to_big_endian(&mut value_bytes);
 
-                    self.memory.store_bytes(offset, &value_bytes);
+                    current_call_frame.memory.store_bytes(offset, &value_bytes);
                 }
                 Opcode::MSTORE8 => {
                     // spend_gas(3);
-                    let offset = self.stack.pop().unwrap().try_into().unwrap();
-                    let value = self.stack.pop().unwrap();
+                    let offset = current_call_frame.stack.pop().unwrap().try_into().unwrap();
+                    let value = current_call_frame.stack.pop().unwrap();
                     let mut value_bytes = [0u8; 32];
                     value.to_big_endian(&mut value_bytes);
 
-                    self.memory
+                    current_call_frame
+                        .memory
                         .store_bytes(offset, value_bytes[31..32].as_ref());
                 }
                 Opcode::MSIZE => {
                     // spend_gas(2);
-                    self.stack.push(self.memory.size());
+                    current_call_frame
+                        .stack
+                        .push(current_call_frame.memory.size());
                 }
                 Opcode::MCOPY => {
                     // spend_gas(3) + dynamic gas
-                    let dest_offset = self.stack.pop().unwrap().try_into().unwrap();
-                    let src_offset = self.stack.pop().unwrap().try_into().unwrap();
-                    let size = self.stack.pop().unwrap().try_into().unwrap();
+                    let dest_offset = current_call_frame.stack.pop().unwrap().try_into().unwrap();
+                    let src_offset = current_call_frame.stack.pop().unwrap().try_into().unwrap();
+                    let size = current_call_frame.stack.pop().unwrap().try_into().unwrap();
                     if size == 0 {
                         continue;
                     }
 
-                    self.memory.copy(src_offset, dest_offset, size);
+                    current_call_frame
+                        .memory
+                        .copy(src_offset, dest_offset, size);
                 }
             }
         }
     }
 
-    fn next_opcode(&mut self, opcodes: &mut Bytes) -> Option<Opcode> {
-        let opcode = opcodes.get(self.pc).copied().map(Opcode::from);
-        self.increment_pc();
-        opcode
-    }
-
-    fn increment_pc_by(&mut self, count: usize) {
-        self.pc += count;
-    }
-
-    fn increment_pc(&mut self) {
-        self.increment_pc_by(1);
-    }
-
-    pub fn pc(&self) -> usize {
-        self.pc
-    }
-}
-
-#[derive(Debug, Clone, Default)]
-pub struct Memory {
-    data: Vec<u8>,
-}
-
-impl Memory {
-    pub fn new() -> Self {
-        Self { data: Vec::new() }
-    }
-
-    fn resize(&mut self, offset: usize) {
-        if offset.next_multiple_of(32) > self.data.len() {
-            self.data.resize(offset.next_multiple_of(32), 0);
-        }
-    }
-
-    pub fn load(&mut self, offset: usize) -> U256 {
-        self.resize(offset + 32);
-        let value_bytes: [u8; 32] = self
-            .data
-            .get(offset..offset + 32)
-            .unwrap()
-            .try_into()
-            .unwrap();
-        U256::from(value_bytes)
-    }
-
-    pub fn store_bytes(&mut self, offset: usize, value: &[u8]) {
-        let len = value.len();
-        self.resize(offset + len);
-        self.data
-            .splice(offset..offset + len, value.iter().copied());
-    }
-
-    pub fn size(&self) -> U256 {
-        U256::from(self.data.len())
-    }
-
-    pub fn copy(&mut self, src_offset: usize, dest_offset: usize, size: usize) {
-        let max_size = std::cmp::max(src_offset + size, dest_offset + size);
-        self.resize(max_size);
-        let mut temp = vec![0u8; size];
-
-        temp.copy_from_slice(&self.data[src_offset..src_offset + size]);
-
-        self.data[dest_offset..dest_offset + size].copy_from_slice(&temp);
+    pub fn current_call_frame(&mut self) -> &mut CallFrame {
+        self.call_frames.last_mut().unwrap()
     }
 }
