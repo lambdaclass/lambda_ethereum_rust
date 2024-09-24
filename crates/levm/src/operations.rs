3eb57214bf46a0a13831d3c581dfb0d86a9c707c
Luca Lazcano <lazcanoluca@gmail.com>
2024-09-24 18:03:21 -0300

    Add to bytecode for push operations

--- crates/levm/src/operations.rs
+++ crates/levm/src/operations.rs
@@ -75,7 +75,7 @@ pub enum Operation {
     // Mcopy,
     // Push0,
     Push32(U256),
-    // Push((u8, U256)),
+    Push((u8, U256)),
     // Dup(u8),
     // Swap(u8),
     // Log(u8),
@@ -164,7 +164,22 @@ impl Operation {
             // Operation::Tstore => Bytes::copy_from_slice(&[Opcode::TSTORE as u8]),
             // Operation::Mcopy => Bytes::copy_from_slice(&[Opcode::MCOPY as u8]),
             // Operation::Push0 => Bytes::copy_from_slice(&[Opcode::PUSH0 as u8]),
-            // Operation::Push((n, x)) => {},
+            Operation::Push((n, value)) => {
+                assert!(*n <= 32);
+                // the amount of bytes needed to represent the value must
+                // be less than the n in PUSHn
+                assert!(value.bits().div_ceil(8) <= *n as usize);
+                let mut word_buffer = [0; 32];
+                value.to_big_endian(&mut word_buffer);
+                // extract the last n bytes to push
+                let value_to_push = &word_buffer[((32 - *n) as usize)..];
+                assert_eq!(value_to_push.len(), *n as usize);
+                let opcode = Opcode::from(Opcode::PUSH0 as u8 + *n);
+                let mut bytes = vec![opcode as u8];
+                bytes.extend_from_slice(&value_to_push);
+
+                Bytes::copy_from_slice(&bytes)
+            }
             Operation::Push32(value) => {
                 let mut value_to_push = [0; 32];
                 value.to_big_endian(&mut value_to_push);
@@ -185,5 +200,28 @@ impl Operation {
               // Operation::Invalid => Bytes::copy_from_slice(&[Opcode::INVALID as u8]),
               // Operation::SelfDestruct => Bytes::copy_from_slice(&[Opcode::SELFDESTRUCT as u8]),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn push1_correct_bytecode() {
+        let op = Operation::Push((1, 0xff.into())).to_bytecode();
+        assert_eq!(op, Bytes::from(vec![0x60, 0xff]))
+    }
+
+    #[test]
+    #[should_panic]
+    fn push_value_greater_than_fits_in_pushn_panics() {
+        Operation::Push((1, 0xfff.into())).to_bytecode();
+    }
+
+    #[test]
+    #[should_panic]
+    fn push_greater_than_32_panics() {
+        Operation::Push((33, U256::zero())).to_bytecode();
     }
 }
