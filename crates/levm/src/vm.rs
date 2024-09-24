b6a6e5dadb9786713932f526e7ff180f7460c0b1
Luca Lazcano <lazcanoluca@gmail.com>
2024-09-24 18:06:12 -0300

    Implement push opcode

--- crates/levm/src/vm.rs
+++ crates/levm/src/vm.rs
@@ -18,13 +18,20 @@ impl VM {
                     let b = self.stack.pop().unwrap();
                     self.stack.push(a + b);
                 }
+                op if (Opcode::PUSH1..Opcode::PUSH32).contains(&op) => {
+                    let n_bytes = (op as u8) - (Opcode::PUSH1 as u8) + 1;
+                    let next_n_bytes = bytecode.get(self.pc..self.pc + n_bytes as usize).unwrap();
+                    let value_to_push = U256::from(next_n_bytes);
+                    self.stack.push(value_to_push);
+                    self.increment_pc_by(n_bytes as usize);
+                }
                 Opcode::PUSH32 => {
                     let next_32_bytes = bytecode.get(self.pc..self.pc + 32).unwrap();
                     let value_to_push = U256::from(next_32_bytes);
-                    dbg!(value_to_push);
                     self.stack.push(value_to_push);
                     self.increment_pc_by(32);
                 }
+                _ => unimplemented!(),
             }
         }
     }
@@ -45,5 +52,42 @@ impl VM {
 
     pub fn pc(&self) -> usize {
         self.pc
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::operations::Operation;
+
+    use super::*;
+
+    #[test]
+    fn push1_ok() {
+        let mut vm = VM::default();
+
+        let to_push = U256::from_big_endian(&[0xff]);
+
+        let operations = [Operation::Push((1, to_push)), Operation::Stop];
+        let bytecode = operations.iter().flat_map(Operation::to_bytecode).collect();
+
+        vm.execute(bytecode);
+
+        assert_eq!(vm.stack[0], to_push);
+        assert_eq!(vm.pc, 3);
+    }
+
+    #[test]
+    fn push5_ok() {
+        let mut vm = VM::default();
+
+        let to_push = U256::from_big_endian(&[0xff, 0xff, 0xff, 0xff, 0xff]);
+
+        let operations = [Operation::Push((5, to_push)), Operation::Stop];
+        let bytecode = operations.iter().flat_map(Operation::to_bytecode).collect();
+
+        vm.execute(bytecode);
+
+        assert_eq!(vm.stack[0], to_push);
+        assert_eq!(vm.pc, 7);
     }
 }
