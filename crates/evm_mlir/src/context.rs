c363ae133b23e9e286fbb1cc81051e6dab4e7f00
Javier Chatruc <jrchatruc@gmail.com>
2024-09-25 18:16:20 -0300

    Test

--- crates/evm_mlir/src/context.rs
+++ crates/evm_mlir/src/context.rs
@@ -69,6 +69,7 @@ impl Context {
     }
 
     pub fn compile(&self, program: &Program, config: Session) -> Result<MLIRModule, CodegenError> {
+        assert!(false);
         static INITIALIZED: OnceLock<()> = OnceLock::new();
         INITIALIZED.get_or_init(|| unsafe {
             LLVM_InitializeAllTargets();
