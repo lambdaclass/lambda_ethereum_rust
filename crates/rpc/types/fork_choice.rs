1bdf863dcf13bf5abfb9aead0c94e64aecced028
fmoletta <fedemoletta@hotmail.com>
2024-09-25 15:25:59 -0300

    Fix ser

--- crates/rpc/types/fork_choice.rs
+++ crates/rpc/types/fork_choice.rs
@@ -15,6 +15,7 @@ pub struct ForkChoiceState {
 #[serde(rename_all = "camelCase")]
 #[allow(unused)]
 pub struct PayloadAttributesV3 {
+    #[serde(with = "serde_utils::u64::hex_str")]
     pub timestamp: u64,
     pub prev_randao: H256,
     pub suggested_fee_recipient: Address,
