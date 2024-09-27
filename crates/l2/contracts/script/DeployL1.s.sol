9bb925241f40abf18bf938a369cb68f5eaffae91
ilitteri <ilitteri@fi.uba.ar>
2024-09-27 13:06:48 -0300

    Cleanup scripts

--- crates/l2/contracts/script/DeployL1.s.sol
+++ crates/l2/contracts/script/DeployL1.s.sol
@@ -3,50 +3,37 @@ pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
 import {BlockExecutor} from "../src/l1/BlockExecutor.sol";
-import {Inbox} from "../src/l1/Inbox.sol";
+import {CommonBridge} from "../src/l1/CommonBridge.sol";
 import {Utils} from "./Utils.sol";
 
 contract DeployL1Script is Script {
+    /// @notice Address of the deterministic create2 factory.
+    /// @dev This address corresponds to a contracts that is set in the storage
+    /// in the genesis file. The same contract with the same address is deployed
+    /// in every testnet, so if this script is run in a testnet instead of in a
+    /// local environment, it should work.
     address constant DETERMINISTIC_CREATE2_ADDRESS = 0x4e59b44847b379578588920cA78FbF26c0B4956C;
-    address internal create2Factory;
 
-    function setUp() public {
-        instantiateCreate2Factory();
-    }
+    function setUp() public {}
 
     function run() public {
         console.log("Deploying L1 contracts");
 
         deployBlockExecutor();
-        deployInbox();
-    }
-
-    function instantiateCreate2Factory() internal {
-        address contractAddress;
-
-        bool isDeterministicDeployed = DETERMINISTIC_CREATE2_ADDRESS.code.length > 0;
-
-        if (isDeterministicDeployed) {
-            contractAddress = DETERMINISTIC_CREATE2_ADDRESS;
-            console.log("Using deterministic Create2Factory address:", contractAddress);
-        } else {
-            contractAddress = Utils.deployCreate2Factory();
-            console.log("Create2Factory deployed at:", contractAddress);
-        }
-        create2Factory = contractAddress;
+        deployCommonBridge();
     }
 
     function deployBlockExecutor() internal {
         bytes memory bytecode = type(BlockExecutor).creationCode;
         bytes32 salt = bytes32(0);
-        address contractAddress = Utils.deployWithCreate2(bytecode, salt, create2Factory);
+        address contractAddress = Utils.deployWithCreate2(bytecode, salt, DETERMINISTIC_CREATE2_ADDRESS);
         console.log("BlockExecutor deployed at:", contractAddress);
     }
 
-    function deployInbox() internal {
-        bytes memory bytecode = type(Inbox).creationCode;
+    function deployCommonBridge() internal {
+        bytes memory bytecode = type(CommonBridge).creationCode;
         bytes32 salt = bytes32(0);
-        address contractAddress = Utils.deployWithCreate2(bytecode, salt, create2Factory);
-        console.log("Inbox deployed at:", contractAddress);
+        address contractAddress = Utils.deployWithCreate2(bytecode, salt, DETERMINISTIC_CREATE2_ADDRESS);
+        console.log("CommonBridge deployed at:", contractAddress);
     }
 }
--- crates/l2/contracts/script/Utils.sol
+++ crates/l2/contracts/script/Utils.sol
@@ -8,24 +8,6 @@ library Utils {
     address internal constant VM_ADDRESS = address(uint160(uint256(keccak256("hevm cheat code"))));
     Vm internal constant vm = Vm(VM_ADDRESS);
 
-    // Create2Factory deterministic bytecode.
-    // https://github.com/Arachnid/deterministic-deployment-proxy
-    bytes internal constant CREATE2_FACTORY_BYTECODE =
-        hex"604580600e600039806000f350fe7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe03601600081602082378035828234f58015156039578182fd5b8082525050506014600cf3";
-
-    function deployCreate2Factory() internal returns (address) {
-        address child;
-        bytes memory bytecode = CREATE2_FACTORY_BYTECODE;
-        vm.startBroadcast();
-        assembly {
-            child := create(0, add(bytecode, 0x20), mload(bytecode))
-        }
-        vm.stopBroadcast();
-        require(child != address(0), "Failed to deploy Create2Factory");
-        require(child.code.length > 0, "Failed to deploy Create2Factory");
-        return child;
-    }
-
     function deployWithCreate2(bytes memory bytecode, bytes32 salt, address create2Factory) internal returns (address) {
         if (bytecode.length == 0) {
             revert("Bytecode is not set");
