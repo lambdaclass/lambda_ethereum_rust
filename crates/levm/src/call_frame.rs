2284cd70f05e895e1e0df5f14b014950c5b45a79
Paolo Belforte <75280184+belfortep@users.noreply.github.com>
2024-09-27 15:51:14 -0300

    Update type of topics vec in logs
    Co-authored-by: Luca Lazcano <64329820+lazcanoluca@users.noreply.github.com>

--- crates/levm/src/call_frame.rs
+++ crates/levm/src/call_frame.rs
@@ -5,7 +5,7 @@ use ethereum_types::{Address, U256};
 #[derive(Clone, Debug, Default, Eq, PartialEq, Hash)]
 pub struct Log {
     pub address: Address,
-    pub topics: Vec<U256>,
+    pub topics: Vec<H32>,
     pub data: Vec<u8>,
 }
 
