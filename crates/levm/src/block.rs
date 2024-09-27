164b7e06cc9ab28dea9b91d18e47656fd217ba36
Paolo Belforte <pbelforte2003@gmail.com>
2024-09-27 09:19:22 -0300

    Added docs for eip-4844 const

--- crates/levm/src/block.rs
+++ crates/levm/src/block.rs
@@ -39,11 +39,12 @@ pub struct BlockEnv {
 }
 
 // EIP-4844 constants.
+/// Minimum gas price for data blobs.
 pub const MIN_BLOB_GASPRICE: u64 = 1;
+/// Controls the maximum rate of change for blob gas price.
 pub const BLOB_GASPRICE_UPDATE_FRACTION: u64 = 3338477;
 /// Gas consumption of a single data blob (== blob byte size).
 pub const GAS_PER_BLOB: u64 = 1 << 17;
-
 /// Target number of the blob per block.
 pub const TARGET_BLOB_NUMBER_PER_BLOCK: u64 = 3;
 /// Target consumable blob gas for data blobs per block (for 1559-like pricing).
