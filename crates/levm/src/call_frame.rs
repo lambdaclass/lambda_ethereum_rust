e10179a82965fcfca3c29106d4d71a349b448fcf
iinaki <llorensinaki22@gmail.com>
2024-09-27 17:24:19 -0300

    Remove comment

--- crates/levm/src/call_frame.rs
+++ crates/levm/src/call_frame.rs
@@ -14,7 +14,7 @@ pub struct CallFrame {
     pub delegate: Option<Address>,
     pub msg_value: U256,
     pub stack: Vec<U256>, // max 1024 in the future
-    pub memory: Memory,   // y si implementamos como una memoria?
+    pub memory: Memory, 
     pub calldata: Bytes,
     pub returndata: Bytes,
     // where to store return data of subcall
