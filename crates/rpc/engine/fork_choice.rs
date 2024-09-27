b85d39f0718e482155fd480d247632c864e6e666
Tomás Arjovsky <t.arjovsky@gmail.com>
2024-09-27 19:44:16 +0200

    Add fork choice ancestry checks for fork_choice_updated_v3

--- crates/rpc/engine/fork_choice.rs
+++ crates/rpc/engine/fork_choice.rs
@@ -1,4 +1,4 @@
-use ethereum_rust_core::types::{BlockHash, BlockNumber};
+use ethereum_rust_core::types::{BlockHash, BlockHeader, BlockNumber};
 use ethereum_rust_storage::{error::StoreError, Store};
 use serde_json::{json, Value};
 use tracing::warn;
@@ -30,54 +30,86 @@ impl RpcHandler for ForkChoiceUpdatedV3 {
     fn handle(&self, storage: Store) -> Result<Value, RpcErr> {
         // Just a minimal implementation to pass rpc-compat Hive tests.
         // TODO (#50): Implement `engine_forkchoiceUpdatedV3`
-        let safe = storage.get_block_number(self.fork_choice_state.safe_block_hash);
-        let finalized = storage.get_block_number(self.fork_choice_state.finalized_block_hash);
 
-        // Check if we already have the blocks stored.
-        let (safe_block_number, finalized_block_number) = match (safe, finalized) {
-            (Ok(Some(safe)), Ok(Some(finalized))) => (safe, finalized),
-            _ => return Err(RpcErr::Internal),
-        };
+        let finalized_header_res =
+            storage.get_block_header_by_hash(self.fork_choice_state.finalized_block_hash)?;
+        let safe_header_res =
+            storage.get_block_header_by_hash(self.fork_choice_state.safe_block_hash)?;
+        let head_header_res =
+            storage.get_block_header_by_hash(self.fork_choice_state.head_block_hash)?;
 
-        // Check if the payload is available for the new head hash.
-        let header = match storage.get_block_header_by_hash(self.fork_choice_state.head_block_hash)
+        // Check that we already have all the needed blocks stored and that we have the ancestors
+        // if we have the descendants, as we are working on the assumption that we only add block
+        // if they are connected to the canonical chain.
+        let (finalized, safe, head) = match (finalized_header_res, safe_header_res, head_header_res)
         {
-            Ok(Some(header)) => header,
-            Ok(None) => {
-                warn!("[Engine - ForkChoiceUpdatedV3] Fork choice head block not found in store (hash {}).", self.fork_choice_state.head_block_hash);
+            (None, Some(_), _) => return invalid_fork_choice_state(),
+            (_, None, Some(_)) => return invalid_fork_choice_state(),
+            (Some(f), Some(s), Some(h)) => (f, s, h),
+            _ => {
+                warn!("[Engine - ForkChoiceUpdatedV3] Fork choice block not found in store (hash {}).", self.fork_choice_state.head_block_hash);
                 return syncing_response();
             }
-            _ => return Err(RpcErr::Internal),
         };
 
-        // We are still under the assumption that the blocks are only added if they are connected
-        // to the canonical chain. That means that for the state to be consistent we only need to
-        // check that the safe and finalized ones are in the canonical chain and that the heads parent is too.
+        // Check that the headers are in the correct order.
+        if finalized.number > safe.number || safe.number > head.number {
+            return invalid_fork_choice_state();
+        }
 
-        let head_valid = is_canonical(&storage, header.number - 1, header.parent_hash)
-            .map_err(|_| RpcErr::Internal)?;
-
-        let safe_valid = is_canonical(
+        // We look if each of the key blocks is canonical in our chain. If two of them are, we already know they are connected and can
+        // skip the check.
+        let finalized_canonical = is_canonical(
             &storage,
-            safe_block_number,
-            self.fork_choice_state.safe_block_hash,
-        )
-        .map_err(|_| RpcErr::Internal)?;
-
-        let finalized_valid = is_canonical(
-            &storage,
-            finalized_block_number,
+            finalized.number,
             self.fork_choice_state.finalized_block_hash,
-        )
-        .map_err(|_| RpcErr::Internal)?;
+        )?;
+        let safe_canonical = is_canonical(
+            &storage,
+            safe.number,
+            self.fork_choice_state.safe_block_hash,
+        )?;
+        let head_canonical = is_canonical(
+            &storage,
+            head.number,
+            self.fork_choice_state.head_block_hash,
+        )?;
 
-        if head_valid && safe_valid && finalized_valid {
-            storage.set_canonical_block(header.number, self.fork_choice_state.head_block_hash)?;
-            storage.update_finalized_block_number(finalized_block_number)?;
-            storage.update_safe_block_number(safe_block_number)?;
-            syncing_response()
+        // Ancestry checks if for the necessary. Empty ancestries will mean that they don't need to be updated.
+        let head_ancestry = if head_canonical && safe_canonical {
+            Some(Vec::new())
         } else {
-            invalid_fork_choice_state()
+            find_ancestry(&storage, &safe, &head).map_err(|_| RpcErr::Internal)?
+        };
+
+        let safe_ancestry = if safe_canonical && finalized_canonical {
+            Some(Vec::new())
+        } else {
+            find_ancestry(&storage, &finalized, &safe).map_err(|_| RpcErr::Internal)?
+        };
+
+        match (head_ancestry, safe_ancestry) {
+            (Some(ha), Some(sa)) => {
+                for (number, hash) in sa {
+                    storage.set_canonical_block(number, hash)?;
+                }
+
+                for (number, hash) in ha {
+                    storage.set_canonical_block(number, hash)?;
+                }
+
+                storage.set_canonical_block(head.number, self.fork_choice_state.head_block_hash)?;
+                storage.set_canonical_block(safe.number, self.fork_choice_state.safe_block_hash)?;
+                storage.set_canonical_block(
+                    finalized.number,
+                    self.fork_choice_state.finalized_block_hash,
+                )?;
+
+                storage.update_finalized_block_number(finalized.number)?;
+                storage.update_safe_block_number(safe.number)?;
+                syncing_response()
+            }
+            _ => invalid_fork_choice_state(),
         }
     }
 }
@@ -96,6 +128,60 @@ fn syncing_response() -> Result<Value, RpcErr> {
 fn invalid_fork_choice_state() -> Result<Value, RpcErr> {
     serde_json::to_value(json!({"error": {"code": -38002, "message": "Invalid forkchoice state"}}))
         .map_err(|_| RpcErr::Internal)
+}
+
+// Find branch of the blockchain connecting two blocks. If the blocks are connected through
+// parent hashes, then a vector of number-hash pairs is returned for the branch. If they are not
+// connected, an error is returned.
+fn find_ancestry(
+    storage: &Store,
+    ancestor: &BlockHeader,
+    descendant: &BlockHeader,
+) -> Result<Option<Vec<(BlockNumber, BlockHash)>>, StoreError> {
+    let mut block_number = descendant.number;
+    let mut found = false;
+    let descendant_hash = descendant.compute_block_hash();
+    let ancestor_hash = ancestor.compute_block_hash();
+    let mut header = descendant.clone();
+    let mut branch = Vec::new();
+
+    if ancestor.number == descendant.number {
+        if ancestor_hash == descendant_hash {
+            return Ok(Some(branch));
+        } else {
+            return Ok(None);
+        }
+    }
+
+    while block_number > ancestor.number && !found {
+        block_number -= 1;
+        let parent_hash = header.parent_hash;
+
+        // Check that the parent exists.
+        let parent_header = match storage.get_block_header_by_hash(parent_hash) {
+            Ok(Some(header)) => header,
+            Ok(None) => return Ok(None),
+            Err(error) => return Err(error),
+        };
+
+        if block_number == ancestor.number {
+            if ancestor_hash == descendant_hash {
+                found = true;
+            } else {
+                return Ok(None);
+            }
+        } else {
+            branch.push((block_number, parent_hash));
+        }
+
+        header = parent_header;
+    }
+
+    if found {
+        Ok(Some(branch))
+    } else {
+        Ok(None)
+    }
 }
 
 fn is_canonical(
