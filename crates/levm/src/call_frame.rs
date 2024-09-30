642fba9678615b017bc32e9bc79bcab3864091e9
iinaki <llorensinaki22@gmail.com>
2024-09-30 11:27:41 -0300

    Merge with main

--- crates/levm/src/call_frame.rs
+++ crates/levm/src/call_frame.rs
@@ -20,7 +20,7 @@ pub struct CallFrame {
     pub stack: Vec<U256>, // max 1024 in the future
     pub memory: Memory,
     pub calldata: Memory,
-    pub return_data: Memory,
+    pub returndata: Memory,
     // where to store return data of subcall
     pub return_data_offset: Option<usize>,
     pub return_data_size: Option<usize>,
