abc29b5a3662051c4af8dd608c0ccafc8a76c80d
Manuel Iñaki Bilbao <manuel.bilbao@lambdaclass.com>
2024-10-04 15:44:03 -0300

    Refactor send_raw_transaction

--- crates/l2/rpc/l1_rpc.rs
+++ crates/l2/rpc/l1_rpc.rs
@@ -1,7 +1,5 @@
-use std::str::FromStr;
-
 use ethereum_rust_core::types::EIP1559Transaction;
-use ethereum_rust_rlp::{encode::RLPEncode, structs::Encoder};
+use ethereum_rust_rlp::encode::RLPEncode;
 use ethereum_rust_rpc::{
     types::receipt::RpcLog,
     utils::{RpcErrorResponse, RpcRequest, RpcRequestId, RpcSuccessResponse},
@@ -12,6 +10,8 @@ use libsecp256k1::{sign, Message, SecretKey};
 use reqwest::Client;
 use serde::Deserialize;
 use serde_json::json;
+
+use super::transaction::PayloadRLPEncode;
 
 #[derive(Deserialize)]
 #[serde(untagged)]
@@ -46,53 +46,28 @@ impl L1Rpc {
             .await
     }
 
-    pub async fn send_raw_transaction(&self, tx_type: u8, data: &[u8]) -> Result<H256, String> {
+    pub async fn send_raw_transaction(&self, data: &[u8]) -> Result<H256, String> {
         let request = RpcRequest {
             id: RpcRequestId::Number(1),
             jsonrpc: "2.0".to_string(),
             method: "eth_sendRawTransaction".to_string(),
-            params: Some(vec![json!(
-                "0x".to_string() + &hex::encode([tx_type]) + &hex::encode(&data)
-            )]),
+            params: Some(vec![json!("0x".to_string() + &hex::encode(&data))]),
         };
 
-        match self
-            .send_request(request)
-            .await
-            .map_err(|e| e.to_string())?
-        {
-            RpcResponse::Success(response) => match response.result.as_str() {
-                Some(result) => Ok(H256::from_str(&result).unwrap()),
-                None => Err("Unexpected response format".to_string()),
-            },
-            RpcResponse::Error(error) => Err(error.error.message),
+        match self.send_request(request).await {
+            Ok(RpcResponse::Success(result)) => Ok(serde_json::from_value(result.result).unwrap()),
+            Ok(RpcResponse::Error(e)) => Err(e.error.message),
+            Err(e) => Err(e.to_string()),
         }
-    }
-
-    fn rlp_encode_payload(tx: &EIP1559Transaction) -> Vec<u8> {
-        let mut buf = Vec::new();
-        Encoder::new(&mut buf)
-            .encode_field(&tx.chain_id)
-            .encode_field(&tx.nonce)
-            .encode_field(&tx.max_priority_fee_per_gas)
-            .encode_field(&tx.max_fee_per_gas)
-            .encode_field(&tx.gas_limit)
-            .encode_field(&tx.to)
-            .encode_field(&tx.value)
-            .encode_field(&tx.data)
-            .encode_field(&tx.access_list)
-            .finish();
-        buf
     }
 
     pub async fn send_eip1559_transaction(
         &self,
-        tx: EIP1559Transaction,
+        mut tx: EIP1559Transaction,
         private_key: SecretKey,
     ) -> Result<H256, String> {
-        let mut tx = tx.clone();
         let mut payload = vec![EIP1559_TX_TYPE];
-        payload.append(&mut Self::rlp_encode_payload(&tx));
+        payload.append(tx.encode_payload_to_vec().as_mut());
 
         let data = Message::parse(&keccak(payload).0);
         let signature = sign(&data, &private_key);
@@ -104,8 +79,10 @@ impl L1Rpc {
         let mut encoded_tx = Vec::new();
         tx.encode(&mut encoded_tx);
 
-        self.send_raw_transaction(EIP1559_TX_TYPE, encoded_tx.as_slice())
-            .await
+        let mut data = vec![EIP1559_TX_TYPE];
+        data.append(&mut encoded_tx);
+
+        self.send_raw_transaction(data.as_slice()).await
     }
 
     pub async fn get_block_number(&self) -> Result<U256, String> {
