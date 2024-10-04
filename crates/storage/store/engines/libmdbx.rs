01291f4b7c1380d07d49fd60ae0ccad69eebf5f4
fmoletta <99273364+fmoletta@users.noreply.github.com>
2024-10-04 15:50:03 -0300

    feat: add mempool txs to payload (#575)
    **Motivation**
    Fetching transactions from the mempool during the block building process
    (engine_GetPayload).
    This code can and should be improved in the future, but it sets a basis
    for block building
    <!-- Why does this pull request exist? What are its goals? -->
    
    **Description**
    * Add method `build_payload`, which fetches transactions from the
    mempool and adds them to the payload.
    * Add a way to remove and filter transactions from the mempool
    * (Misc) Simplify state_trie related `StoreEngine` api. The
    `StoreEngine` now only creates the trie, leaving block and state root
    fetching to the `Store`. This removes duplicated code
    
    LEFTOVER WORK:
    * Save timestamp when a tx is added to the mempool
    * process blobs in block building pipeline (we are now ignoring them)
    
    <!-- A clear and concise general description of the changes this PR
    introduces -->
    
    <!-- Link to issues: Resolves #111, Resolves #222 -->
    
    TESTING_STATUS:
    The pipeline for block_building tests is ForkChoiceUpdated ->
    (SendRawTransaction) -> GetPayload -> NewPayload -> (Fetch
    blocks/receipts).
    Tests are currently failing at NewPayload because of different blob
    versioned hashes.
    
    Closes None, but is part of #344

--- crates/storage/store/engines/libmdbx.rs
+++ crates/storage/store/engines/libmdbx.rs
@@ -20,6 +20,7 @@ use libmdbx::{
     table_info,
 };
 use serde_json;
+use std::collections::HashMap;
 use std::fmt::{Debug, Formatter};
 use std::path::Path;
 use std::sync::Arc;
@@ -49,6 +50,17 @@ impl Store {
     fn read<T: Table>(&self, key: T::Key) -> Result<Option<T::Value>, StoreError> {
         let txn = self.db.begin_read().map_err(StoreError::LibmdbxError)?;
         txn.get::<T>(key).map_err(StoreError::LibmdbxError)
+    }
+
+    // Helper method to remove from a libmdbx table
+    fn remove<T: Table>(&self, key: T::Key) -> Result<(), StoreError> {
+        let txn = self
+            .db
+            .begin_readwrite()
+            .map_err(StoreError::LibmdbxError)?;
+        txn.delete::<T>(key, None)
+            .map_err(StoreError::LibmdbxError)?;
+        txn.commit().map_err(StoreError::LibmdbxError)
     }
 
     fn get_block_hash_by_block_number(
@@ -215,6 +227,35 @@ impl StoreEngine for Store {
         Ok(self.read::<TransactionPool>(hash.into())?.map(|t| t.to()))
     }
 
+    fn remove_transaction_from_pool(&self, hash: H256) -> Result<(), StoreError> {
+        self.remove::<TransactionPool>(hash.into())
+    }
+
+    fn filter_pool_transactions(
+        &self,
+        filter: &dyn Fn(&Transaction) -> bool,
+    ) -> Result<HashMap<Address, Vec<Transaction>>, StoreError> {
+        let cursor = self
+            .db
+            .begin_read()
+            .map_err(StoreError::LibmdbxError)?
+            .cursor::<TransactionPool>()
+            .map_err(StoreError::LibmdbxError)?;
+        let mut tx_iter = cursor.walk(None);
+        let mut txs_by_sender: HashMap<Address, Vec<Transaction>> = HashMap::new();
+        while let Some(Ok((_, tx))) = tx_iter.next() {
+            let tx = tx.to();
+            if filter(&tx) {
+                // Txs are stored in the DB by order of insertion so they should be innately stored by nonce
+                txs_by_sender
+                    .entry(tx.sender())
+                    .or_default()
+                    .push(tx.clone())
+            }
+        }
+        Ok(txs_by_sender)
+    }
+
     /// Stores the chain config serialized as json
     fn set_chain_config(&self, chain_config: &ChainConfig) -> Result<(), StoreError> {
         self.write::<ChainData>(
@@ -336,30 +377,17 @@ impl StoreEngine for Store {
         }
     }
 
-    fn state_trie(&self, block_hash: BlockHash) -> Result<Option<Trie>, StoreError> {
-        let Some(state_root) = self
-            .get_block_header_by_hash(block_hash)?
-            .map(|h| h.state_root)
-        else {
-            return Ok(None);
-        };
-        let db = Box::new(LibmdbxTrieDB::<StateTrieNodes>::new(self.db.clone()));
-        let trie = Trie::open(db, state_root);
-        Ok(Some(trie))
-    }
-
-    fn new_state_trie(&self) -> Result<Trie, StoreError> {
-        let db = Box::new(LibmdbxTrieDB::<StateTrieNodes>::new(self.db.clone()));
-        let trie = Trie::new(db);
-        Ok(trie)
-    }
-
     fn open_storage_trie(&self, address: Address, storage_root: H256) -> Trie {
         let db = Box::new(LibmdbxDupsortTrieDB::<StorageTriesNodes, [u8; 20]>::new(
             self.db.clone(),
             address.0,
         ));
         Trie::open(db, storage_root)
+    }
+
+    fn open_state_trie(&self, state_root: H256) -> Trie {
+        let db = Box::new(LibmdbxTrieDB::<StateTrieNodes>::new(self.db.clone()));
+        Trie::open(db, state_root)
     }
 
     fn set_canonical_block(&self, number: BlockNumber, hash: BlockHash) -> Result<(), StoreError> {
