802922cab3dbacec18d2b2e530b366a460de86cf
Ivan Litteri <67517699+ilitteri@users.noreply.github.com>
2024-10-07 15:59:27 -0300

    refactor(`RpcErr`): add context to RPC errors (#718)
    **Motivation**
    
    <!-- Why does this pull request exist? What are its goals? -->
    
    RPC errors are usually inherited from other errors and during such
    mapping, the context of the original error is lost, making it difficult
    to debug, and impossible in some cases.
    
    **Description**
    
    <!-- A clear and concise general description of the changes this PR
    introduces -->
    
    In this PR I propose the addition of contexts as a `String`s to the RPC
    errors.

--- crates/networking/rpc/engine/payload.rs
+++ crates/networking/rpc/engine/payload.rs
@@ -25,9 +25,11 @@ pub struct GetPayloadV3Request {
 
 impl RpcHandler for NewPayloadV3Request {
     fn parse(params: &Option<Vec<Value>>) -> Result<Self, RpcErr> {
-        let params = params.as_ref().ok_or(RpcErr::BadParams)?;
+        let params = params
+            .as_ref()
+            .ok_or(RpcErr::BadParams("No params provided".to_owned()))?;
         if params.len() != 3 {
-            return Err(RpcErr::BadParams);
+            return Err(RpcErr::BadParams("Expected 3 params".to_owned()));
         }
         Ok(NewPayloadV3Request {
             payload: serde_json::from_value(params[0].clone())?,
@@ -48,7 +50,8 @@ impl RpcHandler for NewPayloadV3Request {
             Ok(block) => block,
             Err(error) => {
                 let result = PayloadStatus::invalid_with_err(&error.to_string());
-                return serde_json::to_value(result).map_err(|_| RpcErr::Internal);
+                return serde_json::to_value(result)
+                    .map_err(|error| RpcErr::Internal(error.to_string()));
             }
         };
 
@@ -56,15 +59,17 @@ impl RpcHandler for NewPayloadV3Request {
 
         // Check timestamp does not fall within the time frame of the Cancun fork
         let chain_config = storage.get_chain_config()?;
-        if chain_config.get_fork(block.header.timestamp) < Fork::Cancun {
-            return Err(RpcErr::UnsuportedFork);
+        let current_fork = chain_config.get_fork(block.header.timestamp);
+        if current_fork < Fork::Cancun {
+            return Err(RpcErr::UnsuportedFork(format!("{current_fork:?}")));
         }
 
         // Check that block_hash is valid
         let actual_block_hash = block.header.compute_block_hash();
         if block_hash != actual_block_hash {
             let result = PayloadStatus::invalid_with_err("Invalid block hash");
-            return serde_json::to_value(result).map_err(|_| RpcErr::Internal);
+            return serde_json::to_value(result)
+                .map_err(|error| RpcErr::Internal(error.to_string()));
         }
         info!("Block hash {block_hash} is valid");
         // Concatenate blob versioned hashes lists (tx.blob_versioned_hashes) of each blob transaction included in the payload, respecting the order of inclusion
@@ -77,28 +82,36 @@ impl RpcHandler for NewPayloadV3Request {
             .collect();
         if self.expected_blob_versioned_hashes != blob_versioned_hashes {
             let result = PayloadStatus::invalid_with_err("Invalid blob_versioned_hashes");
-            return serde_json::to_value(result).map_err(|_| RpcErr::Internal);
+            return serde_json::to_value(result)
+                .map_err(|error| RpcErr::Internal(error.to_string()));
         }
         // Check that the incoming block extends the current chain
-        let last_block_number = storage.get_latest_block_number()?.ok_or(RpcErr::Internal)?;
+        let last_block_number = storage.get_latest_block_number()?.ok_or(RpcErr::Internal(
+            "Could not get latest block number".to_owned(),
+        ))?;
         if block.header.number <= last_block_number {
             // Check if we already have this block stored
             if storage
                 .get_block_number(block_hash)
-                .map_err(|_| RpcErr::Internal)?
+                .map_err(|error| RpcErr::Internal(error.to_string()))?
                 .is_some_and(|num| num == block.header.number)
             {
                 let result = PayloadStatus::valid_with_hash(block_hash);
-                return serde_json::to_value(result).map_err(|_| RpcErr::Internal);
+                return serde_json::to_value(result)
+                    .map_err(|error| RpcErr::Internal(error.to_string()));
             }
             warn!("Should start reorg but it is not supported yet");
-            return Err(RpcErr::Internal);
+            return Err(RpcErr::Internal(
+                "Block reorg is not supported yet".to_owned(),
+            ));
         } else if block.header.number != last_block_number + 1 {
             let result = PayloadStatus::syncing();
-            return serde_json::to_value(result).map_err(|_| RpcErr::Internal);
+            return serde_json::to_value(result)
+                .map_err(|error| RpcErr::Internal(error.to_string()));
         }
 
-        let latest_valid_hash = latest_valid_hash(&storage).map_err(|_| RpcErr::Internal)?;
+        let latest_valid_hash =
+            latest_valid_hash(&storage).map_err(|error| RpcErr::Internal(error.to_string()))?;
 
         // Execute and store the block
         info!("Executing payload with block hash: {block_hash}");
@@ -120,7 +133,7 @@ impl RpcHandler for NewPayloadV3Request {
             }
             Err(ChainError::StoreError(error)) => {
                 warn!("Error storing block: {error}");
-                Err(RpcErr::Internal)
+                Err(RpcErr::Internal(error.to_string()))
             }
             Ok(()) => {
                 info!("Block with hash {block_hash} executed succesfully");
@@ -134,18 +147,22 @@ impl RpcHandler for NewPayloadV3Request {
             }
         }?;
 
-        serde_json::to_value(payload_status).map_err(|_| RpcErr::Internal)
+        serde_json::to_value(payload_status).map_err(|error| RpcErr::Internal(error.to_string()))
     }
 }
 
 impl RpcHandler for GetPayloadV3Request {
     fn parse(params: &Option<Vec<Value>>) -> Result<Self, RpcErr> {
-        let params = params.as_ref().ok_or(RpcErr::BadParams)?;
+        let params = params
+            .as_ref()
+            .ok_or(RpcErr::BadParams("No params provided".to_owned()))?;
         if params.len() != 1 {
-            return Err(RpcErr::BadParams);
+            return Err(RpcErr::BadParams("Expected 1 param".to_owned()));
         };
         let Ok(hex_str) = serde_json::from_value::<String>(params[0].clone()) else {
-            return Err(RpcErr::BadParams);
+            return Err(RpcErr::BadParams(
+                "Expected param to be a string".to_owned(),
+            ));
         };
         // Check that the hex string is 0x prefixed
         let Some(hex_str) = hex_str.strip_prefix("0x") else {
@@ -161,13 +178,17 @@ impl RpcHandler for GetPayloadV3Request {
     fn handle(&self, storage: Store) -> Result<Value, RpcErr> {
         info!("Requested payload with id: {:#018x}", self.payload_id);
         let Some(mut payload) = storage.get_payload(self.payload_id)? else {
-            return Err(RpcErr::UnknownPayload);
+            return Err(RpcErr::UnknownPayload(format!(
+                "Payload with id {:#018x} not found",
+                self.payload_id
+            )));
         };
-        let block_value = build_payload(&mut payload, &storage).map_err(|_| RpcErr::Internal)?;
+        let block_value = build_payload(&mut payload, &storage)
+            .map_err(|error| RpcErr::Internal(error.to_string()))?;
         serde_json::to_value(ExecutionPayloadResponse::new(
             ExecutionPayloadV3::from_block(payload),
             block_value,
         ))
-        .map_err(|_| RpcErr::Internal)
+        .map_err(|error| RpcErr::Internal(error.to_string()))
     }
 }
