02619c9b9542bdecc9dddbbd73a310850ca84932
fborello-lambda <federicoborello@lambdaclass.com>
2024-10-07 19:10:46 -0300

    add diferent program

--- crates/l2/prover/zk_prover.rs
+++ crates/l2/prover/zk_prover.rs
@@ -7,31 +7,88 @@ use sp1_sdk::{ProverClient, SP1ProofWithPublicValues, SP1ProvingKey, SP1Stdin, S
 use ethereum_rust_rlp::encode::RLPEncode;
 
 /// The ELF (executable and linkable format) file for the Succinct RISC-V zkVM.
-pub const FIBONACCI_ELF: &[u8] = include_bytes!("./sp1/program/elf/riscv32im-succinct-zkvm-elf");
+pub const VERIFICATION_ELF: &[u8] =
+    include_bytes!("./sp1/verification_program/elf/riscv32im-succinct-zkvm-elf");
+pub const EXECUTION_ELF: &[u8] =
+    include_bytes!("./sp1/execution_program/elf/riscv32im-succinct-zkvm-elf");
 
 pub struct Prover {
     client: ProverClient,
     pk: SP1ProvingKey,
     vk: SP1VerifyingKey,
+    mode: ProverMode,
+}
+
+pub enum ProverMode {
+    Verification,
+    Execution,
 }
 
 impl Default for Prover {
     fn default() -> Self {
-        Self::new()
+        Self::new_verify()
     }
 }
 
 impl Prover {
-    pub fn new() -> Self {
-        info!("Setting up prover...");
+    pub fn new_verify() -> Self {
+        info!("Setting up Verification Prover...");
         let client = ProverClient::new();
-        let (pk, vk) = client.setup(FIBONACCI_ELF);
-        info!("Prover setup complete!");
+        let (pk, vk) = client.setup(VERIFICATION_ELF);
+        info!("Verification Prover setup complete!");
 
-        Self { client, pk, vk }
+        Self { client, pk, vk, ProverMode::Verification}
     }
 
-    pub fn prove(&self, input: ProverInputNoExecution) -> Result<SP1ProofWithPublicValues, String> {
+    pub fn new_execution() -> Self {
+        info!("Setting up Verification Prover...");
+        let client = ProverClient::new();
+        let (pk, vk) = client.setup(VERIFICATION_ELF);
+        info!("Verification Prover setup complete!");
+
+        Self { client, pk, vk, ProverMode::Execution }
+    }
+
+    pub fn prove_verification(
+        &self,
+        input: ProverInputNoExecution,
+    ) -> Result<SP1ProofWithPublicValues, String> {
+        let head_block_rlp = input.head_block.encode_to_vec();
+        let parent_block_header_rlp = input.parent_block_header.encode_to_vec();
+
+        // Setup the inputs.
+        let mut stdin = SP1Stdin::new();
+
+        stdin.write(&head_block_rlp);
+        stdin.write(&parent_block_header_rlp);
+
+        info!(
+            "Starting block execution proof for block = {:?}",
+            input.head_block
+        );
+
+        // Generate the proof
+        let proof = self
+            .client
+            .prove(&self.pk, stdin)
+            .groth16()
+            .run()
+            .map_err(|_| "Failed to generate proof".to_string())?;
+
+        info!("Successfully generated proof!");
+
+        // Verify the proof.
+        self.client
+            .verify(&proof, &self.vk)
+            .map_err(|_| "Failed to verify proof".to_string())?;
+        info!("Successfully verified proof!");
+
+        Ok(proof)
+    }
+    pub fn prove_execution(
+        &self,
+        input: ProverInputNoExecution,
+    ) -> Result<SP1ProofWithPublicValues, String> {
         let head_block_rlp = input.head_block.encode_to_vec();
         let parent_block_header_rlp = input.parent_block_header.encode_to_vec();
 
