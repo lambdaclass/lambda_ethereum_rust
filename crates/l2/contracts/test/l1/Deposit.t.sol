7791a8dcd374ebb9652deb199ee913a85943d12d
Ivan Litteri <67517699+ilitteri@users.noreply.github.com>
2024-10-09 15:22:25 -0300

    feat(L2): deposits support (#734)
    **Motivation**
    
    <!-- Why does this pull request exist? What are its goals? -->
    
    For users to interact within the L2 we need a way for them to deposit
    assets.
    
    **Description**
    
    <!-- A clear and concise general description of the changes this PR
    introduces -->
    
    TLDR: This PR adds the necessary implementation to support L1->L2 user
    deposits.
    
    - [Update CommonBridge deposit
    impl](https://github.com/lambdaclass/ethereum_rust/commit/0d23c312bf826ea5e8c5c4e61e28ceaa17f7321e)
    - [Adapts
    L1Watcher](https://github.com/lambdaclass/ethereum_rust/commit/53db9bdeb2ed13ba43f1891b66dcac87ab69c5de)
    - [Add modified evm handler functions to support
    deposits](https://github.com/lambdaclass/ethereum_rust/commit/04d7326496d1ae8b3a2a275034d2c0189e61835c)
    
    **TODOs**
    - The deposit transaction should be prepared in the `DepositInitiated`
    event.
    - The transaction gas cost should be part of the value to mint, to
    ensure that the user gets the deposited amount and not less than that.
    Another thing that we can do is to subsidize deposit transactions to
    avoid this problem.
    
    ## Try it yourself
    
    1. Initialize the stack in one console
        ```
        ethereum_rust_l2 stack init
        ```
    2. Transfer money to the bridge
        ```
    cast send --private-key
    0x385c546456b6a603a1cfcaa9ec9494ba4832da08dd6bcf4de9a71e4a01b74924
    --value 1000000000000000000 0x90cD151b6e9500F13240dF68673f3B81245D57d4
    --rpc-url http://localhost:8545
        ```
        You should quickly see a `Mint transaction added to mempool` log.
    3. Wait to see the above log and wait for the next block to be produced
    and then ask for the account balance in L2
        ```
        curl --request POST \
          --url http://localhost:1729 \
          --header 'Content-Type: application/json' \
          --data '{
          "jsonrpc": "2.0",
          "id": 1,
          "method": "eth_getBalance",
          "params": ["0x3d1e15a1a55578f7c920884a9943b3b35d0d885b", "latest"]
        }'
        ```
    
    ---------
    
    Co-authored-by: Manuel Iñaki Bilbao <manuel.bilbao@lambdaclass.com>

--- crates/l2/contracts/test/l1/Deposit.t.sol
+++ crates/l2/contracts/test/l1/Deposit.t.sol
@@ -5,19 +5,32 @@ pragma solidity 0.8.27;
 import {CommonBridgeTest} from "./CommonBridge.t.sol";
 
 contract DepositTest is CommonBridgeTest {
-    event DepositInitiated(bytes32 indexed l2MintTxHash);
+    event DepositInitiated(uint256 indexed amount, address indexed to, bytes32 indexed l2MintTxHash);
 
     error AmountToDepositIsZero();
 
     function test_cannotDepositWithAmountZero() public {
         vm.expectRevert(abi.encodePacked(AmountToDepositIsZero.selector));
-        commonBridge.deposit{value: 0 ether}(alice, alice);
+        commonBridge.deposit{value: 0 ether}(alice);
     }
 
-    function test_depositSuccessfully() public {
+    function test_cannotDepositThroughEOATransferWithAmountZero() public {
+        vm.expectRevert(abi.encodePacked(AmountToDepositIsZero.selector));
+        commonBridge.deposit{value: 0 ether}(alice);
+    }
+
+    function test_canDeposit() public {
         vm.expectEmit(true, true, true, true, address(commonBridge));
-        emit DepositInitiated(dummyl2MintTxHash);
+        emit DepositInitiated(0.1 ether, alice, dummyl2MintTxHash);
         vm.deal(alice, 1 ether);
-        commonBridge.deposit{value: 0.1 ether}(alice, alice);
+        commonBridge.deposit{value: 0.1 ether}(alice);
+    }
+
+    function test_canDepositThroughEOATransfer() public {
+        vm.expectEmit(true, true, true, true, address(commonBridge));
+        emit DepositInitiated(0.1 ether, alice, dummyl2MintTxHash);
+        vm.deal(alice, 1 ether);
+        (bool success,) = address(commonBridge).call{value: 0.1 ether}("");
+        require(success, "Transfer failed");
     }
 }
