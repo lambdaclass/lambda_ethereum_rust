79db6d0dfecbb1d945d4a0f65c238ff44e67bf14
Ivan Litteri <67517699+ilitteri@users.noreply.github.com>
2024-10-09 17:50:40 -0300

    feat(L2): error handling (#802)
    **Motivation**
    
    <!-- Why does this pull request exist? What are its goals? -->
    
    Before moving to the next milestone we need to have proper error
    handling.
    
    **Description**
    
    <!-- A clear and concise general description of the changes this PR
    introduces -->
    
    Adds the following errors to represent the corresponding module errors:
    - `EthClientError`
    - `EngineClientError`
    - `ConfigError`
    - `L1WatcherError`
    - `ProofDataProviderError`
    - `OperatorError`
    
    ---------
    
    Co-authored-by: fmoletta <fedemoletta@hotmail.com>
    Co-authored-by: Manuel Bilbao <manuel.bilbao@lambdaclass.com>
    Co-authored-by: Manuel Iñaki Bilbao <bilbaomanuel98@gmail.com>

--- crates/l2/utils/config/mod.rs
+++ crates/l2/utils/config/mod.rs
@@ -1,7 +1,3 @@
-use keccak_hash::H256;
-use libsecp256k1::SecretKey;
-use serde::{Deserialize, Deserializer};
-
 pub mod engine_api;
 pub mod eth;
 pub mod l1_watcher;
@@ -9,10 +5,4 @@ pub mod operator;
 pub mod proof_data_provider;
 pub mod prover;
 
-fn secret_key_deserializer<'de, D>(deserializer: D) -> Result<SecretKey, D::Error>
-where
-    D: Deserializer<'de>,
-{
-    let hex = H256::deserialize(deserializer)?;
-    SecretKey::parse(hex.as_fixed_bytes()).map_err(serde::de::Error::custom)
-}
+pub mod errors;
