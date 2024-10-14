62d060623fac9b3843f82a709b85cecc7fc4f979
Manuel Iñaki Bilbao <manuel.bilbao@lambdaclass.com>
2024-10-14 12:13:01 -0300

    Load env vars from .env file

--- crates/l2/utils/config/errors.rs
+++ crates/l2/utils/config/errors.rs
@@ -2,4 +2,6 @@
 pub enum ConfigError {
     #[error("Error deserializing config from env: {0}")]
     ConfigDeserializationError(#[from] envy::Error),
+    #[error("Error reading env file: {0}")]
+    EnvFileError(#[from] std::io::Error),
 }
--- crates/l2/utils/config/mod.rs
+++ crates/l2/utils/config/mod.rs
@@ -1,3 +1,7 @@
+use std::io::BufRead;
+
+use tracing::debug;
+
 pub mod engine_api;
 pub mod eth;
 pub mod l1_watcher;
@@ -6,3 +10,20 @@ pub mod proof_data_provider;
 pub mod prover;
 
 pub mod errors;
+
+pub fn read_env_file() -> Result<(), errors::ConfigError> {
+    let env_file = std::fs::File::open(".env")?;
+    let reader = std::io::BufReader::new(env_file);
+
+    for line in reader.lines() {
+        match line?.split_once('=') {
+            Some((key, value)) => {
+                debug!("Setting env var from .env: {key}={value}");
+                std::env::set_var(key, value)
+            }
+            None => continue,
+        };
+    }
+
+    Ok(())
+}
