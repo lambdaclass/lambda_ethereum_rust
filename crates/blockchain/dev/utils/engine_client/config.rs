bada5a3d508a686529e00cc04b88cdcd0584c67b
ilitteri <ilitteri@fi.uba.ar>
2024-10-17 18:39:31 -0300

    Add `dev` bin to `blockchain` module

---
+++ crates/blockchain/dev/utils/engine_client/config.rs
@@ -0,0 +1,16 @@
+use crate::utils::engine_client::errors::ConfigError;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+pub struct EngineApiConfig {
+    pub rpc_url: String,
+    pub jwt_path: String,
+}
+
+impl EngineApiConfig {
+    pub fn from_env() -> Result<Self, ConfigError> {
+        envy::prefixed("ENGINE_API_")
+            .from_env::<Self>()
+            .map_err(ConfigError::from)
+    }
+}
