a5c7ed010a49399ddf7a5d8ff5e2f6d096e0eea4
Ivan Litteri <67517699+ilitteri@users.noreply.github.com>
2024-10-18 13:04:49 -0300

    refactor(l2): replace placeholder names  (#901)
    **Motivation**
    
    <!-- Why does this pull request exist? What are its goals? -->
    
    To replace placeholder components/contracts names
    
    **Description**
    
    <!-- A clear and concise general description of the changes this PR
    introduces -->
    
    - Renames `ProofDataClient` -> `ProverClient`.
    - Renames `ProofDataProvider` -> `ProverServer`.
    - Renames `ProofDataClientConfig` -> `ProverClientConfig`.
    - Renames `ProverConfig` -> `ProverServerConfig`.
    - Renames `BlockExecutor` -> `OnChainOperator`.

--- crates/l2/contracts/src/l1/OnChainOperator.sol
+++ crates/l2/contracts/src/l1/OnChainOperator.sol
@@ -1,17 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.27;
 
-import "./interfaces/IBlockExecutor.sol";
+import "./interfaces/IOnChainOperator.sol";
 
-/// @title BlockExecutor contract.
+/// @title OnChainOperator contract.
 /// @author LambdaClass
-contract BlockExecutor is IBlockExecutor {
-    /// @inheritdoc IBlockExecutor
+contract OnChainOperator is IOnChainOperator {
+    /// @inheritdoc IOnChainOperator
     function commit(bytes32 currentBlockCommitment) external override {
         emit BlockCommitted(currentBlockCommitment);
     }
 
-    /// @inheritdoc IBlockExecutor
+    /// @inheritdoc IOnChainOperator
     function verify(bytes calldata blockProof) external override {
         bytes32 blockHash = keccak256(abi.encode(blockProof));
         emit BlockVerified(blockHash);
