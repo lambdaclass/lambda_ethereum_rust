ed65d52d3595d26dbd42aef96f49c98c43ba100b
Ivan Litteri <67517699+ilitteri@users.noreply.github.com>
2024-10-18 11:45:57 -0300

    feat(l2): use `ethereum_rust` dev as local L1 (#890)
    **Motivation**
    
    <!-- Why does this pull request exist? What are its goals? -->
    
    We need a special setup of this client for users to play around with a
    local L1 devnet because regularly the execution client needs a consensus
    counterpart to advance blocks. For example, when initializing a local
    L2, we use a local L1 devnet that continuously produces blocks. Today
    we're using `reth` but want to replace it asap.
    
    **Description**
    
    <!-- A clear and concise general description of the changes this PR
    introduces -->
    
    - Adds a `dev` (`ethereum_rust-dev`) crate with
    - an `engine_client` implementation. An `EngineClient` is an HTTP client
    that supports performing calls to the EngineAPI. It is used for
    "advancing the blockchain".
    - a `block_producer` module with the logic that makes the blockchain
    "advance".
    - `ethereum_rust-dev` is added as an optional dependency in
    `cmd/ethereum_rust` and is only used under the `dev` feature flag, which
    starts the block production if set.
    - Adds a Dockerfile for running a `dev` client. This is useful for
    tracking the client logs, as most L2s are run by a single command, and
    if the client is run locally, its logs would interfere with the L2 ones.
    
    **Side notes**
    
    - I needed to change `eth_gasPrice` behavior a little bit. Before, we
    were returning an error when someone requested the `eth_gasPrice` and
    the only block in the blockchain was the genesis. Now that we can start
    the client in a dev environment starting with only the genesis block,
    we've updated the response to reply with the minimum gas price.

---
+++ crates/blockchain/dev/block_producer.rs
@@ -0,0 +1,88 @@
+use crate::utils::engine_client::EngineClient;
+use bytes::Bytes;
+use ethereum_rust_rpc::types::fork_choice::{ForkChoiceState, PayloadAttributesV3};
+use ethereum_types::H256;
+use std::{
+    net::SocketAddr,
+    time::{SystemTime, UNIX_EPOCH},
+};
+
+pub async fn start_block_producer(
+    execution_client_auth_url: SocketAddr,
+    jwt_secret: Bytes,
+    head_block_hash: H256,
+    max_tries: u32,
+) {
+    let engine_client =
+        EngineClient::new(&format!("http://{execution_client_auth_url}"), jwt_secret);
+
+    let mut head_block_hash: H256 = head_block_hash;
+    let mut tries = 0;
+    while tries < max_tries {
+        tracing::info!("Producing block");
+        let fork_choice_state = ForkChoiceState {
+            head_block_hash,
+            safe_block_hash: head_block_hash,
+            finalized_block_hash: head_block_hash,
+        };
+        let payload_attributes = PayloadAttributesV3 {
+            timestamp: SystemTime::now()
+                .duration_since(UNIX_EPOCH)
+                .expect("Failed to produce block: error getting current timestamp")
+                .as_secs(),
+            ..Default::default()
+        };
+        let fork_choice_response = match engine_client
+            .engine_forkchoice_updated_v3(fork_choice_state, Some(payload_attributes))
+            .await
+        {
+            Ok(response) => response,
+            Err(error) => {
+                tracing::error!(
+                    "Failed to produce block: error sending engine_forkchoiceUpdateV3 with PayloadAttributes: {error}"
+                );
+                tries += 1;
+                continue;
+            }
+        };
+        let payload_id = fork_choice_response
+            .payload_id
+            .expect("Failed to produce block: payload_id is None in ForkChoiceResponse");
+        let execution_payload_response = match engine_client.engine_get_payload_v3(payload_id).await
+        {
+            Ok(response) => response,
+            Err(error) => {
+                tracing::error!(
+                    "Failed to produce block: error sending engine_getPayloadV3: {error}"
+                );
+                tries += 1;
+                continue;
+            }
+        };
+        let payload_status = match engine_client
+            .engine_new_payload_v3(
+                execution_payload_response.execution_payload,
+                Default::default(),
+                Default::default(),
+            )
+            .await
+        {
+            Ok(response) => response,
+            Err(error) => {
+                tracing::error!(
+                    "Failed to produce block: error sending engine_newPayloadV3: {error}"
+                );
+                tries += 1;
+                continue;
+            }
+        };
+        let produced_block_hash = payload_status
+            .latest_valid_hash
+            .expect("Failed to produce block: latest_valid_hash is None in PayloadStatus");
+        tracing::info!("Produced block {produced_block_hash:#x}");
+
+        head_block_hash = produced_block_hash;
+
+        tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+    }
+}
