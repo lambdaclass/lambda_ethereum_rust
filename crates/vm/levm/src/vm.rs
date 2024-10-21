242aec1a263574c410033a967a896666d17599a3
JereSalo <jeresalo17@gmail.com>
2024-10-21 11:15:56 -0300

    add increase_consumed_gas function

--- crates/vm/levm/src/vm.rs
+++ crates/vm/levm/src/vm.rs
@@ -631,4 +631,14 @@ impl VM {
             code_size_in_memory,
         )
     }
+    
+    /// Increases gas consumption of CallFrame and Environment, returning an error if the callframe gas limit is reached.
+    pub fn increase_consumed_gas(&mut self, current_call_frame: &mut CallFrame, gas: U256) -> Result<(), VMError>{
+        if current_call_frame.gas_used + gas > current_call_frame.gas_limit {
+            return Err(VMError::OutOfGas);
+        }
+        current_call_frame.gas_used += gas;
+        self.env.consumed_gas += gas;
+        Ok(())
+    }
 }
