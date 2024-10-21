5522b6bd2a96ac701ca8741bd2517218e06c12f9
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2024-10-21 18:09:28 -0300

    fix comment

--- crates/l2/prover/build.rs
+++ crates/l2/prover/build.rs
@@ -1,8 +1,6 @@
 use sp1_helper::build_program_with_args;
 
-// Builds the zkVM's program
-// The L1 docker container is building the prover's program
-// Using this variable to avoid compiling it, is it ok? 
+// Builds the zkVM's program if BUILD_ZKVM env variable is defined.
 fn main() {
     if std::env::var("BUILD_ZKVM").is_ok() {
         println!("Building ZKVM");
