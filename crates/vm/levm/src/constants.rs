ad41f0778846409d80e1332ba93f56551dcb28cd
maximopalopoli <mpalopoli@fi.uba.ar>
2024-10-21 18:00:24 -0300

    Delete most transaction constants

--- crates/vm/levm/src/constants.rs
+++ crates/vm/levm/src/constants.rs
@@ -111,11 +111,6 @@ pub const MEMORY_EXPANSION_QUOTIENT: usize = 512;
 
 // Transaction costs in gas (in wei)
 pub const TX_BASE_COST: U256 = U256([21000, 0, 0, 0]);
-pub const TX_DATA_COST_PER_NON_ZERO: u64 = 16;
-pub const TX_DATA_COST_PER_ZERO: u64 = 4;
-pub const TX_CREATE_COST: u64 = 32000;
-pub const TX_ACCESS_LIST_ADDRESS_COST: u64 = 2400;
-pub const TX_ACCESS_LIST_STORAGE_KEY_COST: u64 = 1900;
 
 pub const MAX_CODE_SIZE: usize = 0x6000;
 pub const MAX_CREATE_CODE_SIZE: usize = 2 * MAX_CODE_SIZE;
