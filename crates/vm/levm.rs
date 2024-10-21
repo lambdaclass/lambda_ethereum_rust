69bc8362e1c29922088b737110ec20d4add2bc31
Juani Medone <juan.medone@lambdaclass.com>
2024-10-21 18:37:08 -0300

    Integrate `EvmState`, `EvmError` and (partially) `TransactionReport`

--- crates/vm/levm.rs
+++ crates/vm/levm.rs
@@ -1,32 +1,79 @@
-use crate::{EvmError, EvmState};
+use crate::EvmError;
 use bytes::Bytes;
 use ethereum_rust_core::{
-    types::{Block, Receipt},
+    types::{Block, BlockHeader, Receipt, Transaction},
     Address,
 };
-use ethereum_rust_levm::vm::{Db, VM};
+use ethereum_rust_levm::{
+    report::TransactionReport,
+    report::VmError as LevmError,
+    vm::{Db, VM},
+};
 
-pub fn execute_block(block: &Block, _state: &mut EvmState) -> Result<Vec<Receipt>, EvmError> {
-    for transaction in block.body.transactions.iter() {
-        // TODO: check all the parameters
-        let mut vm = VM::new(
-            Address::random(),
-            transaction.sender(),
-            transaction.value(),
-            Bytes::new(),
-            block.header.gas_limit,
-            transaction.gas_limit().into(),
-            block.header.number.into(),
-            block.header.coinbase,
-            block.header.timestamp.into(),
-            Some(block.header.prev_randao),
-            transaction.chain_id().unwrap().into(),
-            block.header.base_fee_per_gas.unwrap().into(),
-            transaction.gas_price().into(),
-            Db::default(),
-        );
-        let _result = vm.transact();
+pub struct EvmState(Db);
+
+impl EvmState {
+    /// Get a reference to inner `Store` database
+    pub fn database(&self) -> &Db {
+        &self.0
     }
-    // TODO: map the result to the expected Result<Vec<Receipt>, EvmError>
-    Ok(Vec::new())
+}
+
+/// Executes all transactions in a block and returns their receipts.
+pub fn execute_block(block: &Block, state: &mut EvmState) -> Result<Vec<Receipt>, EvmError> {
+    let mut receipts = Vec::new();
+    let mut cumulative_gas_used = 0;
+    for tx in block.body.transactions.iter() {
+        let report = execute_tx(tx, &block.header, state)?;
+        cumulative_gas_used += report.gas_used;
+        let receipt = Receipt::new(
+            tx.tx_type(),
+            report.is_success(),
+            cumulative_gas_used,
+            report.logs,
+        );
+        receipts.push(receipt);
+    }
+    Ok(receipts)
+}
+
+// Executes a single tx, doesn't perform state transitions
+pub fn execute_tx(
+    tx: &Transaction,
+    block_header: &BlockHeader,
+    state: &mut EvmState,
+) -> Result<TransactionReport, EvmError> {
+    // TODO: check all the parameters
+    let mut vm = VM::new(
+        Address::random(),
+        tx.sender(),
+        tx.value(),
+        Bytes::new(),
+        block_header.gas_limit,
+        tx.gas_limit().into(),
+        block_header.number.into(),
+        block_header.coinbase,
+        block_header.timestamp.into(),
+        Some(block_header.prev_randao),
+        tx.chain_id().unwrap().into(),
+        block_header.base_fee_per_gas.unwrap().into(),
+        tx.gas_price().into(),
+        state.database().clone(), // shouldn't clone here
+    );
+    let _result = vm.transact();
+    Ok(TransactionReport {
+        result: todo!(),
+        new_state: todo!(),
+        gas_used: todo!(),
+        gas_refunded: todo!(),
+        output: todo!(),
+        logs: todo!(),
+        created_address: todo!(),
+    })
+}
+
+impl From<LevmError> for EvmError {
+    fn from(_value: LevmError) -> Self {
+        EvmError::Transaction("Levm error".to_string())
+    }
 }
