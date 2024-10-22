73d2b8f609495863db18403804333595deec1f18
ilitteri <ilitteri@fi.uba.ar>
2024-10-22 18:29:32 -0300

    Update deploy script

--- crates/l2/contracts/script/DeployL1.s.sol
+++ crates/l2/contracts/script/DeployL1.s.sol
@@ -12,27 +12,57 @@ contract DeployL1Script is Script {
     /// in the genesis file. The same contract with the same address is deployed
     /// in every testnet, so if this script is run in a testnet instead of in a
     /// local environment, it should work.
-    address constant DETERMINISTIC_CREATE2_ADDRESS = 0x4e59b44847b379578588920cA78FbF26c0B4956C;
+    address constant DETERMINISTIC_CREATE2_ADDRESS =
+        0x4e59b44847b379578588920cA78FbF26c0B4956C;
 
     function setUp() public {}
 
     function run() public {
         console.log("Deploying L1 contracts");
 
-        deployOnChainProposer();
-        deployCommonBridge(msg.sender);
+        bytes32 salt = bytes32(0);
+
+        address commonBridge = vm.computeCreate2Address(
+            salt,
+            keccak256(type(CommonBridge).creationCode),
+            DETERMINISTIC_CREATE2_ADDRESS
+        );
+        address onChainProposer = vm.computeCreate2Address(
+            salt,
+            keccak256(type(OnChainProposer).creationCode),
+            DETERMINISTIC_CREATE2_ADDRESS
+        );
+
+        deployOnChainProposer(commonBridge, salt);
+        deployCommonBridge(msg.sender, onChainProposer, salt);
     }
 
-    function deployOnChainProposer() internal {
+    function deployOnChainProposer(
+        address commonBridge,
+        bytes32 salt
+    ) internal {
         bytes memory bytecode = type(OnChainProposer).creationCode;
-        bytes32 salt = bytes32(0);
-        address contractAddress = Utils.deployWithCreate2(bytecode, salt, DETERMINISTIC_CREATE2_ADDRESS, "");
+        address contractAddress = Utils.deployWithCreate2(
+            bytecode,
+            salt,
+            DETERMINISTIC_CREATE2_ADDRESS,
+            abi.encode(commonBridge)
+        );
+        console.log("OnChainProposer deployed at:", contractAddress);
     }
 
-    function deployCommonBridge(address owner) internal {
+    function deployCommonBridge(
+        address owner,
+        address onChainProposer,
+        bytes32 salt
+    ) internal {
         bytes memory bytecode = type(CommonBridge).creationCode;
-        bytes32 salt = bytes32(0);
-        address contractAddress = Utils.deployWithCreate2(bytecode, salt, DETERMINISTIC_CREATE2_ADDRESS, abi.encode(owner));
+        address contractAddress = Utils.deployWithCreate2(
+            bytecode,
+            salt,
+            DETERMINISTIC_CREATE2_ADDRESS,
+            abi.encode(owner, onChainProposer)
+        );
         console.log("CommonBridge deployed at:", contractAddress);
     }
 }
