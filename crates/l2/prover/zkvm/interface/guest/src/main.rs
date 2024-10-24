864a707684b6b6a0c7d849e3a10a29a7c60ffc0d
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2024-10-24 17:04:39 -0300

    add block validation

--- crates/l2/prover/zkvm/interface/guest/src/main.rs
+++ crates/l2/prover/zkvm/interface/guest/src/main.rs
@@ -1,22 +1,49 @@
-use ethereum_rust_rlp::decode::RLPDecode;
+use ethereum_rust_rlp::{decode::RLPDecode, error::RLPDecodeError};
 use risc0_zkvm::guest::env;
 
-use ethereum_rust_core::types::Block;
-use ethereum_rust_vm::{execute_block, execution_db::ExecutionDB, EvmState};
+use ethereum_rust_blockchain::{validate_block, validate_gas_used, validate_state_root};
+use ethereum_rust_core::types::{Block, BlockHeader};
+use ethereum_rust_vm::{execute_block, execution_db::ExecutionDB, get_state_transitions, EvmState};
 
 fn main() {
-    // Read inputs
+    let (block, execution_db) = read_inputs().expect("failed to read inputs");
+    let mut state = EvmState::from_exec_db(execution_db);
+
+    // Validate if it can be the new head and find the parent
+    // TODO: find parent / check if block can be the new head (this is done by checking if the
+    // parent on the new block's head exists).
+    let parent_header = BlockHeader::default();
+
+    // Validate the block pre-execution
+    validate_block(&block, &parent_header, &state).expect("invalid block");
+
+    let receipts = execute_block(&block, &mut state).unwrap();
+
+    validate_gas_used(&receipts, &block.header).expect("invalid gas used");
+
+    let account_updates = get_state_transitions(&mut state);
+
+    // Apply the account updates over the last block's state and compute the new state root
+    let new_state_root = state
+        .database()
+        .unwrap() // should never fail
+        .apply_account_updates(block.header.parent_hash, &account_updates)
+        .expect("failed to apply account updates")
+        .unwrap_or_default();
+
+    // Check state root matches the one in block header after execution
+    validate_state_root(&block.header, new_state_root).expect("invalid state root");
+}
+
+fn read_inputs() -> Result<(Block, ExecutionDB), RLPDecodeError> {
     let head_block_bytes = env::read::<Vec<u8>>();
     let execution_db = env::read::<ExecutionDB>();
 
-    let block = Block::decode(&head_block_bytes).unwrap();
+    let block = Block::decode(&head_block_bytes)?;
 
-    // Make inputs public
+    // make inputs public
     env::commit(&block);
     env::commit(&execution_db);
 
-    // Execute block
-    let mut state = EvmState::from_exec_db(execution_db);
-    let block_receipts = execute_block(&block, &mut state).unwrap();
-    env::commit(&block_receipts);
+    Ok((block, execution_db))
 }
