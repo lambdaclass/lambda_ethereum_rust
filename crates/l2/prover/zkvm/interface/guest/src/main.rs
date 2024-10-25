7f84c9ea1171220db5a14a49917afd65ea24afc0
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2024-10-25 17:50:51 +0000

    feat(l2): prove block validation (#973)
    **Motivation**
    
    the block execution program is missing many validation steps, this PR
    adds them.
    
    **Description**
    
    - adds validation steps to zkVM program

--- crates/l2/prover/zkvm/interface/guest/src/main.rs
+++ crates/l2/prover/zkvm/interface/guest/src/main.rs
@@ -1,22 +1,39 @@
-use ethereum_rust_rlp::decode::RLPDecode;
+use ethereum_rust_rlp::{decode::RLPDecode, error::RLPDecodeError};
 use risc0_zkvm::guest::env;
 
-use ethereum_rust_core::types::Block;
-use ethereum_rust_vm::{execute_block, execution_db::ExecutionDB, EvmState};
+use ethereum_rust_blockchain::{validate_block, validate_gas_used};
+use ethereum_rust_core::types::{Block, BlockHeader};
+use ethereum_rust_vm::{execute_block, execution_db::ExecutionDB, get_state_transitions, EvmState};
 
 fn main() {
-    // Read inputs
+    let (block, execution_db, parent_header) = read_inputs().expect("failed to read inputs");
+    let mut state = EvmState::from_exec_db(execution_db.clone());
+
+    // Validate the block pre-execution
+    validate_block(&block, &parent_header, &state).expect("invalid block");
+
+    let receipts = execute_block(&block, &mut state).unwrap();
+
+    validate_gas_used(&receipts, &block.header).expect("invalid gas used");
+
+    let _account_updates = get_state_transitions(&mut state);
+
+    // TODO: compute new state root from account updates and check it matches with the block's
+    // header one.
+}
+
+fn read_inputs() -> Result<(Block, ExecutionDB, BlockHeader), RLPDecodeError> {
     let head_block_bytes = env::read::<Vec<u8>>();
     let execution_db = env::read::<ExecutionDB>();
+    let parent_header_bytes = env::read::<Vec<u8>>();
 
-    let block = Block::decode(&head_block_bytes).unwrap();
+    let block = Block::decode(&head_block_bytes)?;
+    let parent_header = BlockHeader::decode(&parent_header_bytes)?;
 
-    // Make inputs public
+    // make inputs public
     env::commit(&block);
     env::commit(&execution_db);
+    env::commit(&parent_header);
 
-    // Execute block
-    let mut state = EvmState::from_exec_db(execution_db);
-    let block_receipts = execute_block(&block, &mut state).unwrap();
-    env::commit(&block_receipts);
+    Ok((block, execution_db, parent_header))
 }
