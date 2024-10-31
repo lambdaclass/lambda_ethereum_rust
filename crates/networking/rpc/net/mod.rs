baa6f069eff9143cdbd2a1954b71e1dbce9b385c
h3lio5 <akashsm@ce.iitr.ac.in>
2024-10-31 23:24:11 +0530

    remove unnecessary map_err(..)

--- crates/networking/rpc/net/mod.rs
+++ crates/networking/rpc/net/mod.rs
@@ -5,8 +5,7 @@ use crate::utils::{RpcErr, RpcRequest};
 
 pub fn version(_req: &RpcRequest, store: Store) -> Result<Value, RpcErr> {
     let chain_spec = store
-        .get_chain_config()
-        .map_err(|error| RpcErr::Internal(error.to_string()))?;
+        .get_chain_config()?;
     serde_json::to_value(format!("{}", chain_spec.chain_id))
         .map_err(|error| RpcErr::Internal(error.to_string()))
 }
