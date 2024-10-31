9ea64501100d1c420dea081a3409b58d9af27010
h3lio5 <akashsm@ce.iitr.ac.in>
2024-10-31 23:25:13 +0530

    cargo fmt

--- crates/networking/rpc/net/mod.rs
+++ crates/networking/rpc/net/mod.rs
@@ -4,8 +4,7 @@ use serde_json::Value;
 use crate::utils::{RpcErr, RpcRequest};
 
 pub fn version(_req: &RpcRequest, store: Store) -> Result<Value, RpcErr> {
-    let chain_spec = store
-        .get_chain_config()?;
+    let chain_spec = store.get_chain_config()?;
     serde_json::to_value(format!("{}", chain_spec.chain_id))
         .map_err(|error| RpcErr::Internal(error.to_string()))
 }
