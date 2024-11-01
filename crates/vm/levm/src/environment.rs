0f6f29952ef8580816787e6765c6b516210ae150
Ivan Litteri <67517699+ilitteri@users.noreply.github.com>
2024-11-01 16:45:29 -0300

    Update crates/vm/levm/src/environment.rs

--- crates/vm/levm/src/environment.rs
+++ crates/vm/levm/src/environment.rs
@@ -21,34 +21,6 @@ pub struct Environment {
     pub tx_blob_hashes: Option<Vec<H256>>,
 }
 
-// The fee on a 1559 transaction is base_fee + priority_fee
-// So because of how things work here, we got priority_fee = gas_price - base_fee_per_gas
-//
-// Things to do:
-// - Fix fee calculations. Use EIP 1559 (base_fee + priority fee etc).
-// - Send the coinbase fee to the coinbase_account.
-// - Do the full gas discount at the beginning and then refund at the end.
-// - Add a method for substracting/adding to the balance of an account. This is done all over the place
-
-/*
-    gas_price = effective_gas_price
-    base_fee_per_gas = base_fee
-    priority_fee_per_gas = gas_price - base_fee_per_gas
-
-    effective_gas_price = priority_fee_per_gas + base_fee_per_gas
-
-    The priority fee per gas field is NOT a part of our VM. It is implicit as the difference
-    between the gas_price and the base_fee_per_gas.
-
-    When setting the VM for execution at the beginning, we have to calculate the priority_fee_per_gas as
-    priority_fee_per_gas = min(
-        tx.max_priority_fee_per_gas,
-        tx.max_fee_per_gas - base_fee_per_gas,
-    )
-    to then set the gas_price as priority_fee_per_gas + base_fee_per_gas
-
-
-*/
 
 impl Environment {
     pub fn default_from_address(origin: Address) -> Self {
