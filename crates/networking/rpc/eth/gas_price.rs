98366a9267a1bc5af9198b1c5dc566e0283aace7
Rodrigo Oliveri <rodrigooliveri10@gmail.com>
2024-11-06 12:20:57 -0300

    Small refactor for avoiding a mut

--- crates/networking/rpc/eth/gas_price.rs
+++ crates/networking/rpc/eth/gas_price.rs
@@ -79,26 +79,28 @@ impl RpcHandler for GasPrice {
         }
         results.sort();
         
-        let mut default_gas: u64 = 0;
+        let sample_gas = 
+            match results.get(results.len() / 2) {
+                Some(gas) => *gas,
+                None => {
+                    // If we don't have enough samples, we'll return the base fee or the min gas limit as a default.
+                    let default_gas =
+                        context
+                            .storage
+                            .get_block_header(latest_block_number)
+                            .ok()
+                            .flatten()
+                            .and_then(|header| header.base_fee_per_gas)
+                            .unwrap_or(MIN_GAS_LIMIT);
 
-        // If we don't have enough samples, we'll return the base fee.
-        let sample_gas = results.get(results.len() / 2).unwrap_or_else(|| {
-            default_gas =
-                context
-                    .storage
-                    .get_block_header(latest_block_number)
-                    .ok()
-                    .flatten()
-                    .and_then(|header| header.base_fee_per_gas)
-                    .unwrap_or(MIN_GAS_LIMIT);
+                    warn!(
+                        "Not enough samples to estimate gas price, returning base fee or min gas limit: {}",
+                        default_gas
+                    );
 
-            warn!(
-                "Not enough samples to estimate gas price, returning base fee or min gas limit: {}",
-                default_gas
-            );
-
-            &default_gas
-        });
+                    default_gas
+                }
+        };
 
         let gas_as_hex = format!("0x{:x}", sample_gas);
         Ok(serde_json::Value::String(gas_as_hex))
