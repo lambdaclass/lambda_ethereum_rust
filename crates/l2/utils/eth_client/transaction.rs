93cb0c1edebbda1389d57877ea13e0704cddae2b
Manuel Iñaki Bilbao <manuel.bilbao@lambdaclass.com>
2024-11-06 15:31:43 -0300

    Add blob_from_bytes

--- crates/l2/utils/eth_client/transaction.rs
+++ crates/l2/utils/eth_client/transaction.rs
@@ -1,5 +1,29 @@
+use bytes::Bytes;
+use c_kzg::{Blob, BYTES_PER_BLOB};
 use ethereum_rust_core::types::{EIP1559Transaction, EIP4844Transaction, PrivilegedL2Transaction};
 use ethereum_rust_rlp::structs::Encoder;
+
+pub fn blob_from_bytes(bytes: Bytes) -> Result<Blob, c_kzg::Error> {
+    // We set the first byte of every 32-bytes chunk to 0x00
+    // so it's always under the field module.
+    if bytes.len() > BYTES_PER_BLOB * 31 / 32 {
+        return Err(c_kzg::Error::InvalidBytesLength(format!(
+            "Bytes too long for a Blob ({})",
+            bytes.len()
+        )));
+    }
+
+    let mut buf = [0u8; BYTES_PER_BLOB];
+    buf[..(bytes.len() * 32).div_ceil(31)].copy_from_slice(
+        &bytes
+            .chunks(31)
+            .map(|x| [&[0x00], x].concat())
+            .collect::<Vec<_>>()
+            .concat(),
+    );
+
+    Blob::from_bytes(&buf)
+}
 
 pub trait PayloadRLPEncode {
     fn encode_payload(&self, buf: &mut dyn bytes::BufMut);
