30ba82fe0cdc4c6c167c157e9ad934bb14f80ca1
fmoletta <fedemoletta@hotmail.com>
2024-11-07 15:36:00 -0300

    Add proptest

--- crates/storage/trie/trie_iter.rs
+++ crates/storage/trie/trie_iter.rs
@@ -95,14 +95,20 @@ pub fn print_node(trie: &Trie, node_hash: NodeHash) {
 
 #[cfg(test)]
 mod tests {
+
     use super::*;
+    use proptest::{
+        collection::{btree_map, vec},
+        prelude::any,
+        proptest,
+    };
 
     #[test]
     fn trie_iter_content() {
         let expected_content = vec![
-            (vec![0, 0], vec![0, 0]),
-            (vec![1, 1], vec![1, 1]),
-            (vec![2, 2], vec![2, 2]),
+            (vec![0, 9], vec![3, 4]),
+            (vec![1, 2], vec![5, 6]),
+            (vec![2, 7], vec![7, 8]),
         ];
         let mut trie = Trie::new_temp();
         for (path, value) in expected_content.clone() {
@@ -110,5 +116,18 @@ mod tests {
         }
         let content = trie.into_iter().content().collect::<Vec<_>>();
         assert_eq!(content, expected_content);
+    }
+    proptest! {
+
+        #[test]
+        fn proptest_trie_iter_content(data in btree_map(vec(any::<u8>(), 5..100), vec(any::<u8>(), 5..100), 5..100)) {
+            let expected_content = data.clone().into_iter().collect::<Vec<_>>();
+            let mut trie = Trie::new_temp();
+            for (path, value) in data.into_iter() {
+                trie.insert(path, value).unwrap()
+            }
+            let content = trie.into_iter().content().collect::<Vec<_>>();
+            assert_eq!(content, expected_content);
+        }
     }
 }
