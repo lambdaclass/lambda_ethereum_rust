f7f0139eb5f91d5b4a920f454841a0537dcdd157
maximopalopoli <mpalopoli@fi.uba.ar>
2024-11-07 15:32:43 -0300

    Fix unsafe arithmetic ops in memory

--- crates/vm/levm/src/memory.rs
+++ crates/vm/levm/src/memory.rs
@@ -31,10 +31,10 @@ impl Memory {
     }
 
     pub fn load(&mut self, offset: usize) -> Result<U256, VMError> {
-        self.resize(offset + 32);
+        self.resize(offset.checked_add(32).ok_or(VMError::OffsetOverflow)?);
         let value_bytes: [u8; 32] = self
             .data
-            .get(offset..offset + 32)
+            .get(offset..offset.checked_add(32).ok_or(VMError::OffsetOverflow)?)
             .ok_or(VMError::MemoryLoadOutOfBounds)?
             .try_into()
             .unwrap();
@@ -42,9 +42,9 @@ impl Memory {
     }
 
     pub fn load_range(&mut self, offset: usize, size: usize) -> Result<Vec<u8>, VMError> {
-        self.resize(offset + size);
+        self.resize(offset.checked_add(size).ok_or(VMError::OffsetOverflow)?);
         self.data
-            .get(offset..offset + size)
+            .get(offset..offset.checked_add(size).ok_or(VMError::OffsetOverflow)?)
             .ok_or(VMError::MemoryLoadOutOfBounds)
             .map(|slice| slice.to_vec())
     }
@@ -52,33 +52,65 @@ impl Memory {
     pub fn store_bytes(&mut self, offset: usize, value: &[u8]) -> Result<(), VMError> {
         let len = value.len();
         let data_len = self.data.len();
-        if data_len < offset || data_len < offset + len {
+        if data_len < offset || data_len < offset.checked_add(len).ok_or(VMError::OffsetOverflow)? {
             return Err(VMError::MemoryStoreOutOfBounds);
         }
-        self.resize(offset + len);
-        self.data
-            .splice(offset..offset + len, value.iter().copied());
+        self.resize(offset.checked_add(len).ok_or(VMError::OffsetOverflow)?);
+        self.data.splice(
+            offset..offset.checked_add(len).ok_or(VMError::OffsetOverflow)?,
+            value.iter().copied(),
+        );
         Ok(())
     }
 
-    pub fn store_n_bytes(&mut self, offset: usize, value: &[u8], size: usize) {
-        self.resize(offset + size);
-        self.data
-            .splice(offset..offset + size, value.iter().copied());
+    pub fn store_n_bytes(
+        &mut self,
+        offset: usize,
+        value: &[u8],
+        size: usize,
+    ) -> Result<(), VMError> {
+        self.resize(offset.checked_add(size).ok_or(VMError::OffsetOverflow)?);
+        self.data.splice(
+            offset..offset.checked_add(size).ok_or(VMError::OffsetOverflow)?,
+            value.iter().copied(),
+        );
+        Ok(())
     }
 
     pub fn size(&self) -> U256 {
         U256::from(self.data.len())
     }
 
-    pub fn copy(&mut self, src_offset: usize, dest_offset: usize, size: usize) {
-        let max_size = std::cmp::max(src_offset + size, dest_offset + size);
+    pub fn copy(
+        &mut self,
+        src_offset: usize,
+        dest_offset: usize,
+        size: usize,
+    ) -> Result<(), VMError> {
+        let max_size = std::cmp::max(
+            src_offset
+                .checked_add(size)
+                .ok_or(VMError::DataSizeOverflow)?,
+            dest_offset
+                .checked_add(size)
+                .ok_or(VMError::DataSizeOverflow)?,
+        );
         self.resize(max_size);
         let mut temp = vec![0u8; size];
 
-        temp.copy_from_slice(&self.data[src_offset..src_offset + size]);
+        temp.copy_from_slice(
+            &self.data[src_offset
+                ..src_offset
+                    .checked_add(size)
+                    .ok_or(VMError::OffsetOverflow)?],
+        );
 
-        self.data[dest_offset..dest_offset + size].copy_from_slice(&temp);
+        self.data[dest_offset
+            ..dest_offset
+                .checked_add(size)
+                .ok_or(VMError::OffsetOverflow)?]
+            .copy_from_slice(&temp);
+        Ok(())
     }
 
     pub fn expansion_cost(&self, memory_byte_size: usize) -> Result<U256, VMError> {
@@ -110,6 +142,9 @@ impl Memory {
             .and_then(|cost| cost.checked_add(last_memory_size_word.checked_mul(3)?))
             .ok_or(VMError::OverflowInArithmeticOp)?;
 
-        Ok((new_memory_cost - last_memory_cost).into())
+        Ok((new_memory_cost
+            .checked_sub(last_memory_cost)
+            .ok_or(VMError::GasCostOverflow)?)
+        .into())
     }
 }
