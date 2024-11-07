0bd40f992567968ab8bc04ac9ad3e897ee445591
JereSalo <jeresalo17@gmail.com>
2024-11-07 15:33:01 -0300

    run cargo fmt

--- crates/vm/levm/src/opcode_handlers/stack_memory_storage_flow.rs
+++ crates/vm/levm/src/opcode_handlers/stack_memory_storage_flow.rs
@@ -187,10 +187,9 @@ impl VM {
 
         if !self.cache.is_slot_cached(&address, key) {
             base_dynamic_gas += U256::from(2100);
-        } 
-        
+        }
+
         let storage_slot = self.get_storage_slot(&address, key);
-        
 
         base_dynamic_gas += if value == storage_slot.current_value {
             U256::from(100)
@@ -210,18 +209,16 @@ impl VM {
         let mut gas_refunds = U256::zero();
         if value != storage_slot.current_value {
             if storage_slot.current_value == storage_slot.original_value {
-            if storage_slot.original_value != U256::zero() && value == U256::zero() {
-                gas_refunds += U256::from(4800);
-            }
-            } 
-            else if storage_slot.original_value != U256::zero() {
+                if storage_slot.original_value != U256::zero() && value == U256::zero() {
+                    gas_refunds += U256::from(4800);
+                }
+            } else if storage_slot.original_value != U256::zero() {
                 if storage_slot.current_value == U256::zero() {
                     gas_refunds -= U256::from(4800);
                 } else if value == U256::zero() {
                     gas_refunds += U256::from(4800);
                 }
-            }
-            else if value == storage_slot.original_value {
+            } else if value == storage_slot.original_value {
                 if storage_slot.original_value == U256::zero() {
                     gas_refunds += U256::from(20000) - U256::from(100);
                 } else {
@@ -229,8 +226,12 @@ impl VM {
                 }
             }
         };
-            
-        self.env.refunded_gas = self.env.refunded_gas.checked_add(gas_refunds).ok_or(VMError::GasLimitPriceProductOverflow)?;
+
+        self.env.refunded_gas = self
+            .env
+            .refunded_gas
+            .checked_add(gas_refunds)
+            .ok_or(VMError::GasLimitPriceProductOverflow)?;
 
         self.cache.write_account_storage(
             &address,
