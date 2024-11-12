cf58233d54bc2ee1adab270aafe375d7da8725ef
JereSalo <jeresalo17@gmail.com>
2024-11-12 12:34:43 -0300

    add internal error

--- crates/vm/levm/src/errors.rs
+++ crates/vm/levm/src/errors.rs
@@ -4,38 +4,76 @@ use ethereum_rust_core::{types::Log, Address};
 use std::collections::HashMap;
 
 /// Errors that halt the program
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, PartialEq, Eq, thiserror::Error)]
 pub enum VMError {
+    #[error("Stack underflow")]
     StackUnderflow,
+    #[error("Stack overflow")]
     StackOverflow,
+    #[error("Invalid jump")]
     InvalidJump,
+    #[error("Opcode not allowed in static context")]
     OpcodeNotAllowedInStaticContext,
+    #[error("Opcode not found")]
     OpcodeNotFound,
+    #[error("Invalid bytecode")]
     InvalidBytecode,
+    #[error("Out of gas")]
     OutOfGas,
+    #[error("Very large number")]
     VeryLargeNumber,
+    #[error("Overflow in arithmetic operation")]
     OverflowInArithmeticOp,
+    #[error("Fatal error")]
     FatalError,
+    #[error("Invalid transaction")]
     InvalidTransaction,
+    #[error("Revert opcode")]
     RevertOpcode,
+    #[error("Invalid opcode")]
     InvalidOpcode,
+    #[error("Missing blob hashes")]
     MissingBlobHashes,
+    #[error("Blob hash index out of bounds")]
     BlobHashIndexOutOfBounds,
+    #[error("Sender account does not exist")]
     SenderAccountDoesNotExist,
+    #[error("Address does not match an account")]
     AddressDoesNotMatchAnAccount,
+    #[error("Sender account should not have bytecode")]
     SenderAccountShouldNotHaveBytecode,
+    #[error("Sender balance should contain transfer value")]
     SenderBalanceShouldContainTransferValue,
+    #[error("Gas price is lower than base fee")]
     GasPriceIsLowerThanBaseFee,
+    #[error("Address already occupied")]
     AddressAlreadyOccupied,
+    #[error("Contract output too big")]
     ContractOutputTooBig,
+    #[error("Invalid initial byte")]
     InvalidInitialByte,
+    #[error("Nonce overflow")]
     NonceOverflow,
+    #[error("Memory load out of bounds")]
     MemoryLoadOutOfBounds,
+    #[error("Memory store out of bounds")]
     MemoryStoreOutOfBounds,
+    #[error("Gas limit price product overflow")]
     GasLimitPriceProductOverflow,
+    #[error("Slicing error")]
     SlicingError,
+    #[error("Indexing error")]
     IndexingError,
+    #[error("Fatal unwrap")]
     FatalUnwrap, // I will use this generic error for things that shouldn't fail
+    #[error("Internal error: {0}")]
+    Internal(#[from] InternalError),
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, thiserror::Error)]
+pub enum InternalError {
+    #[error("exampleerror")]
+    Example,
 }
 
 pub enum OpcodeSuccess {
