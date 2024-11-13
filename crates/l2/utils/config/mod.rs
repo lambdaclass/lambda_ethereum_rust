3bda49899d49d02a6df19b291a13947a128af978
Federico Borello <156438142+fborello-lambda@users.noreply.github.com>
2024-11-13 09:21:46 -0300

    Update crates/l2/utils/config/mod.rs
    Co-authored-by: Ivan Litteri <67517699+ilitteri@users.noreply.github.com>

--- crates/l2/utils/config/mod.rs
+++ crates/l2/utils/config/mod.rs
@@ -37,7 +37,7 @@ pub fn read_env_file() -> Result<(), errors::ConfigError> {
 
 pub fn read_env_as_lines(
 ) -> Result<std::io::Lines<std::io::BufReader<std::fs::File>>, errors::ConfigError> {
-    let env_file_name = std::env::var("ENV_FILE").unwrap_or_else(|_| ".env".to_string());
+    let env_file_name = std::env::var("ENV_FILE").unwrap_or(".env".to_owned());
     let env_file = std::fs::File::open(env_file_name)?;
     let reader = std::io::BufReader::new(env_file);
 
