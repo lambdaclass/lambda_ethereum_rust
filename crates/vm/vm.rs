45aebd5f89b15575ebf9c9160f3a52d074d308ba
JereSalo <jeresalo17@gmail.com>
2024-11-13 16:40:34 -0300

    remove ?

--- crates/vm/vm.rs
+++ crates/vm/vm.rs
@@ -188,7 +188,7 @@ cfg_if::cfg_if! {
                 tx.data().clone(),
                 db,
                 Cache::default(),
-            )?;
+            );
 
             vm?.transact()
         }
