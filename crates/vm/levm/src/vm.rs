e1b0f269cdb311dc386ffd5b1d688fdeca071e16
JereSalo <jeresalo17@gmail.com>
2024-11-13 11:51:02 -0300

    fix double transfer of value

--- crates/vm/levm/src/vm.rs
+++ crates/vm/levm/src/vm.rs
@@ -335,7 +335,6 @@ impl VM {
         let origin = self.env.origin;
         let to = self.call_frames[0].to;
 
-        let mut receiver_account = self.get_account(&to);
         let mut sender_account = self.get_account(&origin);
 
         // See if it's raised in upper layers
@@ -353,12 +352,8 @@ impl VM {
         if sender_account.info.balance < self.call_frames[0].msg_value {
             return Err(VMError::SenderBalanceShouldContainTransferValue);
         }
-        // TODO: This belongs elsewhere.
-        sender_account.info.balance -= self.call_frames[0].msg_value;
-        receiver_account.info.balance += self.call_frames[0].msg_value;
 
         self.cache.add_account(&origin, &sender_account);
-        self.cache.add_account(&to, &receiver_account);
 
         // (7)
         if self.env.gas_price < self.env.base_fee_per_gas {
