9076208bf9db9f807307dc2cff34f30b61843531
Jeremías Salomón <48994069+JereSalo@users.noreply.github.com>
2024-11-13 21:06:53 -0300

    feat(levm): initialize cache with sender, recipient and coinbase accounts (#1114)
    **Motivation**
    
    <!-- Why does this pull request exist? What are its goals? -->
    
    **Description**
    
    As stated in [evm.codes](https://www.evm.codes/about#access_list), this
    PR initializes the cache with the corresponding addresses. (Without
    implementing yet the concept of an Access List transaction)
    <!-- A clear and concise general description of the changes this PR
    introduces -->
    Notes: Our `Cache` struct replaces the need for a `Substate` that keeps
    warm addresses and storage slots because we know that if an account or
    storage slot is in our cache then it is already warm.
    
    <!-- Link to issues: Resolves #111, Resolves #222 -->
    
    Closes #1088
    
    ---------
    
    Co-authored-by: Juani Medone <juan.medone@lambdaclass.com>
    Co-authored-by: maximopalopoli <mpalopoli@fi.uba.ar>
    Co-authored-by: Javier Chatruc <jrchatruc@gmail.com>
    Co-authored-by: Ivan Litteri <67517699+ilitteri@users.noreply.github.com>
    Co-authored-by: ilitteri <ilitteri@fi.uba.ar>

--- crates/vm/levm/src/vm.rs
+++ crates/vm/levm/src/vm.rs
@@ -74,14 +74,25 @@ impl VM {
     ) -> Result<Self, VMError> {
         // Maybe this decision should be made in an upper layer
 
+        // Add sender, coinbase and recipient (in the case of a Call) to cache [https://www.evm.codes/about#access_list]
+        let sender_account_info = db.get_account_info(env.origin);
+        cache.add_account(&env.origin, &Account::from(sender_account_info.clone()));
+
+        let coinbase_account_info = db.get_account_info(env.coinbase);
+        cache.add_account(&env.coinbase, &Account::from(coinbase_account_info));
+
         match to {
             TxKind::Call(address_to) => {
+                // add address_to to cache
+                let recipient_account_info = db.get_account_info(address_to);
+                cache.add_account(&address_to, &Account::from(recipient_account_info.clone()));
+
                 // CALL tx
                 let initial_call_frame = CallFrame::new(
                     env.origin,
                     address_to,
                     address_to,
-                    db.get_account_info(address_to).bytecode,
+                    recipient_account_info.bytecode,
                     value,
                     calldata.clone(),
                     false,
@@ -101,8 +112,6 @@ impl VM {
             }
             TxKind::Create => {
                 // CREATE tx
-                let sender_account_info = db.get_account_info(env.origin);
-                // Note that this is a copy of account, not the real one
 
                 // (2)
                 let new_contract_address =
