edbcf91f57036f435764850c9e7adbfcdb02d890
JereSalo <jeresalo17@gmail.com>
2024-11-14 12:10:28 -0300

    uncomment particular test

--- crates/vm/levm/tests/ef/runner.rs
+++ crates/vm/levm/tests/ef/runner.rs
@@ -37,11 +37,11 @@ pub fn run_ef_tests() -> Result<EFTestsReport, Box<dyn Error>> {
             {
                 continue;
             }
-            if test
-                .path()
-                .file_name()
-                .is_some_and(|name| name == "senderBalance.json")
-            {
+            // if test
+            //     .path()
+            //     .file_name()
+            //     .is_some_and(|name| name == "senderBalance.json")
+            // {
                 let test_result = run_ef_test(
                     serde_json::from_reader(std::fs::File::open(test.path())?)?,
                     &mut report,
@@ -49,7 +49,7 @@ pub fn run_ef_tests() -> Result<EFTestsReport, Box<dyn Error>> {
                 if test_result.is_err() {
                     continue;
                 }
-            }
+            // }
         }
         spinner.update_text(report.progress());
     }
