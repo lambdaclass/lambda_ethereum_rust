a595d25fa59304aba42a9ebb0ed237453d43229d
h3lio5 <akashsm@ce.iitr.ac.in>
2024-11-14 22:31:32 +0530

    refactor: remove explicit map_err

--- crates/networking/rpc/net/mod.rs
+++ crates/networking/rpc/net/mod.rs
@@ -8,8 +8,8 @@ use crate::{
 pub fn version(_req: &RpcRequest, context: RpcApiContext) -> Result<Value, RpcErr> {
     let chain_spec = context
         .storage
-        .get_chain_config()
-        .map_err(|error| RpcErr::Internal(error.to_string()))?;
-    serde_json::to_value(format!("{}", chain_spec.chain_id))
-        .map_err(|error| RpcErr::Internal(error.to_string()))
+        .get_chain_config()?;
+    
+    let value = serde_json::to_value(format!("{}", chain_spec.chain_id))?;
+    Ok(value)
 }
