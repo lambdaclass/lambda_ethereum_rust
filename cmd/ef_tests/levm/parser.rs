950f2aadb2424c702513aa640f274b2a7c82795d
ilitteri <ilitteri@fi.uba.ar>
2024-11-21 19:02:13 -0300

    Isolate EF test parsing

---
+++ cmd/ef_tests/levm/parser.rs
@@ -0,0 +1,99 @@
+use crate::{
+    runner::EFTestRunnerOptions,
+    types::{EFTest, EFTests},
+};
+use spinoff::{spinners::Dots, Color, Spinner};
+use std::fs::DirEntry;
+
+#[derive(Debug, thiserror::Error)]
+pub enum EFTestParseError {
+    #[error("Failed to read directory: {0}")]
+    FailedToReadDirectory(String),
+    #[error("Failed to read file: {0}")]
+    FailedToReadFile(String),
+    #[error("Failed to get file type: {0}")]
+    FailedToGetFileType(String),
+    #[error("Failed to parse test file: {0}")]
+    FailedToParseTestFile(String),
+}
+
+pub fn parse_ef_tests(opts: &EFTestRunnerOptions) -> Result<Vec<EFTest>, EFTestParseError> {
+    let cargo_manifest_dir = std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    let ef_general_state_tests_path = cargo_manifest_dir.join("vectors/GeneralStateTests");
+    let mut spinner = Spinner::new(Dots, "Parsing EF Tests", Color::Cyan);
+    let mut tests = Vec::new();
+    for test_dir in std::fs::read_dir(ef_general_state_tests_path.clone())
+        .map_err(|err| {
+            EFTestParseError::FailedToReadDirectory(format!(
+                "{:?}: {err}",
+                ef_general_state_tests_path.file_name()
+            ))
+        })?
+        .flatten()
+    {
+        let directory_tests = parse_ef_test_dir(test_dir, opts, &mut spinner)?;
+        tests.extend(directory_tests);
+    }
+    spinner.success("Parsed EF Tests");
+    Ok(tests)
+}
+
+pub fn parse_ef_test_dir(
+    test_dir: DirEntry,
+    opts: &EFTestRunnerOptions,
+    directory_parsing_spinner: &mut Spinner,
+) -> Result<Vec<EFTest>, EFTestParseError> {
+    let mut directory_tests = Vec::new();
+    for test in std::fs::read_dir(test_dir.path())
+        .map_err(|err| {
+            EFTestParseError::FailedToReadDirectory(format!("{:?}: {err}", test_dir.file_name()))
+        })?
+        .flatten()
+    {
+        if test
+            .file_type()
+            .map_err(|err| {
+                EFTestParseError::FailedToGetFileType(format!("{:?}: {err}", test.file_name()))
+            })?
+            .is_dir()
+        {
+            let sub_directory_tests = parse_ef_test_dir(test, opts, directory_parsing_spinner)?;
+            directory_tests.extend(sub_directory_tests);
+            continue;
+        }
+        // Skip non-JSON files.
+        if test.path().extension().is_some_and(|ext| ext != "json")
+            | test.path().extension().is_none()
+        {
+            continue;
+        }
+        // Skip the ValueOverflowParis.json file.
+        if test
+            .path()
+            .file_name()
+            .is_some_and(|name| name == "ValueOverflowParis.json")
+        {
+            continue;
+        }
+
+        // Skip tests that are not in the list of tests to run.
+        if !opts
+            .tests
+            .contains(&test_dir.file_name().to_str().unwrap().to_owned())
+        {
+            continue;
+        }
+
+        directory_parsing_spinner
+            .update_text(format!("Parsing directory {:?}", test_dir.file_name()));
+
+        let test_file = std::fs::File::open(test.path()).map_err(|err| {
+            EFTestParseError::FailedToReadFile(format!("{:?}: {err}", test.path()))
+        })?;
+        let test: EFTests = serde_json::from_reader(test_file).map_err(|err| {
+            EFTestParseError::FailedToParseTestFile(format!("{:?} parse error: {err}", test.path()))
+        })?;
+        directory_tests.extend(test.0);
+    }
+    Ok(directory_tests)
+}
