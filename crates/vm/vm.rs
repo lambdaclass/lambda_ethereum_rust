aea8462ac829ea9b1c600724406e06e6d4d73acb
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2024-11-22 16:40:50 -0300

    disable for levm too

--- crates/vm/vm.rs
+++ crates/vm/vm.rs
@@ -94,9 +94,10 @@ cfg_if::cfg_if! {
             state: &mut EvmState,
         ) -> Result<(Vec<Receipt>, Vec<AccountUpdate>), EvmError> {
             let block_header = &block.header;
-            let spec_id = spec_id(&state.chain_config()?, block_header.timestamp);
             //eip 4788: execute beacon_root_contract_call before block transactions
+            #[cfg(not(feature = "l2"))]
             if block_header.parent_beacon_block_root.is_some() && spec_id == SpecId::CANCUN {
+                let spec_id = spec_id(&state.chain_config()?, block_header.timestamp);
                 beacon_root_contract_call(state, block_header, spec_id)?;
             }
             let mut receipts = Vec::new();
