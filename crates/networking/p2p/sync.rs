a875680596c29c8ff67c31853592718179c9b11e
fmoletta <fedemoletta@hotmail.com>
2024-11-22 17:24:44 -0300

    First iteration of state retrieval planning

--- crates/networking/p2p/sync.rs
+++ crates/networking/p2p/sync.rs
@@ -1,6 +1,6 @@
 use std::{sync::Arc, time::Duration};
 
-use ethereum_rust_core::H256;
+use ethereum_rust_core::{types::{validate_block_header, BlockHash, BlockHeader, InvalidBlockHeaderError}, H256};
 use tokio::sync::Mutex;
 use tracing::{debug, info};
 
@@ -55,8 +55,12 @@ impl SyncManager {
             limit: BYTES_PER_REQUEST,
             reverse: false,
         };
+        let mut all_block_headers = vec![];
+        let mut all_block_hashes = vec![];
         loop {
             // TODO: Randomize id
+            block_headers_request.id +=1;
+            info!("[Sync] Sending request {block_headers_request:?}");
             // Send a GetBlockHeaders request to a peer
             if self
                 .peers
@@ -66,6 +70,7 @@ impl SyncManager {
                 .await
                 .is_err()
             {
+                info!("[Sync] No peers available, retrying in 10 sec");
                 // This is the unlikely case where we just started the node and don't have peers, wait a bit and try again
                 tokio::time::sleep(tokio::time::Duration::from_secs(10)).await;
                 continue;
@@ -83,6 +88,12 @@ impl SyncManager {
 
                 // If the response is empty, lets ask another peer
                 if message.block_headers.is_empty() {
+                    debug!("[Sync] Bad peer response");
+                    continue;
+                }
+                // Validate header batch
+                if validate_header_batch(&message.block_headers).is_err() {
+                    debug!("[Sync] Invalid header in batch");
                     continue;
                 }
                 // Discard the first header as we already have it
@@ -96,8 +107,12 @@ impl SyncManager {
                     block_hashes.first().unwrap(),
                     block_hashes.last().unwrap()
                 );
-                // Process headers (validate + download state)
-                // TODO!
+
+                // First iteration will not process the batch, but will wait for all headers to be fetched and validated
+                // before processing the whole batch
+                all_block_headers.extend_from_slice(&headers);
+                all_block_hashes.extend_from_slice(&block_hashes);
+
                 // Check if we already reached our sync head or if we need to fetch more blocks
                 if !block_hashes.contains(&sync_head) {
                     // Update the request to fetch the next batch
@@ -107,8 +122,19 @@ impl SyncManager {
                     break;
                 }
             };
+            debug!("[Sync] Peer response timeout");
             // Reply timeouted/ peer shut down, lets try a different peer
         }
+        info!("[Sync] All headers fetched and validated");
+        // [First Iteration] We finished fetching all headers, now we can process them
+        // We will launch 3 tasks to:
+        // 1) Fetch each block's state via snap p2p requests
+        // 2) Fetch each blocks and its receipts via eth p2p requests
+        // 3) Receive replies from the receiver and send them to the two tasks
+        let (snap_state_receiver, snap_state_sender) =
+        tokio::sync::mpsc::channel::<Message>(10);
+        let (block_and_receipt_receiver, block_and_receipt_sender) =
+        tokio::sync::mpsc::channel::<Message>(10);
 
         // Sync finished
     }
@@ -137,4 +163,18 @@ async fn receive_block_headers(
             None => return None,
         }
     }
+}
+
+fn validate_header_batch(headers: &[BlockHeader]) -> Result<(), InvalidBlockHeaderError>  {
+    // The first header is a header we have already validated (either current last block or last block in previous batch)
+    for headers in headers.windows(2) {
+        validate_block_header(&headers[0], &headers[1])?;
+    }
+    Ok(())
+}
+
+// Process an already validated block header
+// In parallel: Fetches the block and its receipt
+fn process_header(header: &BlockHeader, hash: &BlockHash) {
+
 }
