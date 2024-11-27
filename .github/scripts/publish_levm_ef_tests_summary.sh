7afe1bc670f5710b050b28decd7b06a13d743985
ilitteri <ilitteri@fi.uba.ar>
2024-11-27 18:46:58 -0300

    Add job to run daily LEVM EF Tests summary

---
+++ .github/scripts/publish_levm_ef_tests_summary.sh
@@ -0,0 +1,22 @@
+curl -X POST $url \
+-H 'Content-Type: application/json; charset=utf-8' \
+--data @- <<EOF
+$(jq -n --arg text "$(cat levm_ef_tests_summary.txt)" '{
+    "blocks": [
+        {
+            "type": "header",
+            "text": {
+                "type": "plain_text",
+                "text": "Daily LEVM EF Tests Run Report"
+            }
+        },
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": $text
+            }             
+        }
+    ]
+}')
+EOF
