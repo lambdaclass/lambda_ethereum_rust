1e0449d837349052acc6c64254666fa39ad1f71c
JereSalo <jeresalo17@gmail.com>
2024-11-27 16:56:15 -0300

    change return type of has_code in Account

--- crates/vm/levm/src/account.rs
+++ crates/vm/levm/src/account.rs
@@ -59,8 +59,8 @@ impl Account {
         }
     }
 
-    pub fn has_code(&self) -> Result<bool, VMError> {
-        Ok(!(self.info.bytecode.is_empty() || self.bytecode_hash() == EMPTY_CODE_HASH))
+    pub fn has_code(&self) -> bool {
+        !(self.info.bytecode.is_empty() || self.bytecode_hash() == EMPTY_CODE_HASH)
     }
 
     pub fn bytecode_hash(&self) -> H256 {
