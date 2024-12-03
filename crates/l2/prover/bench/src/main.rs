2c762208f1705420f2d708e2d57610dc648ada61
Estéfano Bargas <estefano.bargas@fing.edu.uy>
2024-12-03 13:12:41 -0300

    rate limited account fetching

--- crates/l2/prover/bench/src/main.rs
+++ crates/l2/prover/bench/src/main.rs
@@ -2,9 +2,12 @@ use bench::rpc::{get_account, get_block};
 use clap::Parser;
 use ethrex_vm::{execution_db::touched_state::get_touched_state, SpecId};
 use futures_util::future::join_all;
+use tokio_utils::RateLimiter;
 
 const MAINNET_CHAIN_ID: u64 = 0x1;
 const MAINNET_SPEC_ID: SpecId = SpecId::CANCUN;
+
+const RPC_RATE_LIMIT: usize = 100; // requests per second
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -21,25 +24,46 @@ async fn main() {
         block_number,
     } = Args::parse();
 
-    // fetch block
+    println!("fetching block {block_number}");
     let block = get_block(&rpc_url, &block_number)
         .await
         .expect("failed to fetch block");
 
-    // get all accounts and storage keys touched during execution of block
+    println!("pre-executing transactions to get touched state keys");
     let touched_state = get_touched_state(&block, MAINNET_CHAIN_ID, MAINNET_SPEC_ID)
         .expect("failed to get touched state");
 
-    // fetch all accounts and storage touched
-    let _accounts = join_all(
-        touched_state
-            .iter()
-            .map(|(address, _)| get_account(&rpc_url, &block_number, address)),
-    )
-    .await
-    .into_iter()
-    .collect::<Result<Vec<_>, String>>()
-    .expect("failed to fetch accounts");
+    println!("fetching touched state values");
+    let mut accounts = Vec::with_capacity(touched_state.len());
+
+    let rate_limiter = RateLimiter::new(std::time::Duration::from_secs(1));
+    let mut fetched_accs = 0;
+    for request_chunk in touched_state.chunks(RPC_RATE_LIMIT) {
+        let rate_limited = rate_limiter.throttle(|| async {
+            join_all(
+                request_chunk
+                    .iter()
+                    .map(|(address, _)| get_account(&rpc_url, &block_number, address)),
+            )
+            .await
+        });
+
+        let account_chunk = rate_limited
+            .await
+            .into_iter()
+            .collect::<Result<Vec<_>, String>>()
+            .expect("failed to fetch accounts");
+
+        accounts.extend(account_chunk);
+
+        fetched_accs += request_chunk.len();
+        println!(
+            "fetched {} accounts of {}",
+            fetched_accs,
+            touched_state.len()
+        );
+    }
+
     // TODO: storage
 
     // 4. create prover program input and execute. Measure time.
