891a7815a7b2f4fe25e1e9ef11d37bc7ade6f9a9
Damian Ramirez <damian.ramirez@lambdaclass.com>
2024-12-03 15:09:00 -0300

    Add early return on op_div if the dividend is 0

--- crates/vm/levm/src/opcode_handlers/arithmetic.rs
+++ crates/vm/levm/src/opcode_handlers/arithmetic.rs
@@ -53,6 +53,10 @@ impl VM {
 
         let dividend = current_call_frame.stack.pop()?;
         let divisor = current_call_frame.stack.pop()?;
+        if dividend.is_zero() {
+            current_call_frame.stack.push(U256::zero())?;
+            return Ok(OpcodeSuccess::Continue);
+        }
         let Some(quotient) = dividend.checked_div(divisor) else {
             current_call_frame.stack.push(U256::zero())?;
             return Ok(OpcodeSuccess::Continue);
