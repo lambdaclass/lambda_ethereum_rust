6349a7acd23f179e56b856782015bbf14f93d63f
Dylan Socolobsky <dylan.socolobsky@lambdaclass.com>
2024-12-04 10:54:37 -0300

    Run flamegraph for reth in parallel in a separate job

--- .github/scripts/flamegraph_reth.sh
+++ .github/scripts/flamegraph_reth.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 cd "$(dirname "$0")" || exit # Make sure to run from script's directory
 
-account=0x33c6b73432B3aeA0C1725E415CC40D04908B85fd
+iterations=3
 value=10000000
-end_val=$((172 * 1000 * $value))
+account=0x33c6b73432B3aeA0C1725E415CC40D04908B85fd
+end_val=$((172 * $iterations * $value))
 
-ethrex_l2 test load --path ../../test_data/private_keys.txt -i 1000 -v  --value $value --to $account
+ethrex_l2 test load --path ../../test_data/private_keys.txt -i $iterations -v  --value $value --to $account
 
 echo "Monitoring..."
 output=$(cast balance $account --rpc-url=http://localhost:1729 2>&1)
@@ -18,4 +19,4 @@ while [[ $output -le $end_val ]]; do
 done
 echo "Balance of $output reached, killing process reth"
 
-sudo pkill reth && while pgrep -l cargo flamegraph; do sleep 1;done;
+sudo pkill reth && while pgrep -l "cargo-flamegraph"; do sleep 1;done;
