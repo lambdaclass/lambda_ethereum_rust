0dd0e05105bc9417dfae8e5c1bf2e2885f1b6363
Dylan Socolobsky <dylan.socolobsky@lambdaclass.com>
2024-12-04 14:46:21 -0300

    Adjust paths in workflow

--- .github/scripts/flamegraph_reth.sh
+++ .github/scripts/flamegraph_reth.sh
@@ -5,16 +5,13 @@ value=10000000
 account=0x33c6b73432B3aeA0C1725E415CC40D04908B85fd
 end_val=$((172 * $iterations * $value))
 
-ethrex_l2 test load --path ../../test_data/private_keys.txt -i $iterations -v  --value $value --to $account
+ethrex_l2 test load --path /home/runner/work/ethrex/ethrex/test_data/private_keys.txt -i $iterations -v  --value $value --to $account
 
 echo "Monitoring..."
 output=$(cast balance $account --rpc-url=http://localhost:1729 2>&1)
-echo "ini $output"
-echo "end $end_val"
 while [[ $output -le $end_val ]]; do
     sleep 5
     output=$(cast balance $account --rpc-url=http://localhost:1729 2>&1)
-    echo "out $output"
 done
 echo "Balance of $output reached, killing process reth"
 
