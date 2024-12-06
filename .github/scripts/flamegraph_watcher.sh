3ff306380842fc098ce1ab66d179025ecf5af1de
Dylan Socolobsky <dylan.socolobsky@lambdaclass.com>
2024-12-06 13:19:27 -0300

    Measure load test time more accurately

--- .github/scripts/flamegraph_watcher.sh
+++ .github/scripts/flamegraph_watcher.sh
@@ -4,18 +4,23 @@
 # then polls the account balance until the expected balance has been reached
 # and then kills the process. It also measures the elapsed time of the test and
 # outputs it to Github Action's outputs.
-SECONDS=0
-
 iterations=1000
-value=10000000
+value=1
 account=0x33c6b73432B3aeA0C1725E415CC40D04908B85fd
 end_val=$((172 * $iterations * $value))
 
 ethrex_l2 test load --path /home/runner/work/ethrex/ethrex/test_data/private_keys.txt -i $iterations -v  --value $value --to $account
 
 output=$(cast balance $account --rpc-url=http://localhost:1729 2>&1)
-while [[ $output -le $end_val ]]; do
+while [[ $output -lt 1 ]]; do
     sleep 5
+    echo "Balance is $output"
+    output=$(cast balance $account --rpc-url=http://localhost:1729 2>&1)
+done
+SECONDS=0 # Server is online since balance started, so start measuring time
+while [[ $output -lt $end_val ]]; do
+    sleep 5
+    echo "Balance is $output waiting for it to reach $end_val"
     output=$(cast balance $account --rpc-url=http://localhost:1729 2>&1)
 done
 elapsed=$SECONDS
