8765cea8b79055e7cab6a34d71667dcf905fbf55
maximopalopoli <mpalopoli@fi.uba.ar>
2024-12-06 14:52:59 -0300

    create get_valid_jump_destinations function

--- crates/vm/levm/src/vm.rs
+++ crates/vm/levm/src/vm.rs
@@ -71,6 +71,43 @@ pub fn word_to_address(word: U256) -> Address {
     Address::from_slice(&bytes[12..])
 }
 
+fn get_valid_jump_destinations(code: &Bytes) -> Result<HashSet<usize>, VMError> {
+    let mut valid_jump_destinations = HashSet::new();
+    let mut pc = 0;
+
+    while pc < code.len() {
+        let code_position = match code.get(pc) {
+            Some(code) => *code,
+            None => {
+                break;
+            }
+        };
+
+        let current_opcode = Opcode::from(code_position);
+
+        if current_opcode == Opcode::JUMPDEST {
+            // If current opcode is jumpdest, add it to valid destinations set
+            valid_jump_destinations.insert(pc);
+        } else if (Opcode::PUSH1..=Opcode::PUSH32).contains(&current_opcode) {
+            // If current opcode is push, skip as many positions as the size of the push
+            let pushed_size = code_position
+                .checked_sub(u8::from(Opcode::PUSH1))
+                .ok_or(VMError::InvalidBytecode)?;
+            let skip_length =
+                usize::from(pushed_size.checked_add(1).ok_or(VMError::InvalidBytecode)?);
+            pc = pc.checked_add(skip_length).ok_or(VMError::Internal(
+                InternalError::ArithmeticOperationOverflow, // to fail, pc should be at least usize max - 31
+            ))?;
+        }
+
+        pc = pc.checked_add(1).ok_or(VMError::Internal(
+            InternalError::ArithmeticOperationOverflow, // to fail, code len should be more than usize max
+        ))?;
+    }
+
+    Ok(valid_jump_destinations)
+}
+
 impl VM {
     // TODO: Refactor this.
     #[allow(clippy::too_many_arguments)]
