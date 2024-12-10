46623d3aadbe6ea8524dc904fe3b46eabca593a4
Leandro Serra <leandro.serra@lambdaclass.com>
2024-12-10 14:31:44 -0300

    fix add check for size 0 memory operations staticcall

--- crates/vm/levm/src/opcode_handlers/system.rs
+++ crates/vm/levm/src/opcode_handlers/system.rs
@@ -1,7 +1,7 @@
 use crate::{
     call_frame::CallFrame,
     constants::WORD_SIZE_IN_BYTES_USIZE,
-    errors::{InternalError, OpcodeSuccess, OutOfGasError, ResultReason, VMError},
+    errors::{OpcodeSuccess, OutOfGasError, ResultReason, VMError},
     gas_cost,
     memory::{self, calculate_memory_size},
     vm::{word_to_address, VM},
@@ -328,22 +328,26 @@ impl VM {
 
         let (_account_info, address_was_cold) = self.access_account(code_address);
 
-        let new_memory_size_for_args = (args_start_offset
-            .checked_add(args_size)
-            .ok_or(InternalError::ArithmeticOperationOverflow)?)
-        .checked_next_multiple_of(WORD_SIZE_IN_BYTES_USIZE)
-        .ok_or(VMError::Internal(
-            InternalError::ArithmeticOperationOverflow,
-        ))?;
-        let new_memory_size_for_return_data = (return_data_start_offset
-            .checked_add(return_data_size)
-            .ok_or(InternalError::ArithmeticOperationOverflow)?)
-        .checked_next_multiple_of(WORD_SIZE_IN_BYTES_USIZE)
-        .ok_or(VMError::Internal(
-            InternalError::ArithmeticOperationOverflow,
-        ))?;
-        let new_memory_size = new_memory_size_for_args.max(new_memory_size_for_return_data);
         let current_memory_size = current_call_frame.memory.len();
+        let new_memory_size_for_args = if args_size > 0 {
+            (args_start_offset
+                .checked_add(args_size)
+                .ok_or(VMError::OutOfGas(OutOfGasError::MaxGasLimitExceeded))?)
+            .checked_next_multiple_of(WORD_SIZE_IN_BYTES_USIZE)
+            .ok_or(VMError::OutOfGas(OutOfGasError::MaxGasLimitExceeded))?
+        } else {
+            current_memory_size
+        };
+        let new_memory_size_for_return_data = if return_data_size > 0 {
+            (return_data_start_offset
+                .checked_add(return_data_size)
+                .ok_or(VMError::OutOfGas(OutOfGasError::MaxGasLimitExceeded))?)
+            .checked_next_multiple_of(WORD_SIZE_IN_BYTES_USIZE)
+            .ok_or(VMError::OutOfGas(OutOfGasError::MaxGasLimitExceeded))?
+        } else {
+            current_memory_size
+        };
+        let new_memory_size = new_memory_size_for_args.max(new_memory_size_for_return_data);
 
         self.increase_consumed_gas(
             current_call_frame,
