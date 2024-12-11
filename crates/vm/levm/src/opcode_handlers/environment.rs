496c64e4f429e2312e8fa7745f5e2797cea83081
Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>
2024-12-11 11:21:41 -0300

    WIP: If the offset for the bytecode is larger than the bytecode itself, return 0's

--- crates/vm/levm/src/opcode_handlers/environment.rs
+++ crates/vm/levm/src/opcode_handlers/environment.rs
@@ -220,11 +220,7 @@ impl VM {
             .pop()?
             .try_into()
             .map_err(|_| VMError::VeryLargeNumber)?;
-        let code_offset: usize = current_call_frame
-            .stack
-            .pop()?
-            .try_into()
-            .map_err(|_| VMError::VeryLargeNumber)?;
+        let code_offset = current_call_frame.stack.pop()?;
         let size: usize = current_call_frame
             .stack
             .pop()?
@@ -243,15 +239,18 @@ impl VM {
         }
 
         let mut data = vec![0u8; size];
-        for (i, byte) in current_call_frame
-            .bytecode
-            .iter()
-            .skip(code_offset)
-            .take(size)
-            .enumerate()
-        {
-            if let Some(data_byte) = data.get_mut(i) {
-                *data_byte = *byte;
+
+        if code_offset < current_call_frame.bytecode.len().into() {
+            for (i, byte) in current_call_frame
+                .bytecode
+                .iter()
+                .skip(code_offset)
+                .take(size)
+                .enumerate()
+            {
+                if let Some(data_byte) = data.get_mut(i) {
+                    *data_byte = *byte;
+                }
             }
         }
 
